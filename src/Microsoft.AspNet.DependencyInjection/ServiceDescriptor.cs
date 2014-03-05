a50f3a61074c2bf351a10d44c0b5ea31191bd858
Stephen Halter <halter73@gmail.com>
2014-03-04 18:42:03 -0800

    Added an implementation of IServiceDescriptor
    #19

---
+++ src/Microsoft.AspNet.DependencyInjection/ServiceDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.AspNet.DependencyInjection
+{
+    public class ServiceDescriptor : IServiceDescriptor
+    {
+        public LifecycleKind Lifecycle { get; set;  }
+        public Type ServiceType { get; set;  }
+
+        // Exactly one of the two following properties should be set
+        public Type ImplementationType { get; set; } // nullable
+        public object ImplementationInstance { get; set; } // nullable
+    }
+}
