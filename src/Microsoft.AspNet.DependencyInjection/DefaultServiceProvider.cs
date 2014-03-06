c32997c13cd7bbfd15f295ab249c8de7264ff387
Stephen Halter <halter73@gmail.com>
2014-03-06 12:59:48 -0800

    Static DefaultServiceProvider.Create
    - Requires an IServiceProvider to be wrapped
    - Eventually ServiceProvider won't be public so this should be used instead

---
+++ src/Microsoft.AspNet.DependencyInjection/DefaultServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.DependencyInjection
+{
+    public static class DefaultServiceProvider
+    {
+        public static IServiceProvider Create(
+                IServiceProvider wrappedProvider,
+                IEnumerable<IServiceDescriptor> descriptors)
+        {
+            var provider = new ServiceProvider(wrappedProvider);
+            return provider.Add(descriptors);
+        }
+    }
+}
