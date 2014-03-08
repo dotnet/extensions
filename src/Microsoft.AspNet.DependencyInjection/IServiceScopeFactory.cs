7cc5f5bdbc33ae13d2578368f827d9d338f462d4
Stephen Halter <halter73@gmail.com>
2014-03-07 17:06:12 -0800

    Added IServiceScopeFactory

---
+++ src/Microsoft.AspNet.DependencyInjection/IServiceScopeFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Net.Runtime;
+
+namespace Microsoft.AspNet.DependencyInjection
+{
+    [NotAssemblyNeutral]
+    public interface IServiceScopeFactory
+    {
+        /// <summary>
+        /// Create an <see cref="System.IServiceProvider"/> which can be used to resolve
+        /// dependencies from a newly created scope.
+        /// </summary>
+        /// <param name="scope">
+        /// The <see cref="System.IServiceProvider"/> used to resolve dependencies from the scope.
+        /// </param>
+        /// <returns>
+        /// An <see cref="System.IDisposable"/> controlling the lifetime of the scope. Once this is
+        /// disposed, any scoped services that have been resolved from the scope will be disposed.
+        /// </returns>
+        IDisposable CreateScope(out IServiceProvider scope);
+    }
+}
