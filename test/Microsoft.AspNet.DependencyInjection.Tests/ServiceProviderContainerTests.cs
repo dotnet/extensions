c3197d0c11662fbd65795196f95241172d59b3a3
Stephen Halter <halter73@gmail.com>
2014-03-17 17:10:27 -0700

    Copied tests from AutofacContainerTests to ServiceProviderContainerTests
    - ServicesCanBeAddedToServicesFromFallbackServiceProvider was changed
      to ServicesFromFallbackServicProviderCanBeReplacedAndIEnumerableResolved
      in order to reflect the new expected behavior

--- test/Microsoft.AspNet.DependencyInjection.Tests/ServiceProviderContainerTests.cs
+++ test/Microsoft.AspNet.DependencyInjection.Tests/ServiceProviderContainerTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNet.DependencyInjection.Fallback;
 using Microsoft.AspNet.DependencyInjection.Tests.Fakes;
+using Xunit;
 
 namespace Microsoft.AspNet.DependencyInjection.Tests
 {
@@ -10,7 +13,104 @@ namespace Microsoft.AspNet.DependencyInjection.Tests
         {
             return new ServiceCollection()
                 .Add(TestServices.DefaultServices())
-                .BuildServiceProvider();
+                .BuildServiceProvider(new FakeFallbackServiceProvider());
+        }
+
+        [Fact]
+        public void SingletonServiceCanBeResolved()
+        {
+            var container = CreateContainer();
+
+            var service1 = container.GetService<IFakeSingletonService>();
+            var service2 = container.GetService<IFakeSingletonService>();
+
+            Assert.NotNull(service1);
+            Assert.Equal(service1, service2);
+        }
+
+        [Fact]
+        public void ScopedServiceCanBeResolved()
+        {
+            var container = CreateContainer();
+
+            var scopeFactory = container.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var containerScopedService = container.GetService<IFakeScopedService>();
+                var scopedService1 = scope.ServiceProvider.GetService<IFakeScopedService>();
+                var scopedService2 = scope.ServiceProvider.GetService<IFakeScopedService>();
+
+                Assert.NotEqual(containerScopedService, scopedService1);
+                Assert.Equal(scopedService1, scopedService2);
+            }
+        }
+
+        [Fact]
+        public void NestedScopedServiceCanBeResolved()
+        {
+            var container = CreateContainer();
+
+            var outerScopeFactory = container.GetService<IServiceScopeFactory>();
+            using (var outerScope = outerScopeFactory.CreateScope())
+            {
+                var innerScopeFactory = outerScope.ServiceProvider.GetService<IServiceScopeFactory>();
+                using (var innerScope = innerScopeFactory.CreateScope())
+                {
+                    var outerScopedService = outerScope.ServiceProvider.GetService<IFakeScopedService>();
+                    var innerScopedService = innerScope.ServiceProvider.GetService<IFakeScopedService>();
+
+                    Assert.NotEqual(outerScopedService, innerScopedService);
+                }
+            }
+        }
+
+        [Fact]
+        public void DisposingScopeDisposesService()
+        {
+            var container = CreateContainer();
+            FakeService disposableService;
+
+            var scopeFactory = container.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                disposableService = (FakeService)scope.ServiceProvider.GetService<IFakeScopedService>();
+
+                Assert.False(disposableService.Disposed);
+            }
+
+            Assert.True(disposableService.Disposed);
+        }
+
+        [Fact]
+        public void ServicesCanBeResolvedFromFallbackServiceProvider()
+        {
+            var container = CreateContainer();
+
+            var service = container.GetService<string>();
+
+            Assert.Equal("FakeFallbackServiceProvider", service);
+        }
+
+        [Fact]
+        public void ServicesFromFallbackServicProviderCanBeReplaced()
+        {
+            var container = CreateContainer();
+
+            var service = container.GetService<IFakeFallbackService>();
+
+            Assert.Equal("FakeServiceSimpleMethod", service.SimpleMethod());
+        }
+
+        [Fact]
+        public void ServicesFromFallbackServicProviderCanBeReplacedAndIEnumerableResolved()
+        {
+            var container = CreateContainer();
+
+            var services = container.GetService<IEnumerable<IFakeFallbackService>>();
+            var messages = services.Select(service => service.SimpleMethod());
+
+            Assert.Equal(1, services.Count());
+            Assert.Contains("FakeServiceSimpleMethod", messages);
         }
     }
 }
