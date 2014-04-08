78e2fe5e4ae2c7db33ecbca782e5933a60e7af72
Wei Wang <chengtian.wang@gmail.com>
2014-04-07 17:00:39 -0700

    Strip prefix off when reading connection strings from Azure WebSites

--- src/Microsoft.AspNet.ConfigurationModel/Sources/EnvironmentVariablesConfigurationSource.cs
+++ src/Microsoft.AspNet.ConfigurationModel/Sources/EnvironmentVariablesConfigurationSource.cs
@@ -7,6 +7,7 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
     public class EnvironmentVariablesConfigurationSource : BaseConfigurationSource
     {
         private readonly string _prefix;
+        private static readonly string AzureConnStrPrefix = "SQLAZURECONNSTR_";
 
         public EnvironmentVariablesConfigurationSource(string prefix)
         {
@@ -22,6 +23,9 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
         {
             ReplaceData(Environment.GetEnvironmentVariables()
                 .Cast<DictionaryEntry>()
+                .Select(entry => 
+                    ((string)entry.Key).StartsWith(AzureConnStrPrefix, StringComparison.OrdinalIgnoreCase) ? 
+                    new DictionaryEntry(((string)entry.Key).Substring(AzureConnStrPrefix.Length), entry.Value) : entry)
                 .Where(entry => ((string)entry.Key).StartsWith(_prefix, StringComparison.OrdinalIgnoreCase))
                 .ToDictionary(
                     entry => ((string)entry.Key).Substring(_prefix.Length),
