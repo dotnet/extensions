25c6c32c14243bd5bddd3c7c54a5b61cd4a865fd
Wei Wang <chengtian.wang@gmail.com>
2014-05-02 11:51:46 -0700

    Add Commit() method in XmlConfigurationSource

--- test/Microsoft.AspNet.ConfigurationModel.Xml.Test/XmlConfigurationSourceTest.cs
+++ test/Microsoft.AspNet.ConfigurationModel.Xml.Test/XmlConfigurationSourceTest.cs
@@ -13,8 +13,8 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
         [Fact]
         public void LoadKeyValuePairsFromValidXml()
         {
-            var xml = 
-                @"<settings>
+            var xml = @"
+                <settings>
                     <Data>
                         <DefaultConnection>
                             <ConnectionString>TestConnectionString</ConnectionString>
@@ -41,16 +41,16 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
         public void CommonAttributesContributeToKeyValuePairs()
         {
             var xml =
-                @"<settings Port='8008'>
-                    <Data>
-                        <DefaultConnection
-                            ConnectionString='TestConnectionString'
-                            Provider='SqlClient'/>
-                        <Inventory
-                            ConnectionString='AnotherTestConnectionString'
-                            Provider='MySql'/>
-                    </Data>
-                </settings>";
+@"<settings Port='8008'>
+    <Data>
+        <DefaultConnection
+            ConnectionString='TestConnectionString'
+            Provider='SqlClient'/>
+        <Inventory
+            ConnectionString='AnotherTestConnectionString'
+            Provider='MySql'/>
+    </Data>
+</settings>";
             var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
 
             xmlConfigSrc.Load(StringToStream(xml));
@@ -365,6 +365,219 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
             Assert.Equal(expectedMsg, exception.Message);
         }
 
+        [Fact]
+        public void CommitMethodPreservesCommmentsAndProcessingInstructionsAndWhiteSpaces()
+        {
+            var xml = @"<?xml version='1.0' encoding='UTF-8'?>
+                    <?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+                    <settings>
+                        <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+                        <Data>
+                            <DefaultConnection>
+                                <ConnectionString>TestConnectionString</ConnectionString>
+                                <Provider>SqlClient</Provider>
+                            </DefaultConnection>
+                            <Inventory>
+                                <ConnectionString>AnotherTestConnectionString</ConnectionString>
+                                <Provider>MySql</Provider>
+                            </Inventory>
+                        </Data>
+                    </settings>";
+            xml = xml.Replace('\'', '"');
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Load(StringToStream(xml));
+
+            xmlConfigSrc.Commit(StringToStream(xml), outputCacheStream);
+
+            var newContents = StreamToString(outputCacheStream);
+            Assert.Equal(xml, newContents);
+        }
+
+        [Fact]
+        public void CommitMethodUpdatesValues()
+        {
+            var xml = @"<?xml version='1.0' encoding='UTF-8'?>
+<?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+<settings>
+    <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+    <Data>
+        <DefaultConnection>
+            <ConnectionString>TestConnectionString</ConnectionString>
+            <Provider>SqlClient</Provider>
+        </DefaultConnection>
+        <Inventory>
+            <ConnectionString>AnotherTestConnectionString</ConnectionString>
+            <Provider>MySql</Provider>
+        </Inventory>
+    </Data>
+</settings>";
+            xml = xml.Replace('\'', '"');
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Load(StringToStream(xml));
+            xmlConfigSrc.Set("Data:DefaultConnection:Provider", "NewSqlClient");
+            xmlConfigSrc.Set("Data:Inventory:Provider", "NewMySql");
+
+            xmlConfigSrc.Commit(StringToStream(xml), outputCacheStream);
+
+            var newContents = StreamToString(outputCacheStream);
+            Assert.Equal(xml.Replace("SqlClient", "NewSqlClient").Replace("MySql", "NewMySql"), newContents);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenFindInvalidModificationAfterLoadOperation()
+        {
+            var xml = @"<?xml version='1.0' encoding='UTF-8'?>
+                    <?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+                    <settings>
+                        <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+                        <Data>
+                            <DefaultConnection>
+                                <ConnectionString>TestConnectionString</ConnectionString>
+                                <Provider>SqlClient</Provider>
+                            </DefaultConnection>
+                            <Inventory>
+                                <ConnectionString>AnotherTestConnectionString</ConnectionString>
+                                <Provider>MySql</Provider>
+                            </Inventory>
+                        </Data>
+                    </settings>";
+            var modifiedXml = @"<?xml version='1.0' encoding='UTF-8'?>
+                    <?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+                    <settings xmlns:MyNameSpace='http://microsoft.com/wwa/mynamespace'>
+                        <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+                        <MyNameSpace:Data>
+                            <DefaultConnection>
+                                <ConnectionString>TestConnectionString</ConnectionString>
+                                <Provider>SqlClient</Provider>
+                            </DefaultConnection>
+                            <Inventory>
+                                <ConnectionString>AnotherTestConnectionString</ConnectionString>
+                                <Provider>MySql</Provider>
+                            </Inventory>
+                        </MyNameSpace:Data>
+                    </settings>";
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Load(StringToStream(xml));
+
+            var exception = Assert.Throws<FormatException>(
+                () => xmlConfigSrc.Commit(StringToStream(modifiedXml), outputCacheStream));
+
+            Assert.Equal(Resources.FormatError_NamespaceIsNotSupported(Resources.FormatMsg_LineInfo(3, 31)),
+                exception.Message);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenFindNewlyAddedKeyAfterLoadOperation()
+        {
+            var xml = @"<?xml version='1.0' encoding='UTF-8'?>
+<?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+<settings>
+    <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+    <Data>
+        <DefaultConnection>
+            <ConnectionString>TestConnectionString</ConnectionString>
+            <Provider>SqlClient</Provider>
+        </DefaultConnection>
+        <Inventory>
+            <ConnectionString>AnotherTestConnectionString</ConnectionString>
+            <Provider>MySql</Provider>
+        </Inventory>
+    </Data>
+</settings>";
+            var modifiedXml = @"<?xml version='1.0' encoding='UTF-8'?>
+<?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+<settings>
+    <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+    <Data>
+        <DefaultConnection>
+            <ConnectionString>TestConnectionString</ConnectionString>
+            <Provider>SqlClient</Provider>
+            <NewKey>NewValue</NewKey>
+        </DefaultConnection>
+        <Inventory>
+            <ConnectionString>AnotherTestConnectionString</ConnectionString>
+            <Provider>MySql</Provider>
+        </Inventory>
+    </Data>
+</settings>";
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Load(StringToStream(xml));
+
+            var exception = Assert.Throws<InvalidOperationException>(
+                () => xmlConfigSrc.Commit(StringToStream(modifiedXml), outputCacheStream));
+
+            Assert.Equal(
+                Resources.FormatError_CommitWhenNewKeyFound("Data:DefaultConnection:NewKey"), exception.Message);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenKeysAreMissingInConfigFile()
+        {
+            var xml = @"<?xml version='1.0' encoding='UTF-8'?>
+<?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+<settings>
+    <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+    <Data>
+        <DefaultConnection>
+            <ConnectionString>TestConnectionString</ConnectionString>
+            <Provider>SqlClient</Provider>
+        </DefaultConnection>
+        <Inventory>
+            <ConnectionString>AnotherTestConnectionString</ConnectionString>
+            <Provider>MySql</Provider>
+        </Inventory>
+    </Data>
+</settings>";
+            var modifiedXml = @"<?xml version='1.0' encoding='UTF-8'?>
+<?xml-stylesheet type='text/xsl' href='style1.xsl'?>
+<settings>
+    <?xml-stylesheet type='text/xsl' href='style2.xsl'?>
+    <Data>
+        <DefaultConnection>
+            <ConnectionString>TestConnectionString</ConnectionString>
+        </DefaultConnection>
+        <Inventory>
+            <Provider>MySql</Provider>
+        </Inventory>
+    </Data>
+</settings>";
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Load(StringToStream(xml));
+
+            var exception = Assert.Throws<InvalidOperationException>(
+                () => xmlConfigSrc.Commit(StringToStream(modifiedXml), outputCacheStream));
+
+            Assert.Equal(
+                Resources.
+                FormatError_CommitWhenKeyMissing("Data:DefaultConnection:Provider, Data:Inventory:ConnectionString"),
+                exception.Message);
+        }
+
+        [Fact]
+        public void CanCreateNewConfig()
+        {
+            var targetXml = @"<?xml version='1.0' encoding='utf-8'?>
+<settings>
+  <Key1 Name='Key2:Key3'>Value1</Key1>
+  <Key4>Value2</Key4>
+</settings>";
+            targetXml = targetXml.Replace('\'', '"');
+            var xmlConfigSrc = new XmlConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            xmlConfigSrc.Data["Key1:Key2:Key3"] = "Value1";
+            xmlConfigSrc.Data["Key4"] = "Value2";
+
+            xmlConfigSrc.GenerateNewConfig(outputCacheStream);
+
+            var newContents = StreamToString(outputCacheStream);
+            Assert.Equal(targetXml, newContents);
+        }
+
         private static Stream StringToStream(string str)
         {
             var memStream = new MemoryStream();
@@ -374,6 +587,14 @@ namespace Microsoft.AspNet.ConfigurationModel.Sources
             memStream.Seek(0, SeekOrigin.Begin);
 
             return memStream;
+        }
+
+        private static string StreamToString(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            StreamReader reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
         }
     }
 }
