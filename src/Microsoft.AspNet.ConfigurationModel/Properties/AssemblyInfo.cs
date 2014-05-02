3b536718df4cdf4f22200888c18d187685d7a551
Brice Lambson <bricelam@microsoft.com>
2014-05-02 09:21:42 -0700

    Re-add using statement that was accidentally removed while resolving a merge conflict

--- src/Microsoft.AspNet.ConfigurationModel/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.ConfigurationModel/Properties/AssemblyInfo.cs
@@ -16,6 +16,7 @@
 // permissions and limitations under the License.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
