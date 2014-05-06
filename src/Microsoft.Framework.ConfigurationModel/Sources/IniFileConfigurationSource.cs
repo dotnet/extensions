3ea67bd3d860330f6f95bc27fa6369062d605bf5
Wei Wang <chengtian.wang@gmail.com>
2014-05-05 18:14:42 -0700

    Rename namespace to Microsoft.Framework.ConfigurationModel

---
+++ src/Microsoft.Framework.ConfigurationModel/Sources/IniFileConfigurationSource.cs
@@ -0,0 +1,269 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+// WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF
+// TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR
+// NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing
+// permissions and limitations under the License.
+
+#if NET45 || K10
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.Framework.ConfigurationModel.Sources
+{
+    public class IniFileConfigurationSource : BaseConfigurationSource, ICommitableConfigurationSource
+    {
+        // http://en.wikipedia.org/wiki/INI_file
+        /// <summary>
+        /// Files are simple line structures
+        /// [Section:Header]
+        /// key1=value1
+        /// key2 = " value2 "
+        /// ; comment
+        /// # comment
+        /// / comment
+        /// </summary>
+        /// <param name="path">The path and file name to load.</param>
+        public IniFileConfigurationSource(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException(Resources.Error_InvalidFilePath, "path");
+            }
+
+            Path = path;
+        }
+
+        public string Path { get; private set; }
+
+        public override void Load()
+        {
+            using (var stream = new FileStream(Path, FileMode.Open))
+            {
+                Load(stream);
+            }
+        }
+        
+        public virtual void Commit()
+        {
+            // If the config file is not found in given path
+            // i.e. we don't have a template to follow when generating contents of new config file
+            if (!File.Exists(Path))
+            {
+                var newConfigFileStream = new FileStream(Path, FileMode.CreateNew);
+
+                try
+                {
+                    // Generate contents and write it to the newly created config file
+                    GenerateNewConfig(newConfigFileStream);
+                }
+                catch
+                {
+                    newConfigFileStream.Dispose();
+
+                    // The operation should be atomic because we don't want a corrupted config file
+                    // So we roll back if the operation fails
+                    if (File.Exists(Path))
+                    {
+                        File.Delete(Path);
+                    }
+
+                    // Rethrow the exception
+                    throw;
+                }
+                finally
+                {
+                    newConfigFileStream.Dispose();
+                }
+
+                return;
+            }
+
+            // Because we need to read the original contents while generating new contents, the new contents are
+            // cached in memory and used to overwrite original contents after we finish reading the original contents
+            using (var cacheStream = new MemoryStream())
+            {
+                using (var inputStream = new FileStream(Path, FileMode.Open))
+                {
+                    Commit(inputStream, cacheStream);
+                }
+
+                // Use the cached new contents to overwrite original contents
+                cacheStream.Seek(0, SeekOrigin.Begin);
+                using (var outputStream = new FileStream(Path, FileMode.Truncate))
+                {
+                    cacheStream.CopyTo(outputStream);
+                }
+            }
+        }
+
+        internal void Load(Stream stream)
+        {
+            var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var sectionPrefix = string.Empty;
+
+                while (reader.Peek() != -1)
+                {
+                    var rawLine = reader.ReadLine();
+                    var line = rawLine.Trim();
+
+                    // Ignore blank lines
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    // Ignore comments
+                    if (line[0] == ';' || line[0] == '#' || line[0] == '/')
+                    {
+                        continue;
+                    }
+                    // [Section:header] 
+                    if (line[0] == '[' && line[line.Length - 1] == ']')
+                    {
+                        // remove the brackets
+                        sectionPrefix = line.Substring(1, line.Length - 2) + ":";
+                        continue;
+                    }
+
+                    // key = value OR "value"
+                    int separator = line.IndexOf('=');
+                    if (separator < 0)
+                    {
+                        throw new FormatException(Resources.FormatError_UnrecognizedLineFormat(rawLine));
+                    }
+
+                    string key = sectionPrefix + line.Substring(0, separator).Trim();
+                    string value = line.Substring(separator + 1).Trim();
+
+                    // Remove quotes
+                    if (value.Length > 1 && value[0] == '"' && value[value.Length - 1] == '"')
+                    {
+                        value = value.Substring(1, value.Length - 2);
+                    }
+
+                    if (data.ContainsKey(key))
+                    {
+                        throw new FormatException(Resources.FormatError_KeyIsDuplicated(key));
+                    }
+
+                    data[key] = value;
+                }
+            }
+
+            ReplaceData(data);
+        }
+
+        // Use the original file as a template while generating new file contents
+        // to make sure the format is consistent and comments are not lost
+        internal void Commit(Stream inputStream, Stream outputStream)
+        {
+            var processedKeys = new HashSet<string>();
+            var outputWriter = new StreamWriter(outputStream);
+
+            using (var inputReader = new StreamReader(inputStream))
+            {
+                var sectionPrefix = string.Empty;
+
+                while (inputReader.Peek() != -1)
+                {
+                    var rawLine = inputReader.ReadLine();
+                    var line = rawLine.Trim();
+
+                    // Is this the last line?
+                    var lineEnd = inputReader.Peek() == -1 ? string.Empty : Environment.NewLine;
+
+                    // Ignore blank lines
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        outputWriter.Write(rawLine + lineEnd);
+                        continue;
+                    }
+                    // Ignore comments
+                    if (line[0] == ';' || line[0] == '#' || line[0] == '/')
+                    {
+                        outputWriter.Write(rawLine + lineEnd);
+                        continue;
+                    }
+                    // [Section:header] 
+                    if (line[0] == '[' && line[line.Length - 1] == ']')
+                    {
+                        outputWriter.Write(rawLine + lineEnd);
+
+                        // remove the brackets
+                        sectionPrefix = line.Substring(1, line.Length - 2) + ":";
+                        continue;
+                    }
+
+                    // key = value OR "value"
+                    int separator = line.IndexOf('=');
+                    if (separator < 0)
+                    {
+                        throw new FormatException(Resources.FormatError_UnrecognizedLineFormat(rawLine));
+                    }
+
+                    var key = sectionPrefix + line.Substring(0, separator).Trim();
+                    var value = line.Substring(separator + 1).Trim();
+
+                    // Output preserves white spaces in original file
+                    int rawSeparator = rawLine.IndexOf('=');
+                    var outKeyStr = rawLine.Substring(0, rawSeparator);
+                    var outValueStr = rawLine.Substring(rawSeparator + 1);
+
+                    // Remove quotes
+                    if (value.Length > 1 && value[0] == '"' && value[value.Length - 1] == '"')
+                    {
+                        value = value.Substring(1, value.Length - 2);
+                    }
+
+                    if (!Data.ContainsKey(key))
+                    {
+                        throw new InvalidOperationException(Resources.FormatError_CommitWhenNewKeyFound(key));
+                    }
+
+                    outValueStr = outValueStr.Replace(value, Data[key]);
+
+                    outputWriter.Write(string.Format("{0}={1}{2}", outKeyStr, outValueStr, lineEnd));
+
+                    processedKeys.Add(key);
+                }
+
+                outputWriter.Flush();
+            }
+
+            if (Data.Count() != processedKeys.Count())
+            {
+                var missingKeys = string.Join(", ", Data.Keys.Except(processedKeys));
+                throw new InvalidOperationException(Resources.FormatError_CommitWhenKeyMissing(missingKeys));
+            }
+        }
+
+        // Write the contents of newly created config file to given stream
+        internal void GenerateNewConfig(Stream outputStream)
+        {
+            var outputWriter = new StreamWriter(outputStream);
+
+            foreach (var entry in Data)
+            {
+                outputWriter.WriteLine(string.Format("{0}={1}", entry.Key, entry.Value));
+            }
+
+            outputWriter.Flush();
+        }
+    }
+}
+#endif
---
+++ test/Microsoft.Framework.ConfigurationModel.Json.Test/JsonConfigurationSourceTest.cs
@@ -0,0 +1,313 @@
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+// WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF
+// TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR
+// NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.IO;
+using Microsoft.Framework.ConfigurationModel.Sources;
+using Xunit;
+
+using Resources = Microsoft.Framework.ConfigurationModel.Json.Resources;
+
+namespace Microsoft.Framework.ConfigurationModel.Sources
+{
+    public class JsonConfigurationSourceTest
+    {
+        private static readonly string ArbitraryFilePath = "Unit tests do not touch file system";
+
+        [Fact]
+        public void LoadKeyValuePairsFromValidJson()
+        {
+            var json = @"
+{
+    'name': 'test',
+    'address': {
+        'street': 'Something street',
+        'zipcode': '12345'
+    }
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+
+            jsonConfigSrc.Load(StringToStream(json));
+
+            Assert.Equal(3, jsonConfigSrc.Data.Count);
+            Assert.Equal("test", jsonConfigSrc.Data["NAME"]);
+            Assert.Equal("Something street", jsonConfigSrc.Data["address:STREET"]);
+            Assert.Equal("12345", jsonConfigSrc.Data["address:zipcode"]);
+        }
+
+        [Fact]
+        public void NonObjectRootIsInvalid()
+        {
+            var json = @"'test'";
+            var jsonConfigSource = new JsonConfigurationSource(ArbitraryFilePath);
+            var expectedMsg = Resources.FormatError_RootMustBeAnObject(string.Empty, 1, 6);
+
+            var exception = Assert.Throws<FormatException>(() => jsonConfigSource.Load(StringToStream(json)));
+
+            Assert.Equal(expectedMsg, exception.Message);
+        }
+
+        [Fact]
+        public void SupportAndIgnoreComments()
+        {
+            var json = @"/* Comments */
+                {/* Comments */
+                ""name"": /* Comments */ ""test"",
+                ""address"": {
+                    ""street"": ""Something street"", /* Comments */
+                    ""zipcode"": ""12345""
+                }
+            }";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+
+            jsonConfigSrc.Load(StringToStream(json));
+
+            Assert.Equal(3, jsonConfigSrc.Data.Count);
+            Assert.Equal("test", jsonConfigSrc.Data["name"]);
+            Assert.Equal("Something street", jsonConfigSrc.Data["address:street"]);
+            Assert.Equal("12345", jsonConfigSrc.Data["address:zipcode"]);
+        }
+
+        [Fact]
+        public void ArraysAreNotSupported()
+        {
+            var json = @"{
+                'name': 'test',
+                'address': ['Something street', '12345']
+            }";
+            var jsonConfigSource = new JsonConfigurationSource(ArbitraryFilePath);
+            var expectedMsg = Resources.FormatError_UnsupportedJSONToken("StartArray", "address", 3, 29);
+
+            var exception = Assert.Throws<FormatException>(() => jsonConfigSource.Load(StringToStream(json)));
+
+            Assert.Equal(expectedMsg, exception.Message);
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenUnexpectedEndFoundBeforeFinishParsing()
+        {
+            var json = @"{
+                'name': 'test',
+                'address': {
+                    'street': 'Something street',
+                    'zipcode': '12345'
+                }
+            /* Missing a right brace here*/";
+            var jsonConfigSource = new JsonConfigurationSource(ArbitraryFilePath);
+            var expectedMsg = Resources.FormatError_UnexpectedEnd("address", 7, 44);
+
+            var exception = Assert.Throws<FormatException>(() => jsonConfigSource.Load(StringToStream(json)));
+
+            Assert.Equal(expectedMsg, exception.Message);
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenPassingNullAsFilePath()
+        {
+            var expectedMsg = new ArgumentException(Resources.Error_InvalidFilePath, "path").Message;
+
+            var exception = Assert.Throws<ArgumentException>(() => new JsonConfigurationSource(null));
+
+            Assert.Equal(expectedMsg, exception.Message);
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenPassingEmptyStringAsFilePath()
+        {
+            var expectedMsg = new ArgumentException(Resources.Error_InvalidFilePath, "path").Message;
+
+            var exception = Assert.Throws<ArgumentException>(() => new JsonConfigurationSource(string.Empty));
+
+            Assert.Equal(expectedMsg, exception.Message);
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenKeyIsDuplicated()
+        {
+            var json = @"{
+                'name': 'test',
+                'address': {
+                    'street': 'Something street',
+                    'zipcode': '12345'
+                },
+                'name': 'new name'
+            }";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+
+            var exception = Assert.Throws<FormatException>(() => jsonConfigSrc.Load(StringToStream(json)));
+
+            Assert.Equal(Resources.FormatError_KeyIsDuplicated("name"), exception.Message);
+        }
+
+        [Fact]
+        public void CommitMethodPreservesCommments()
+        {
+            var json = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Load(StringToStream(json));
+
+            jsonConfigSrc.Commit(StringToStream(json), outputCacheStream);
+
+            var newContents = StreamToString(outputCacheStream);
+            Assert.Equal(json, newContents);
+        }
+
+        [Fact]
+        public void CommitMethodUpdatesValues()
+        {
+            var json = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Load(StringToStream(json));
+            jsonConfigSrc.Set("name", "new_name");
+            jsonConfigSrc.Set("address:zipcode", "67890");
+
+            jsonConfigSrc.Commit(StringToStream(json), outputCacheStream);
+
+            var newContents = StreamToString(outputCacheStream);
+            Assert.Equal(json.Replace("test", "new_name").Replace("12345", "67890"), newContents);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenFindInvalidModificationAfterLoadOperation()
+        {
+            var json = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var modifiedJson = @"
+{
+  ""name"": [""first"", ""last""],
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Load(StringToStream(json));
+
+            var exception = Assert.Throws<FormatException>(
+                () => jsonConfigSrc.Commit(StringToStream(modifiedJson), outputCacheStream));
+
+            Assert.Equal(Resources.FormatError_UnsupportedJSONToken("StartArray", "name", 3, 12), exception.Message);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenFindNewlyAddedKeyAfterLoadOperation()
+        {
+            var json = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var newJson = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  },
+  ""NewKey"": ""NewValue""
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Load(StringToStream(json));
+
+            var exception = Assert.Throws<InvalidOperationException>(
+                () => jsonConfigSrc.Commit(StringToStream(newJson), outputCacheStream));
+
+            Assert.Equal(Resources.FormatError_CommitWhenNewKeyFound("NewKey"), exception.Message);
+        }
+
+        [Fact]
+        public void CommitOperationThrowsExceptionWhenKeysAreMissingInConfigFile()
+        {
+            var json = @"{
+  ""name"": ""test"",
+  ""address"": {
+    ""street"": ""Something street"",
+    ""zipcode"": ""12345""
+  }
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Load(StringToStream(json));
+            json = json.Replace(@"""name"": ""test"",", string.Empty);
+
+            var exception = Assert.Throws<InvalidOperationException>(
+                () => jsonConfigSrc.Commit(StringToStream(json), outputCacheStream));
+
+            Assert.Equal(Resources.FormatError_CommitWhenKeyMissing("name"), exception.Message);
+        }
+
+        [Fact]
+        public void CanCreateNewConfig()
+        {
+            var targetJson = @"{
+  ""name"": ""test"",
+  ""address:street"": ""Something street"",
+  ""address:zipcode"": ""12345""
+}";
+            var jsonConfigSrc = new JsonConfigurationSource(ArbitraryFilePath);
+            var outputCacheStream = new MemoryStream();
+            jsonConfigSrc.Data["name"] = "test";
+            jsonConfigSrc.Data["address:street"] = "Something street";
+            jsonConfigSrc.Data["address:zipcode"] = "12345";
+
+            jsonConfigSrc.GenerateNewConfig(outputCacheStream);
+
+            Assert.Equal(targetJson, StreamToString(outputCacheStream));
+        }
+
+        private static Stream StringToStream(string str)
+        {
+            var memStream = new MemoryStream();
+            var textWriter = new StreamWriter(memStream);
+            textWriter.Write(str);
+            textWriter.Flush();
+            memStream.Seek(0, SeekOrigin.Begin);
+
+            return memStream;
+        }
+
+        private static string StreamToString(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            StreamReader reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
