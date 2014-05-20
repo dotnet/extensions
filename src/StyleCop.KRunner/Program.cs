5a30e8aae3d8f0a0246b48c7d1a23b4777d9d817
Ryan Nowak <nowakra@gmail.com>
2014-05-20 12:46:15 -0700

    Adding projects for a stylecop runner
    - Runner is a console.exe because stylecop won't work on K10
    - Rules project is an empty placeholder for now
    
    - Next change will port our 'header' rule

---
+++ src/StyleCop.KRunner/Program.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+
+namespace StyleCop.KRunner
+{
+    public class Program
+    {
+        private static readonly List<Violation> _violations = new List<Violation>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("StyleCop.KRunner - A StyleCop commandline runner");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\tStyleCop.KRunner <path/to/project.json>");
+                Console.WriteLine();
+                return;
+            }
+
+            var projectFile = args[0];
+            if (!File.Exists(projectFile))
+            {
+                Console.WriteLine("File '{0}' does not exist.", Path.GetFullPath(projectFile));
+                return;
+            }
+
+            var runner = new StyleCopConsole(
+                settings: null, 
+                writeResultsCache: 
+                false, 
+                outputFile: null, 
+                addInPaths: null, 
+                loadFromDefaultPath: true); // Loads rules next to StyleCop.dll in the file system.
+
+            var projectDirectory = Path.GetDirectoryName(projectFile);
+
+            // It's OK if the settings file is null.
+            var settingsFile = FindSettingsFile(projectDirectory);
+
+            var project = new CodeProject(0, projectDirectory, new Configuration(null));
+            foreach (var file in Directory.EnumerateFiles(projectDirectory, "*.cs", SearchOption.AllDirectories))
+            {
+                runner.Core.Environment.AddSourceCode(project, file, context: null);
+            }
+
+            try
+            {
+                runner.OutputGenerated += Runner_OutputGenerated;
+                runner.ViolationEncountered += Runner_ViolationEncountered;
+
+                if (settingsFile == null)
+                {
+                    runner.Core.Analyze(new CodeProject[] { project });
+                }
+                else
+                {
+                    runner.Core.Analyze(new CodeProject[] { project }, settingsFile);
+                }
+            }
+            finally
+            {
+                runner.OutputGenerated -= Runner_OutputGenerated;
+                runner.ViolationEncountered -= Runner_ViolationEncountered;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("{0} errors found.", _violations.Count);
+            Console.WriteLine();
+        }
+
+        // Performs an ascending directory search starting at the project directory
+        private static string FindSettingsFile(string projectDirectory)
+        {
+            var current = new DirectoryInfo(projectDirectory);
+            var root = current.Root;
+
+            do
+            {
+                var settingsFile = Path.Combine(current.FullName, "Settings.StyleCop");
+                if (File.Exists(settingsFile))
+                {
+                    return settingsFile;
+                }
+            }
+            while ((current = current.Parent) != null);
+
+            return null;
+        }
+
+        private static void Runner_ViolationEncountered(object sender, ViolationEventArgs e)
+        {
+            Console.WriteLine("{0}: {1} Line {2} - {3}", e.Violation.Rule.CheckId, e.SourceCode.Path, e.LineNumber, e.Message);
+
+            _violations.Add(e.Violation);
+        }
+
+        private static void Runner_OutputGenerated(object sender, OutputEventArgs e)
+        {
+            // There will be a bunch of message like "processing file Bleh.cs" with low importance,
+            // we're intentionally excluding those.
+            if (e.Importance != MessageImportance.Low)
+            {
+                Console.WriteLine(e.Output);
+            }
+        }
+    }
+}
