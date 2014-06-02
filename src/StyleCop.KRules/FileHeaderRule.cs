08a24dcc27535bdfb4646d61e58923f939dde99a
Ryan Nowak <nowakra@gmail.com>
2014-06-02 14:03:02 -0700

    Porting old rules from webstack

---
+++ src/StyleCop.KRules/FileHeaderRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using StyleCop.CSharp;
+
+namespace StyleCop.KRules
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class FileHeaderRule : SourceAnalyzer
+    {
+        private const string FileHeaderTextPropertyName = "FileHeaderText";
+        private const string RuleName = "FileMustHaveHeader";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            var csharpDocument = (CsDocument)document;
+            if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated)
+            {
+                if (IsRuleEnabled(csharpDocument, RuleName))
+                {
+                    CheckDocumentHeader(csharpDocument);
+                }
+            }
+        }
+
+        private void CheckDocumentHeader(CsDocument csharpDocument)
+        {
+            var prop = GetSetting(csharpDocument.Settings, FileHeaderTextPropertyName) as StringProperty;
+            if (prop != null)
+            {
+                var expectedText = prop.Value;
+                var text = csharpDocument.FileHeader.HeaderText;
+                if (!string.Equals(text, expectedText, StringComparison.Ordinal))
+                {
+                    AddViolation(csharpDocument.RootElement, 1, RuleName);
+                }
+            }
+        }
+    }
+}
---
+++ src/StyleCop.KRules/UsingOutsideNamespaceRule.cs
@@ -0,0 +1,49 @@
+﻿using StyleCop.CSharp;
+
+namespace StyleCop.KRules
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class UsingOutsideNamespaceRule : SourceAnalyzer
+    {
+        private const string RuleName = "UsingDirectivesMustBePlacedOutsideNamespace";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            var csharpDocument = (CsDocument)document;
+            if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated)
+            {
+                if (IsRuleEnabled(csharpDocument, RuleName))
+                {
+                    ProcessDocumentRoot(csharpDocument.RootElement);
+                }
+            }
+        }
+
+        private void ProcessDocumentRoot(DocumentRoot documentRoot)
+        {
+            foreach (var child in documentRoot.ChildElements)
+            {
+                if (child.ElementType == ElementType.Namespace)
+                {
+                    ProcessNamespace((Namespace)child);
+                }
+            }
+        }
+
+        private void ProcessNamespace(Namespace ns)
+        {
+            foreach (var child in ns.ChildElements)
+            {
+                switch (child.ElementType)
+                {
+                    case ElementType.Namespace:
+                        ProcessNamespace((Namespace)child);
+                        break;
+                    case ElementType.UsingDirective:
+                        AddViolation(child, RuleName);
+                        break;
+                }
+            }
+        }
+    }
+}
