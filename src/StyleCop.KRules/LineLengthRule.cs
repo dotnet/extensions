b79b4553b05127d10cbfe619e1bfc9bd3e454259
Ryan Nowak <nowakra@gmail.com>
2014-06-05 13:37:31 -0700

    Adding a max-line-length rule

---
+++ src/StyleCop.KRules/LineLengthRule.cs
@@ -0,0 +1,46 @@
+﻿using StyleCop.CSharp;
+
+namespace StyleCop.KRules
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class LineLengthRule : SourceAnalyzer
+    {
+        private const string LineLengthProperty = "LineLength";
+        private const string RuleName = "LineMustNotBeTooLong";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            var csharpDocument = (CsDocument)document;
+            if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated)
+            {
+                if (IsRuleEnabled(csharpDocument, RuleName))
+                {
+                    CheckLineLength(csharpDocument);
+                }
+            }
+        }
+
+        private void CheckLineLength(CsDocument csharpDocument)
+        {
+            var lengthProperty = GetSetting(csharpDocument.Settings, LineLengthProperty) as IntProperty;
+            if (lengthProperty == null)
+            {
+                return;
+            }
+
+            using (var reader = csharpDocument.SourceCode.Read())
+            {
+                string line;
+                var lineNumber = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lineNumber++;
+                    if (line.Length > lengthProperty.Value)
+                    {
+                        AddViolation(csharpDocument.RootElement, lineNumber, RuleName);
+                    }
+                }
+            }
+        }
+    }
+}
