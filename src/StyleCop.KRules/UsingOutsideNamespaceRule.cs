ca7f65d9339d35e5dfbebfb6a890d6a1a23afb8e
Ryan Nowak <nowakra@gmail.com>
2014-06-05 13:39:10 -0700

    adding headers for other rules

--- src/StyleCop.KRules/UsingOutsideNamespaceRule.cs
+++ src/StyleCop.KRules/UsingOutsideNamespaceRule.cs
@@ -1,4 +1,7 @@
-﻿using StyleCop.CSharp;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using StyleCop.CSharp;
 
 namespace StyleCop.KRules
 {
