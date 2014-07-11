69ab5a16e34558cafd66da9339b8b8050233ac1c
Louis DeJardin <louis.dejardin@gmail.com>
2014-07-11 16:10:46 -0700

    Adding SampleApp

---
+++ samples/SampleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Framework.Logging;
+using System;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        private readonly ILogger _logger;
+
+        public Program()
+        {
+            // a DI based application would get ILoggerFactory injected instead
+            var factory = new LoggerFactory();
+
+            // getting the logger immediately using the class's name is conventional
+            _logger = factory.Create(typeof(Program).FullName);
+
+            // providers may be added to an ILoggerFactory at any time, existing ILoggers are updated
+            factory.AddNLog(new global::NLog.LogFactory());
+        }
+
+        public void Main(string[] args)
+        {
+            _logger.WriteInformation("Starting");
+
+            try
+            {
+                throw new Exception("Boom");
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteError("Unexpected error starting application", ex);
+            }
+
+            using (_logger.BeginScope("Main"))
+            {
+                Console.WriteLine("Hello World");
+
+                _logger.WriteInformation("Waiting for user input");
+                Console.ReadLine();
+            }
+
+            _logger.WriteInformation("Stopping");
+
+        }
+    }
+}
