81016a2f3301d2cd11de64690629ea6445618850
Ben Brown <t-benbro@microsoft.com>
2014-07-31 12:37:10 -0700

    Added LoggerFactoryExtensions Provides Create<T> to create type named loggers.

---
+++ src/Microsoft.Framework.Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Framework.Logging
+{
+    /// <summary>
+    /// ILoggerFactory extension methods for common scenarios.
+    /// </summary>
+    public static class LoggerFactoryExtensions
+    {
+        /// <summary>
+        /// Creates a new ILogger instance using the full name of the given type.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="factory">The factory.</param>
+        public static ILogger Create<T>(this ILoggerFactory factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException("factory");
+            }
+
+            return factory.Create(typeof(T).FullName);
+        }
+    }
+}
---
+++ test/Microsoft.Framework.Logging.Test/LoggerFactoryExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if NET45
+using Moq;
+#endif
+using Xunit;
+
+namespace Microsoft.Framework.Logging.Test
+{
+    public class LoggerFactoryExtensionsTest
+    {
+#if NET45
+        [Fact] 
+        public void LoggerFactoryCreateOfT_CallsCreateWithCorrectName()
+        {
+            // Arrange
+            var expected = typeof(TestType).FullName;
+
+            var factory = new Mock<ILoggerFactory>();
+            factory.Setup(f => f.Create(
+                It.IsAny<string>()))
+            .Returns(new Mock<ILogger>().Object);
+
+            // Act
+            factory.Object.Create<TestType>();
+
+            // Assert
+            factory.Verify(f => f.Create(expected));
+        }
+#endif
+
+        private class TestType
+        {
+            // intentionally holds nothing
+        }
+    }
+}
