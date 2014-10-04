70d65e391d36fea3928c213ab6693dae6399b5a2
David Fowler <davidfowl@gmail.com>
2014-10-04 13:17:31 -0700

    Added the TestAdapter project

---
+++ src/Microsoft.Framework.TestAdapter/TestOutcome.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Framework.TestAdapter
+{
+    public enum TestOutcome
+    {
+        None,
+        Passed,
+        Failed,
+        Skipped,
+        NotFound
+    }
+}
---
+++ src/Microsoft.Framework.TestAdapter/TestResult.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.Framework.TestAdapter
+{
+    public sealed class TestResult
+    {
+        public TestResult(Test test)
+        {
+            if (test == null)
+            {
+                throw new ArgumentNullException("test");
+            }
+
+            Test = test;
+            Messages = new Collection<string>();
+        }
+
+        public Test Test { get; private set; }
+
+        public TestOutcome Outcome { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string ErrorStackTrace { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public Collection<string> Messages { get;  private set; }
+
+        public string ComputerName { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public DateTimeOffset StartTime { get; set; }
+
+        public DateTimeOffset EndTime { get; set; }
+    }
+}
