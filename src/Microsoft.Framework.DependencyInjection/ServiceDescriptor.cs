d3dd2132e17ec884f4f9b53c8ce15b3afea527e2
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2014-11-26 10:24:11 -0800

    Debugger display attribute for ServiceDescriptor

--- src/Microsoft.Framework.DependencyInjection/ServiceDescriptor.cs
+++ src/Microsoft.Framework.DependencyInjection/ServiceDescriptor.cs
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 
 namespace Microsoft.Framework.DependencyInjection
 {
+    [DebuggerDisplay("Lifecycle = {Lifecycle}, ServiceType = {ServiceType}, ImplementationType = {ImplementationType}")]
     public class ServiceDescriptor : IServiceDescriptor
     {
         /// <summary>
