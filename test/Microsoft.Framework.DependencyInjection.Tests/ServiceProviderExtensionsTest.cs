360cacad43b4fa63806208830bec7ee044a8afc9
Yishai Galatzer <yigalatz@microsoft.com>
2015-02-22 10:24:46 -0800

    Remove nested provider managers and add GetRequiredServices.
    A followup change in MVC is coming
    
    Fixes #119
    Fixes #178

---
+++ test/Microsoft.Framework.DependencyInjection.Tests/ServiceProviderExtensionsTest.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using Xunit;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public class ServiceProviderExtensionsTest
+    {
+        [Fact]
+        public void GetServicesReturnsAllServices()
+        {
+            // Arrange
+            var serviceProvider = CreateFooServiceProvider(2);
+
+            // Act
+            var services = serviceProvider.GetRequiredServices<IFoo>();
+
+            // Assert
+            Assert.Contains(services, item => item is Foo1);
+            Assert.Contains(services, item => item is Foo2);
+            Assert.Equal(2, services.Count());
+        }
+
+        [Fact]
+        public void GetServicesReturns_A_Service()
+        {
+            // Arrange
+            var serviceProvider = CreateFooServiceProvider(1);
+
+            // Act
+            var services = serviceProvider.GetRequiredServices<IFoo>();
+
+            // Assert
+            Assert.Contains(services, item => item is Foo1);
+            Assert.Equal(1, services.Count());
+        }
+
+        [Fact]
+        public void ThrowsWhenNoServicesAreRegistered()
+        {
+            // Arrange
+            var serviceProvider = CreateFooServiceProvider(0);
+
+            // Act + Assert
+            Assert.Throws<InvalidOperationException>(() => serviceProvider.GetRequiredServices<IFoo>());
+        }
+
+        [Fact]
+        public void ThrowsWhenNoServicesAreAvailable()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddInstance<IEnumerable<IFoo>>(new List<IFoo>());
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Act + Assert
+            Assert.Throws<InvalidOperationException>(() => serviceProvider.GetRequiredServices<IFoo>());
+        }
+
+        private IServiceProvider CreateFooServiceProvider(int count)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            if (count > 0)
+            {
+                serviceCollection.AddTransient<IFoo, Foo1>();
+            }
+
+            if (count > 1)
+            {
+                serviceCollection.AddTransient<IFoo, Foo2>();
+            }
+
+            return serviceCollection.BuildServiceProvider();
+        }
+
+        public interface IFoo { }
+
+        public class Foo1 : IFoo { }
+
+        public class Foo2 : IFoo { }
+    }
+}
