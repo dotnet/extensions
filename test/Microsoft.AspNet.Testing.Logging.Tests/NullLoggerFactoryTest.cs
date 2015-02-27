7165b2252a58349908bf16c9eb8201d9b95b8f99
Brennan <brecon@microsoft.com>
2015-02-26 18:10:11 -0800

    Adding NullLogger and NullLoggerFactory

---
+++ test/Microsoft.AspNet.Testing.Logging.Tests/NullLoggerFactoryTest.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Framework.Logging;
+using Xunit;
+
+namespace Microsoft.AspNet.Testing.Logging
+{
+    public class NullLoggerFactoryTest
+    {
+        [Fact]
+        public void MinimumLevelIsVerbose()
+        {
+            // Act & Assert
+            Assert.True(LogLevel.Verbose == NullLoggerFactory.Instance.MinimumLevel);
+        }
+
+        [Fact]
+        public void Create_GivesSameLogger()
+        {
+            // Arrange
+            var factory = NullLoggerFactory.Instance;
+
+            // Act
+            var logger1 = factory.Create("Logger1");
+            var logger2 = factory.Create("Logger2");
+
+            // Assert
+            Assert.Same(logger1, logger2);
+        }
+    }
+}
---
+++ test/Microsoft.AspNet.Testing.Logging.Tests/NullLoggerTest.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Framework.Logging;
+using Xunit;
+
+namespace Microsoft.AspNet.Testing.Logging
+{
+    public class NullLoggerTest
+    {
+        [Fact]
+        public void BeginScope_CanDispose()
+        {
+            // Arrange
+            var logger = NullLogger.Instance;
+
+            // Act & Assert
+            using (logger.BeginScope(null))
+            {
+            }
+        }
+
+        [Fact]
+        public void IsEnabled_AlwaysFalse()
+        {
+            // Arrange
+            var logger = NullLogger.Instance;
+
+            // Act & Assert
+            Assert.False(logger.IsEnabled(LogLevel.Debug));
+            Assert.False(logger.IsEnabled(LogLevel.Verbose));
+            Assert.False(logger.IsEnabled(LogLevel.Information));
+            Assert.False(logger.IsEnabled(LogLevel.Warning));
+            Assert.False(logger.IsEnabled(LogLevel.Error));
+            Assert.False(logger.IsEnabled(LogLevel.Critical));
+        }
+
+        [Fact]
+        public void Write_Does_Nothing()
+        {
+            // Arrange
+            var logger = NullLogger.Instance;
+            bool isCalled = false;
+
+            // Act
+            logger.Write(LogLevel.Verbose, eventId: 0, state: null, exception: null, formatter: (ex, message) => { isCalled = true; return string.Empty; });
+
+            // Assert
+            Assert.False(isCalled);
+        }
+    }
+}
