46d82417c04e6ffee7c07d8a508f43ed216bd162
David Fowler <davidfowl@gmail.com>
2015-03-05 02:57:10 -0800

    React to Logging refactoring - Added WriteContext - Depend on Logging.Interfaces only

---
+++ src/Microsoft.AspNet.Testing.Logging/WriteContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Framework.Logging;
+
+namespace Microsoft.AspNet.Testing.Logging
+{
+    public class WriteContext
+    {
+        public LogLevel LogLevel { get; set; }
+
+        public int EventId { get; set; }
+
+        public object State { get; set; }
+
+        public Exception Exception { get; set; }
+
+        public Func<object, Exception, string> Formatter { get; set; }
+
+        public object Scope { get; set; }
+
+        public string LoggerName { get; set; }
+    }
+}
