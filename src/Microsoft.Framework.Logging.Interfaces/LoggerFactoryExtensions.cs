ycrumeyrolle/Logging

Beautifying generic types

src/Microsoft.Framework.Logging/LoggerFactoryExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Text;
 
 namespace Microsoft.Framework.Logging
 {
@@ -22,7 +23,8 @@ namespace Microsoft.Framework.Logging
                 throw new ArgumentNullException("factory");
             }
 
-            return factory.Create(typeof(T).FullName);
+            return factory.Create(TypeNameHelper.GetTypeDisplayFullName(typeof(T)));
         }
     }
+        
 }

test/Microsoft.Framework.Logging.Test/LoggerFactoryExtensionsTest.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Framework.Logging.Test
     public class LoggerFactoryExtensionsTest
     {
 #if ASPNET50
-        [Fact] 
+        [Fact]
         public void LoggerFactoryCreateOfT_CallsCreateWithCorrectName()
         {
             // Arrange
@@ -28,11 +28,56 @@ namespace Microsoft.Framework.Logging.Test
             // Assert
             factory.Verify(f => f.Create(expected));
         }
-#endif
 
-        private class TestType
+        [Fact]
+        public void LoggerFactoryCreateOfT_SingleGeneric_CallsCreateWithCorrectName()
         {
-            // intentionally holds nothing
+            // Arrange
+            var factory = new Mock<ILoggerFactory>();
+            factory.Setup(f => f.Create(It.Is<string>(
+                x => x.Equals("Microsoft.Framework.Logging.Test.GenericClass<Microsoft.Framework.Logging.Test.TestType>"))))
+            .Returns(new Mock<ILogger>().Object);
+
+            var logger = factory.Object.Create<GenericClass<TestType>>();
+
+            // Assert
+            Assert.NotNull(logger);
         }
+
+        [Fact]
+        public void LoggerFactoryCreateOfT_TwoGenerics_CallsCreateWithCorrectName()
+        {
+            // Arrange
+            var factory = new Mock<ILoggerFactory>();
+            factory.Setup(f => f.Create(It.Is<string>(
+                x => x.Equals("Microsoft.Framework.Logging.Test.GenericClass<Microsoft.Framework.Logging.Test.TestType, Microsoft.Framework.Logging.Test.SecondTestType>"))))
+            .Returns(new Mock<ILogger>().Object);
+
+            var logger = factory.Object.Create<GenericClass<TestType,SecondTestType>>();
+
+            // Assert
+            Assert.NotNull(logger);
+        }
+#endif
+    }
+
+    internal class TestType
+    {
+        // intentionally holds nothing
+    }
+
+    internal class SecondTestType
+    {
+        // intentionally holds nothing
+    }
+
+    internal class GenericClass<X, Y> where X : class where Y : class
+    {
+        // intentionally holds nothing
+    }
+
+    internal class GenericClass<X> where X : class
+    {
+        // intentionally holds nothing
     }
 }

dotnet/extensions

src/Microsoft.Framework.Logging.Interfaces/LoggerFactoryExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Framework.Logging.Internal;
 
 namespace Microsoft.Framework.Logging
 {
@@ -22,7 +23,8 @@ namespace Microsoft.Framework.Logging
                 throw new ArgumentNullException("factory");
             }
 
-            return factory.CreateLogger(typeof(T).FullName);
+            return factory.CreateLogger(TypeNameHelper.GetTypeDisplayFullName(typeof(T)));
         }
     }
+        
 }

test/Microsoft.Framework.Logging.Test/LoggerFactoryExtensionsTest.cs
@@ -28,11 +28,56 @@ namespace Microsoft.Framework.Logging.Test
             // Assert
             factory.Verify(f => f.CreateLogger(expected));
         }
-#endif
 
-        private class TestType
+        [Fact]
+        public void LoggerFactoryCreateOfT_SingleGeneric_CallsCreateWithCorrectName()
         {
-            // intentionally holds nothing
+            // Arrange
+            var factory = new Mock<ILoggerFactory>();
+            factory.Setup(f => f.CreateLogger(It.Is<string>(
+                x => x.Equals("Microsoft.Framework.Logging.Test.GenericClass<Microsoft.Framework.Logging.Test.TestType>"))))
+            .Returns(new Mock<ILogger>().Object);
+
+            var logger = factory.Object.CreateLogger<GenericClass<TestType>>();
+
+            // Assert
+            Assert.NotNull(logger);
         }
+
+        [Fact]
+        public void LoggerFactoryCreateOfT_TwoGenerics_CallsCreateWithCorrectName()
+        {
+            // Arrange
+            var factory = new Mock<ILoggerFactory>();
+            factory.Setup(f => f.CreateLogger(It.Is<string>(
+                x => x.Equals("Microsoft.Framework.Logging.Test.GenericClass<Microsoft.Framework.Logging.Test.TestType, Microsoft.Framework.Logging.Test.SecondTestType>"))))
+            .Returns(new Mock<ILogger>().Object);
+
+            var logger = factory.Object.CreateLogger<GenericClass<TestType,SecondTestType>>();
+
+            // Assert
+            Assert.NotNull(logger);
+        }
+#endif
+    }
+
+    internal class TestType
+    {
+        // intentionally holds nothing
+    }
+
+    internal class SecondTestType
+    {
+        // intentionally holds nothing
+    }
+
+    internal class GenericClass<X, Y> where X : class where Y : class
+    {
+        // intentionally holds nothing
+    }
+
+    internal class GenericClass<X> where X : class
+    {
+        // intentionally holds nothing
     }
 }