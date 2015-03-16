e8835314887bc71fec72945a6e4610e310e63b4c
iKewell <kewell.tsao@qq.com>
2015-03-16 11:10:31 +0800

    Fix "Check ex.InnerException is null"

--- src/Microsoft.Framework.DependencyInjection/ServiceLookup/Service.cs
+++ src/Microsoft.Framework.DependencyInjection/ServiceLookup/Service.cs
@@ -109,7 +109,11 @@ namespace Microsoft.Framework.DependencyInjection.ServiceLookup
                 }
                 catch (Exception ex)
                 {
-                    ExceptionDispatchInfo.Capture(ex.InnerException != null ? ex.InnerException : ex).Throw();
+                	if (ex.InnerException == null) 
+					{
+					    throw;
+					}
+					ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
                     // The above line will always throw, but the compiler requires we throw explicitly.
                     throw;
                 }
