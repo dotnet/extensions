ee72e744ffa8294b7e7caa9a99052da363e5ddea
Praburaj <praburaj.t@microsoft.com>
2015-03-16 17:14:55 -0700

    Remove RestrictedFileNames checks
    Fixes: https://github.com/aspnet/FileSystem/issues/37

--- src/Microsoft.AspNet.FileProviders/PhysicalFileProvider.cs
+++ src/Microsoft.AspNet.FileProviders/PhysicalFileProvider.cs
@@ -14,34 +14,6 @@ namespace Microsoft.AspNet.FileProviders
     /// </summary>
     public class PhysicalFileProvider : IFileProvider
     {
-        // These are restricted file names on Windows, regardless of extension.
-        private static readonly Dictionary<string, string> RestrictedFileNames = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-        {
-            { "con", string.Empty },
-            { "prn", string.Empty },
-            { "aux", string.Empty },
-            { "nul", string.Empty },
-            { "com1", string.Empty },
-            { "com2", string.Empty },
-            { "com3", string.Empty },
-            { "com4", string.Empty },
-            { "com5", string.Empty },
-            { "com6", string.Empty },
-            { "com7", string.Empty },
-            { "com8", string.Empty },
-            { "com9", string.Empty },
-            { "lpt1", string.Empty },
-            { "lpt2", string.Empty },
-            { "lpt3", string.Empty },
-            { "lpt4", string.Empty },
-            { "lpt5", string.Empty },
-            { "lpt6", string.Empty },
-            { "lpt7", string.Empty },
-            { "lpt8", string.Empty },
-            { "lpt9", string.Empty },
-            { "clock$", string.Empty },
-        };
-
         private readonly PhysicalFilesWatcher _filesWatcher;
 
         /// <summary>
@@ -122,7 +94,7 @@ namespace Microsoft.AspNet.FileProviders
             }
 
             var fullPath = GetFullPath(subpath);
-            if (fullPath == null || IsRestricted(subpath))
+            if (fullPath == null)
             {
                 return new NotFoundFileInfo(subpath);
             }
@@ -203,12 +175,6 @@ namespace Microsoft.AspNet.FileProviders
             {
             }
             return new NotFoundDirectoryContents();
-        }
-
-        private bool IsRestricted(string name)
-        {
-            string fileName = Path.GetFileNameWithoutExtension(name);
-            return RestrictedFileNames.ContainsKey(fileName);
         }
 
         public IExpirationTrigger Watch(string filter)
