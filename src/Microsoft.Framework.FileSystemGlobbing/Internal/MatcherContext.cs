4b0ba34b8bedb05f46b060c953eedb45604c0a5c
Troy Dai <troy.dai@outlook.com>
2015-04-06 10:15:44 -0700

    Remove unused using statement

--- src/Microsoft.Framework.FileSystemGlobbing/Internal/MatcherContext.cs
+++ src/Microsoft.Framework.FileSystemGlobbing/Internal/MatcherContext.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Microsoft.Framework.FileSystemGlobbing.Abstractions;
 using Microsoft.Framework.FileSystemGlobbing.Internal.PathSegments;
