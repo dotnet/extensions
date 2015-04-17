4d6fac459797031a101736922e62ca751e750d21
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2015-04-17 15:24:41 -0700

    Moved CommonTestEncoder from HttpAbstractions to Testing

---
+++ test/Microsoft.Framework.WebEncoders.Testing.Tests/CommonTestEncoderTest.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Framework.WebEncoders.Testing
+{
+    public class CommonTestEncoderTest
+    {
+        [Fact]
+        public void Encode_returnsEncodedValue()
+        {
+            // Arrange
+            var encoder = new CommonTestEncoder();
+            var input = "TestValue";
+
+            // Act
+            var htmlEncodedValue = encoder.HtmlEncode(input);
+            var javaScriptStringEncodedValue = encoder.JavaScriptStringEncode(input);
+            var urlEncodedValue = encoder.UrlEncode(input);
+
+            // Assert
+            Assert.Equal($"HtmlEncode[[{input}]]", htmlEncodedValue);
+            Assert.Equal($"JavaScriptStringEncode[[{input}]]", javaScriptStringEncodedValue);
+            Assert.Equal($"UrlEncode[[{input}]]", urlEncodedValue);
+        }
+    }
+}
---
+++ test/Microsoft.Framework.WebEncoders.Testing.Tests/NullTestEncoderTest.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Framework.WebEncoders.Testing
+{
+    public class NullTestEncoderTest
+    {
+        [Fact]
+        public void Encode_returnsSameValue()
+        {
+            // Arrange
+            var encoder = new NullTestEncoder();
+            var input = "TestValue";
+
+            // Act
+            var htmlEncodedValue = encoder.HtmlEncode(input);
+            var javaScriptStringEncodedValue = encoder.JavaScriptStringEncode(input);
+            var urlEncodedValue = encoder.UrlEncode(input);
+
+            // Assert
+            Assert.Equal(input, htmlEncodedValue);
+            Assert.Equal(input, javaScriptStringEncodedValue);
+            Assert.Equal(input, urlEncodedValue);
+        }
+    }
+}
