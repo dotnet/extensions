81656946410c03916a8d5049b0433bdfe1f0ebd3
Brennan <brecon@microsoft.com>
2015-04-29 23:02:30 -0700

    Test in wrong namespace

--- test/Microsoft.Framework.DependencyInjection.Tests/ActivatorUtilitiesTests.cs
+++ test/Microsoft.Framework.DependencyInjection.Tests/ActivatorUtilitiesTests.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using Microsoft.Framework.DependencyInjection.Tests.Fakes;
 using Xunit;
 
-namespace Microsoft.Framework.DependencyInjection.Abstractions.Tests
+namespace Microsoft.Framework.DependencyInjection.Interfaces.Tests
 {
     public class ActivatorUtilitiesTests
     {
