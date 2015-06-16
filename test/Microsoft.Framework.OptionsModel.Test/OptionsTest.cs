4afe90d3dd5525204fbc97f6ec0a2ca9b529fa49
Ryan Meyer <ryan1234@gmail.com>
2015-06-16 13:48:48 -0700

    using statement duplicated
    Trying to debug some code, looking for inspiration and noticed a duplicated using.

--- test/Microsoft.Framework.OptionsModel.Test/OptionsTest.cs
+++ test/Microsoft.Framework.OptionsModel.Test/OptionsTest.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Framework.Configuration;
-using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Xunit;
 
