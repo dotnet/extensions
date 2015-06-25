8e2f4b2c403dbb15497f6a291ea7935d7a2b7a51
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2015-06-24 17:08:12 -0700

    Added ObjectToDictionary to PropertyHelper

--- src/Microsoft.Framework.PropertyHelper.Sources/PropertyHelper.cs
+++ src/Microsoft.Framework.PropertyHelper.Sources/PropertyHelper.cs
@@ -304,6 +304,38 @@ namespace Microsoft.Framework.Internal
             return (Action<object, object>)callPropertySetterDelegate;
         }
 
+        /// <summary>
+        /// Given an object, adds each instance property with a public get method as a key and its
+        /// associated value to a dictionary.
+        ///
+        /// If the object is already an <see cref="IDictionary{string, object}"/> instance, then a copy
+        /// is returned.
+        /// </summary>
+        /// <remarks>
+        /// The implementation of PropertyHelper will cache the property accessors per-type. This is
+        /// faster when the the same type is used multiple times with ObjectToDictionary.
+        /// </remarks>
+        public static IDictionary<string, object> ObjectToDictionary(object value)
+        {
+            var dictionary = value as IDictionary<string, object>;
+            if (dictionary != null)
+            {
+                return new Dictionary<string, object>(dictionary, StringComparer.OrdinalIgnoreCase);
+            }
+
+            dictionary = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+            if (value != null)
+            {
+                foreach (var helper in GetProperties(value))
+                {
+                    dictionary[helper.Name] = helper.GetValue(value);
+                }
+            }
+
+            return dictionary;
+        }
+
         private static PropertyHelper CreateInstance(PropertyInfo property)
         {
             return new PropertyHelper(property);
