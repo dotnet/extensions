aspnet/Caching

AddRedisCache extension method to IServiceCollection in order to enable Redis

src/Microsoft.Framework.Caching.Redis/CachingServiceExtensions.cs
@@ -1,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Framework.Caching.Distributed;
+using Microsoft.Framework.Caching.Redis;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class CachingServicesExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection collection)
+        {
+            collection.AddOptions();
+            collection.TryAdd(ServiceDescriptor.Transient<IDistributedCache, RedisCache>());
+            return collection;
+        }
+    }
+}

dotnet/extensions

src/Microsoft.Framework.Caching.Redis/CachingServiceExtensions.cs
@@ -1,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Framework.Caching.Distributed;
+using Microsoft.Framework.Caching.Redis;
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class CachingServicesExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection collection)
+        {
+            collection.AddOptions();
+            collection.TryAdd(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
+            return collection;
+        }
+    }
+}
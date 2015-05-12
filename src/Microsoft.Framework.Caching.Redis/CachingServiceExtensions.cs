a8790019715bca25555238ef4a792960d3ca1e4c
Unai Zorrilla <unai@plainconcepts.com>
2015-05-12 08:13:57 -0700

    AddRedisCache extension method to IServiceCollection in order to enable Redis

---
+++ src/Microsoft.Framework.Caching.Redis/CachingServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Framework.Caching.Distributed;
+using Microsoft.Framework.Caching.Redis;
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class CachingServicesExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection collection)
+        {
+            collection.AddOptions();
+            collection.TryAdd(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
+            return collection;
+        }
+    }
+}
---
+++ test/Microsoft.Framework.Caching.Redis.Tests/CacheServiceExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Framework.Caching.Distributed;
+using Microsoft.Framework.DependencyInjection;
+using Xunit;
+namespace Microsoft.Framework.Caching.Redis
+{
+    public class CacheServiceExtensionsTests
+    {
+        
+
+        [Fact]
+        public void AddRedisCache_RegistersDistributedCacheAsTransient()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddRedisCache();
+
+            // Assert
+            var distributedCache = services.FirstOrDefault(desc => desc.ServiceType == typeof(IDistributedCache));
+
+            Assert.NotNull(distributedCache);
+            Assert.Equal(ServiceLifetime.Transient, distributedCache.Lifetime);
+        }
+
+        [Fact]
+        public void AddRedisCache_DoesNotReplaceUserRegisteredServices()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddScoped<IDistributedCache, TestDistributedCache>();
+
+            // Act
+            services.AddCaching();
+
+            // Assert
+            var serviceProvider = services.BuildServiceProvider();
+
+            var distributedCache = services.FirstOrDefault(desc => desc.ServiceType == typeof(IDistributedCache));
+
+            Assert.NotNull(distributedCache);
+            Assert.Equal(ServiceLifetime.Scoped, distributedCache.Lifetime);
+            Assert.IsType<TestDistributedCache>(serviceProvider.GetRequiredService<IDistributedCache>());
+        }
+
+
+        private class TestDistributedCache : IDistributedCache
+        {
+            public void Connect()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Refresh(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Remove(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Stream Set(string key, object state, Action<ICacheContext> create)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetValue(string key, out Stream value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
