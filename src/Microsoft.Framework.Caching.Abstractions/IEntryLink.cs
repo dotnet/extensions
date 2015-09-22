aspnet/Caching

Replacing IExpirationTrigger with IChangeToken

src/Microsoft.Framework.Caching.Abstractions/IEntryLink.cs
@@ -3,12 +3,13 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.Framework.Primitives;
 
 namespace Microsoft.Framework.Caching.Memory
 {
     /// <summary>
-    /// Used to flow expiration information from one entry to another. Triggers and minimum absolute expiration will
-    /// be copied from the dependent entry to the parent entry. The parent entry will not expire if the
+    /// Used to flow expiration information from one entry to another. <see cref="IChangeToken"/>s and minimum absolute 
+    /// expiration will be copied from the dependent entry to the parent entry. The parent entry will not expire if the
     /// dependent entry is removed manually, removed due to memory pressure, or expires due to sliding expiration.
     /// </summary>
     public interface IEntryLink : IDisposable
@@ -19,15 +20,15 @@ namespace Microsoft.Framework.Caching.Memory
         DateTimeOffset? AbsoluteExpiration { get; }
 
         /// <summary>
-        /// Gets all the triggers from the dependent entries.
+        /// Gets all the <see cref="IChangeToken"/>s from the dependent entries.
         /// </summary>
-        IEnumerable<IExpirationTrigger> Triggers { get; }
+        IEnumerable<IChangeToken> ChangeTokens { get; }
 
         /// <summary>
-        /// Adds triggers from a dependent entries.
+        /// Adds <see cref="IChangeToken"/>s from a dependent entries.
         /// </summary>
-        /// <param name="triggers"></param>
-        void AddExpirationTriggers(IList<IExpirationTrigger> triggers);
+        /// <param name="changeTokens"><see cref="IChangeToken"/>s from dependent entries.</param>
+        void AddChangeTokens(IList<IChangeToken> changeTokens);
 
         /// <summary>
         /// Sets the absolute expiration for from a dependent entry. The minimum value across all dependent entries

test/Microsoft.Framework.Caching.Memory.Tests/TokenExpirationTests.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace Microsoft.Framework.Caching.Memory
 {
-    public class TriggeredExpirationTests
+    public class TokenExpirationTests
     {
         private IMemoryCache CreateCache()
         {
@@ -26,46 +26,46 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void SetWithTriggerRegistersForNotificaiton()
+        public void SetWithTokenRegistersForNotificaiton()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
-            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            var token = new TestChangeToken() { ActiveChangeCallbacks = true };
+            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationToken(token));
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.True(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.NotNull(trigger.Registration);
-            Assert.NotNull(trigger.Registration.RegisteredCallback);
-            Assert.NotNull(trigger.Registration.RegisteredState);
-            Assert.False(trigger.Registration.Disposed);
+            Assert.True(token.IsExpiredWasCalled);
+            Assert.True(token.ActiveChangeCallbacksWasCalled);
+            Assert.NotNull(token.Registration);
+            Assert.NotNull(token.Registration.RegisteredCallback);
+            Assert.NotNull(token.Registration.RegisteredState);
+            Assert.False(token.Registration.Disposed);
         }
 
         [Fact]
-        public void SetWithLazyTriggerDoesntRegisterForNotification()
+        public void SetWithLazyTokenDoesntRegisterForNotification()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
-            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            var token = new TestChangeToken() { ActiveChangeCallbacks = false };
+            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationToken(token));
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.True(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.Null(trigger.Registration);
+            Assert.True(token.IsExpiredWasCalled);
+            Assert.True(token.ActiveChangeCallbacksWasCalled);
+            Assert.Null(token.Registration);
         }
 
         [Fact]
-        public void FireTriggerRemovesItem()
+        public void FireTokenRemovesItem()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
+            var token = new TestChangeToken() { ActiveChangeCallbacks = true };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(token)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
                 {
                     // TODO: Verify params
@@ -73,7 +73,7 @@ namespace Microsoft.Framework.Caching.Memory
                     localCallbackInvoked.Set();
                 }, state: callbackInvoked));
 
-            trigger.Fire();
+            token.Fire();
 
             var found = cache.TryGetValue(key, out value);
             Assert.False(found);
@@ -82,15 +82,15 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void ExpiredLazyTriggerRemovesItemOnNextAccess()
+        public void ExpiredLazyTokenRemovesItemOnNextAccess()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
+            var token = new TestChangeToken() { ActiveChangeCallbacks = false };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(token)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
                 {
                     // TODO: Verify params
@@ -101,7 +101,7 @@ namespace Microsoft.Framework.Caching.Memory
             var found = cache.TryGetValue(key, out value);
             Assert.True(found);
 
-            trigger.IsExpired = true;
+            token.HasChanged = true;
 
             found = cache.TryGetValue(key, out value);
             Assert.False(found);
@@ -110,16 +110,16 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void ExpiredLazyTriggerRemovesItemInBackground()
+        public void ExpiredLazyTokenRemovesItemInBackground()
         {
             var clock = new TestClock();
             var cache = CreateCache(clock);
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
+            var token = new TestChangeToken() { ActiveChangeCallbacks = false };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(token)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -130,7 +130,7 @@ namespace Microsoft.Framework.Caching.Memory
             Assert.True(found);
 
             clock.Add(TimeSpan.FromMinutes(2));
-            trigger.IsExpired = true;
+            token.HasChanged = true;
             var ignored = cache.Get("otherKey"); // Background expiration checks are triggered by misc cache activity.
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
 
@@ -139,15 +139,15 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void RemoveItemDisposesTriggerRegistration()
+        public void RemoveItemDisposesTokenRegistration()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
+            var token = new TestChangeToken() { ActiveChangeCallbacks = true };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(token)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -156,21 +156,21 @@ namespace Microsoft.Framework.Caching.Memory
             }, state: callbackInvoked));
             cache.Remove(key);
 
-            Assert.NotNull(trigger.Registration);
-            Assert.True(trigger.Registration.Disposed);
+            Assert.NotNull(token.Registration);
+            Assert.True(token.Registration.Disposed);
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
         }
 
         [Fact]
-        public void AddExpiredTriggerPreventsCaching()
+        public void AddExpiredTokenPreventsCaching()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { IsExpired = true };
+            var token = new TestChangeToken() { HasChanged = true };
             var result = cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(token)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -179,9 +179,9 @@ namespace Microsoft.Framework.Caching.Memory
             }, state: callbackInvoked));
             Assert.Same(value, result); // The created item should be returned, but not cached.
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.False(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.Null(trigger.Registration);
+            Assert.True(token.IsExpiredWasCalled);
+            Assert.False(token.ActiveChangeCallbacksWasCalled);
+            Assert.Null(token.Registration);
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
 
             result = cache.Get(key);

dotnet/extensions

src/Microsoft.Framework.Caching.Abstractions/IEntryLink.cs
@@ -3,12 +3,13 @@
 
 using System;
 using System.Collections.Generic;
+using Microsoft.Framework.Primitives;
 
 namespace Microsoft.Framework.Caching.Memory
 {
     /// <summary>
-    /// Used to flow expiration information from one entry to another. Triggers and minimum absolute expiration will
-    /// be copied from the dependent entry to the parent entry. The parent entry will not expire if the
+    /// Used to flow expiration information from one entry to another. <see cref="IChangeToken"/> instances and minimum absolute
+    /// expiration will be copied from the dependent entry to the parent entry. The parent entry will not expire if the
     /// dependent entry is removed manually, removed due to memory pressure, or expires due to sliding expiration.
     /// </summary>
     public interface IEntryLink : IDisposable
@@ -19,15 +20,15 @@ namespace Microsoft.Framework.Caching.Memory
         DateTimeOffset? AbsoluteExpiration { get; }
 
         /// <summary>
-        /// Gets all the triggers from the dependent entries.
+        /// Gets all the <see cref="IChangeToken"/> instances from the dependent entries.
         /// </summary>
-        IEnumerable<IExpirationTrigger> Triggers { get; }
+        IEnumerable<IChangeToken> ExpirationTokens { get; }
 
         /// <summary>
-        /// Adds triggers from a dependent entries.
+        /// Adds <see cref="IChangeToken"/> instances from a dependent entries.
         /// </summary>
-        /// <param name="triggers"></param>
-        void AddExpirationTriggers(IList<IExpirationTrigger> triggers);
+        /// <param name="expirationTokens"><see cref="IChangeToken"/> instances from dependent entries.</param>
+        void AddExpirationTokens(IList<IChangeToken> expirationTokens);
 
         /// <summary>
         /// Sets the absolute expiration for from a dependent entry. The minimum value across all dependent entries

test/Microsoft.Framework.Caching.Memory.Tests/TokenExpirationTests.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace Microsoft.Framework.Caching.Memory
 {
-    public class TriggeredExpirationTests
+    public class TokenExpirationTests
     {
         private IMemoryCache CreateCache()
         {
@@ -26,46 +26,46 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void SetWithTriggerRegistersForNotificaiton()
+        public void SetWithTokenRegistersForNotificaiton()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
-            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = true };
+            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationToken(expirationToken));
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.True(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.NotNull(trigger.Registration);
-            Assert.NotNull(trigger.Registration.RegisteredCallback);
-            Assert.NotNull(trigger.Registration.RegisteredState);
-            Assert.False(trigger.Registration.Disposed);
+            Assert.True(expirationToken.HasChangedWasCalled);
+            Assert.True(expirationToken.ActiveChangeCallbacksWasCalled);
+            Assert.NotNull(expirationToken.Registration);
+            Assert.NotNull(expirationToken.Registration.RegisteredCallback);
+            Assert.NotNull(expirationToken.Registration.RegisteredState);
+            Assert.False(expirationToken.Registration.Disposed);
         }
 
         [Fact]
-        public void SetWithLazyTriggerDoesntRegisterForNotification()
+        public void SetWithLazyTokenDoesntRegisterForNotification()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
-            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = false };
+            cache.Set(key, value, new MemoryCacheEntryOptions().AddExpirationToken(expirationToken));
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.True(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.Null(trigger.Registration);
+            Assert.True(expirationToken.HasChangedWasCalled);
+            Assert.True(expirationToken.ActiveChangeCallbacksWasCalled);
+            Assert.Null(expirationToken.Registration);
         }
 
         [Fact]
-        public void FireTriggerRemovesItem()
+        public void FireTokenRemovesItem()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = true };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(expirationToken)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
                 {
                     // TODO: Verify params
@@ -73,7 +73,7 @@ namespace Microsoft.Framework.Caching.Memory
                     localCallbackInvoked.Set();
                 }, state: callbackInvoked));
 
-            trigger.Fire();
+            expirationToken.Fire();
 
             var found = cache.TryGetValue(key, out value);
             Assert.False(found);
@@ -82,15 +82,15 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void ExpiredLazyTriggerRemovesItemOnNextAccess()
+        public void ExpiredLazyTokenRemovesItemOnNextAccess()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = false };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(expirationToken)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
                 {
                     // TODO: Verify params
@@ -101,7 +101,7 @@ namespace Microsoft.Framework.Caching.Memory
             var found = cache.TryGetValue(key, out value);
             Assert.True(found);
 
-            trigger.IsExpired = true;
+            expirationToken.HasChanged = true;
 
             found = cache.TryGetValue(key, out value);
             Assert.False(found);
@@ -110,16 +110,16 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void ExpiredLazyTriggerRemovesItemInBackground()
+        public void ExpiredLazyTokenRemovesItemInBackground()
         {
             var clock = new TestClock();
             var cache = CreateCache(clock);
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = false };
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = false };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(expirationToken)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -130,7 +130,7 @@ namespace Microsoft.Framework.Caching.Memory
             Assert.True(found);
 
             clock.Add(TimeSpan.FromMinutes(2));
-            trigger.IsExpired = true;
+            expirationToken.HasChanged = true;
             var ignored = cache.Get("otherKey"); // Background expiration checks are triggered by misc cache activity.
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
 
@@ -139,15 +139,15 @@ namespace Microsoft.Framework.Caching.Memory
         }
 
         [Fact]
-        public void RemoveItemDisposesTriggerRegistration()
+        public void RemoveItemDisposesTokenRegistration()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { ActiveExpirationCallbacks = true };
+            var expirationToken = new TestExpirationToken() { ActiveChangeCallbacks = true };
             cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(expirationToken)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -156,21 +156,21 @@ namespace Microsoft.Framework.Caching.Memory
             }, state: callbackInvoked));
             cache.Remove(key);
 
-            Assert.NotNull(trigger.Registration);
-            Assert.True(trigger.Registration.Disposed);
+            Assert.NotNull(expirationToken.Registration);
+            Assert.True(expirationToken.Registration.Disposed);
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
         }
 
         [Fact]
-        public void AddExpiredTriggerPreventsCaching()
+        public void AddExpiredTokenPreventsCaching()
         {
             var cache = CreateCache();
             string key = "myKey";
             var value = new object();
             var callbackInvoked = new ManualResetEvent(false);
-            var trigger = new TestTrigger() { IsExpired = true };
+            var expirationToken = new TestExpirationToken() { HasChanged = true };
             var result = cache.Set(key, value, new MemoryCacheEntryOptions()
-                .AddExpirationTrigger(trigger)
+                .AddExpirationToken(expirationToken)
                 .RegisterPostEvictionCallback((subkey, subValue, reason, state) =>
             {
                 // TODO: Verify params
@@ -179,9 +179,9 @@ namespace Microsoft.Framework.Caching.Memory
             }, state: callbackInvoked));
             Assert.Same(value, result); // The created item should be returned, but not cached.
 
-            Assert.True(trigger.IsExpiredWasCalled);
-            Assert.False(trigger.ActiveExpirationCallbacksWasCalled);
-            Assert.Null(trigger.Registration);
+            Assert.True(expirationToken.HasChangedWasCalled);
+            Assert.False(expirationToken.ActiveChangeCallbacksWasCalled);
+            Assert.Null(expirationToken.Registration);
             Assert.True(callbackInvoked.WaitOne(TimeSpan.FromSeconds(30)), "Callback");
 
             result = cache.Get(key);