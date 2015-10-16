74ed4755a9672d24c1adce05dd3a6ce1183d7034
Pranav K <prkrishn@hotmail.com>
2015-10-16 11:28:37 -0700

    Revert "Removing Autofac tests"
    This reverts commit a514aabded31e93a9b9a000f7677dafce245d873.

---
+++ test/Microsoft.Extensions.DependencyInjection.Tests/AutofacContainerTests.cs
@@ -0,0 +1,23 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Tests.Fakes;
+
+namespace Microsoft.Extensions.DependencyInjection.Tests
+{
+    public class AutofacContainerTests : ScopingContainerTestBase
+    {
+        protected override IServiceProvider CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Populate(TestServices.DefaultServices());
+
+            var container = builder.Build();
+            return container.Resolve<IServiceProvider>();
+        }
+    }
+}
