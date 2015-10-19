718ac11d11dc8b3fd0a96c4d866d164f073ef23b
Kiran Challa <kichalla@microsoft.com>
2015-10-19 14:04:30 -0700

    Moved Microsoft.Extensions.Logging.Testing and its test project from Testing repo

---
+++ src/Microsoft.Extensions.Logging.Testing/NullLoggerFactory.cs
@@ -0,0 +1,27 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Extensions.Logging.Testing
+{
+    public class NullLoggerFactory : ILoggerFactory
+    {
+        public static readonly NullLoggerFactory Instance = new NullLoggerFactory();
+
+        public LogLevel MinimumLevel { get; set; } = LogLevel.Verbose;
+
+        public ILogger CreateLogger(string name)
+        {
+            return NullLogger.Instance;
+        }
+
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ test/Microsoft.Extensions.Logging.Testing.Tests/NullLoggerFactoryTest.cs
@@ -0,0 +1,32 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Extensions.Logging.Testing
+{
+    public class NullLoggerFactoryTest
+    {
+        [Fact]
+        public void MinimumLevelIsVerbose()
+        {
+            // Act & Assert
+            Assert.True(LogLevel.Verbose == NullLoggerFactory.Instance.MinimumLevel);
+        }
+
+        [Fact]
+        public void Create_GivesSameLogger()
+        {
+            // Arrange
+            var factory = NullLoggerFactory.Instance;
+
+            // Act
+            var logger1 = factory.CreateLogger("Logger1");
+            var logger2 = factory.CreateLogger("Logger2");
+
+            // Assert
+            Assert.Same(logger1, logger2);
+        }
+    }
+}
