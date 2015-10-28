99ac8681ac551595429e9cb2598f67ddf29fd5a8
Ryan Nowak <nowakra@gmail.com>
2015-10-28 09:10:04 -0700

    Add test loggers for ILogger<T>

---
+++ src/Microsoft.Extensions.Logging.Testing/NullLoggerOfT.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Extensions.Logging.Testing
+{
+    public class NullLogger<T> : ILogger<T>
+    {
+        public static readonly NullLogger<T> Instance = new NullLogger<T>();
+
+        public IDisposable BeginScopeImpl(object state)
+        {
+            return NullDisposable.Instance;
+        }
+
+        public void Log(
+            LogLevel logLevel,
+            int eventId,
+            object state,
+            Exception exception,
+            Func<object, Exception, string> formatter)
+        {
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        private class NullDisposable : IDisposable
+        {
+            public static readonly NullDisposable Instance = new NullDisposable();
+
+            public void Dispose()
+            {
+                // intentionally does nothing
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Extensions.Logging.Testing/TestLoggerOfT.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Extensions.Logging.Testing
+{
+    public class TestLogger<T> : ILogger
+    {
+        private readonly ILogger _logger;
+
+        public TestLogger(TestLoggerFactory factory)
+        {
+            _logger = factory.CreateLogger<T>();
+        }
+
+        public IDisposable BeginScopeImpl(object state)
+        {
+            return _logger.BeginScopeImpl(state);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return _logger.IsEnabled(logLevel);
+        }
+
+        public void Log(
+            LogLevel logLevel,
+            int eventId,
+            object state,
+            Exception exception,
+            Func<object, Exception, string> formatter)
+        {
+            _logger.Log(logLevel, eventId, state, exception, formatter);
+        }
+    }
+}
