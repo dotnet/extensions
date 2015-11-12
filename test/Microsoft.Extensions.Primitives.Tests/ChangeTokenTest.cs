01eff58f033d0c37215094bd923182aaa48a884f
Hao Kung <haok@microsoft.com>
2015-11-11 18:29:23 -0800

    Add ChangeToken helpers

---
+++ test/Microsoft.Extensions.Primitives.Tests/ChangeTokenTest.cs
@@ -0,0 +1,59 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.Extensions.Primitives
+{
+    public class ChangeTokenTests
+    {
+        public class TestChangeToken : IChangeToken
+        {
+            private bool _changed = false;
+
+            public bool ActiveChangeCallbacks { get; set; }
+            public bool HasChanged { get; set; }
+
+            public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+            {
+                _callback = () => callback(state);
+                return null;
+            }
+
+            public void Changed()
+            {
+                HasChanged = true;
+                _callback();
+            }
+
+            private Action _callback;
+        }
+
+        [Fact]
+        public void HasChangeFiresChange()
+        {
+            var token = new TestChangeToken();
+            bool fired = false;
+            ChangeToken.OnChange(() => token, () => fired = true);
+            Assert.False(fired);
+            token.Changed();
+            Assert.True(fired);
+        }
+
+        [Fact]
+        public void HasChangeFiresChangeWithState()
+        {
+            var token = new TestChangeToken();
+            object state = new object();
+            object callbackState = null;
+            ChangeToken.OnChange(() => token, s => callbackState = s, state);
+            Assert.Null(callbackState);
+            token.Changed();
+            Assert.Equal(state, callbackState);
+        }
+    }
+}
