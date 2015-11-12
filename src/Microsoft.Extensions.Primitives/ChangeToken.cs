f2b3373c11eabdde2e216a9d9a0418ca34918495
Hao Kung <haok@microsoft.com>
2015-11-11 16:43:51 -0800

    Add paramref

--- src/Microsoft.Extensions.Primitives/ChangeToken.cs
+++ src/Microsoft.Extensions.Primitives/ChangeToken.cs
@@ -44,7 +44,7 @@ namespace Microsoft.Extensions.Primitives
         }
 
         /// <summary>
-        /// Registers the changeTokenConsumer action to be called whenever the token produced changes.
+        /// Registers the <paramref="changeTokenConsumer"/> action to be called whenever the token produced changes.
         /// </summary>
         /// <param name="changeTokenProducer">Produces the change token.</param>
         /// <param name="changeTokenConsumer">Action called when the token changes.</param>
