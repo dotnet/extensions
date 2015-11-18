8ec3e45517b7ee1e351f2720827f440353a53031
Pavel Krymets <pavel@krymets.com>
2015-11-18 09:49:58 -0800

    Fix build

--- test/Microsoft.Extensions.Logging.Test/LoggerMessageTest.cs
+++ test/Microsoft.Extensions.Logging.Test/LoggerMessageTest.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using Microsoft.Extensions.Logging.Test;
 using Xunit;
 using Xunit.Sdk;
