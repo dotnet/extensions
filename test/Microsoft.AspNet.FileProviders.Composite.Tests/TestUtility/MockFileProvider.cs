15c9f1d229b000d9b2472b1fcfd22120a116669e
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2015-11-19 16:27:33 +0100

    Add CompositeFileProvider The CompositeFileProvider merges the content of the directories
    Fixes #49

---
+++ test/Microsoft.AspNet.FileProviders.Composite.Tests/TestUtility/MockFileProvider.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Primitives;
+
+namespace Microsoft.AspNet.FileProviders.Composite.Tests.TestUtility
+{
+    public class MockFileProvider : IFileProvider
+    {
+        private IEnumerable<IFileInfo> _files;
+        private Dictionary<string, IChangeToken> _changeTokens;
+
+        public MockFileProvider()
+        {}
+
+        public MockFileProvider(params IFileInfo[] files)
+        {
+            _files = files;
+        }
+
+        public MockFileProvider(params KeyValuePair<string, IChangeToken>[] changeTokens)
+        {
+            _changeTokens = changeTokens.ToDictionary(
+                changeToken => changeToken.Key,
+                changeToken => changeToken.Value,
+                StringComparer.Ordinal);
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            if (string.IsNullOrEmpty(subpath))
+            {
+                return new EnumerableDirectoryContents(_files);
+            }
+
+            var filesInFolder = _files.Where(f => f.Name.StartsWith(subpath, StringComparison.Ordinal));
+            if (filesInFolder.Any())
+            {
+                return new EnumerableDirectoryContents(filesInFolder);
+            }
+            return new NotFoundDirectoryContents();
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            var file = _files.FirstOrDefault(f => f.Name == subpath);
+            return file ?? new NotFoundFileInfo(subpath);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            if (_changeTokens != null && _changeTokens.ContainsKey(filter))
+            {
+                return _changeTokens[filter];
+            }
+            return NoopChangeToken.Singleton;
+        }
+    }
+}
