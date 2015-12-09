9a0d4e8fd290db0b76f17f1d38ccaf7eee5309ad
Hao Kung <haok@microsoft.com>
2015-12-08 16:24:21 -0800

    Fix build break

--- test/Microsoft.Extensions.OptionsModel.Test/OptionsTest.cs
+++ test/Microsoft.Extensions.OptionsModel.Test/OptionsTest.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
