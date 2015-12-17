9ea4b3c5307f641e6bf089a54b986bdd587c26bb
Ryan Nowak <nowakra@gmail.com>
2015-12-16 17:40:32 -0800

    ObjectPool strawman

---
+++ src/Microsoft.Extensions.ObjectPool/DefaultPooledObjectPolicy.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Extensions.ObjectPool
+{
+    public class DefaultPooledObjectPolicy<T> : IPooledObjectPolicy<T> where T : class, new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
+
+        public bool Return(T obj)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Microsoft.Extensions.ObjectPool/StringBuilderPooledObjectPolicy.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Text;
+
+namespace Microsoft.Extensions.ObjectPool
+{
+    public class StringBuilderPooledObjectPolicy : IPooledObjectPolicy<StringBuilder>
+    {
+        public int InitialCapacity { get; set; } = 100;
+
+        public int MaximumRetainedCapacity { get; set; } = 4 * 1024;
+
+        public StringBuilder Create()
+        {
+            return new StringBuilder(InitialCapacity);
+        }
+
+        public bool Return(StringBuilder obj)
+        {
+            if (obj.Capacity > MaximumRetainedCapacity)
+            {
+                // Too big. Discard this one.
+                return false;
+            }
+
+            obj.Clear();
+            return true;
+        }
+    }
+}
