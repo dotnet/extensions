9e2f59e4cf6889e273db7ef5fc828c17dc7d61cc
N. Taylor Mullen <nimullen@microsoft.com>
2016-01-22 12:20:06 -0800

    Rename AspNet 5 file contents.
    See https://github.com/aspnet/Announcements/issues/144 for more information.

--- test/Microsoft.Extensions.DiagnosticAdapter.Test/Internal/ProxyTypeEmitterTest.cs
+++ test/Microsoft.Extensions.DiagnosticAdapter.Test/Internal/ProxyTypeEmitterTest.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNet.Testing.xunit;
+using Microsoft.AspNetCore.Testing.xunit;
 using Xunit;
 
 namespace Microsoft.Extensions.DiagnosticAdapter.Internal
