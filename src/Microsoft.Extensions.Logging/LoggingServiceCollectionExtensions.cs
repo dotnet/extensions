d730fc9267cc566e69009bf48b1ee546f24f7673
jacalvar <jacalvar@microsoft.com>
2016-02-24 22:47:52 -0800

    Make ServiceCollectionExtensions consistent

--- src/Microsoft.Extensions.Logging/LoggingServiceCollectionExtensions.cs
+++ src/Microsoft.Extensions.Logging/LoggingServiceCollectionExtensions.cs
@@ -1,18 +1,30 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Extension methods for setting up logging services in an <see cref="IServiceCollection" />.
+    /// </summary>
     public static class LoggingServiceCollectionExtensions
     {
-        public static IServiceCollection AddLogging(this IServiceCollection services)
+        /// <summary>
+        /// Adds logging services to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        public static void AddLogging(this IServiceCollection services)
         {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
             services.TryAdd(ServiceDescriptor.Singleton<ILoggerFactory, LoggerFactory>());
             services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
-            return services;
         }
     }
 }
