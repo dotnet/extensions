5d6b9e547d27cd6d44dabec91059b1f029f50c17
Hao Kung <haok@microsoft.com>
2016-04-01 16:25:23 -0700

    Revert "Adding back GetBasePath for UserSecrets for now"
    This reverts commit d7da902a0aeca14c6b7ee7e6796214ce5bc7985b.

--- test/Microsoft.Extensions.Configuration.FileExtensions.Test/FileConfigurationBuilderExtensionsTest.cs
+++ test/Microsoft.Extensions.Configuration.FileExtensions.Test/FileConfigurationBuilderExtensionsTest.cs
@@ -11,64 +11,6 @@ namespace Microsoft.Extensions.Configuration.Json
     public class FileConfigurationBuilderExtensionsTest
     {
         [Fact]
-        public void SetBasePath_ThrowsIfBasePathIsNull()
-        {
-            // Arrange
-            var configurationBuilder = new ConfigurationBuilder();
-
-            // Act and Assert
-            var ex = Assert.Throws<ArgumentNullException>(() => configurationBuilder.SetBasePath(null));
-            Assert.Equal("basePath", ex.ParamName);
-        }
-
-        [Fact]
-        public void SetBasePath_CheckPropertiesValueOnBuilder()
-        {
-            var expectedBasePath = Directory.GetCurrentDirectory();
-            var configurationBuilder = new ConfigurationBuilder();
-
-            configurationBuilder.SetBasePath(expectedBasePath);
-            Assert.Equal(expectedBasePath, configurationBuilder.Properties["BasePath"]);
-        }
-
-        [Fact]
-        public void GetBasePath_ReturnBaseBathIfSet()
-        {
-            // Arrange
-            var testDir = Path.GetDirectoryName(Path.GetTempFileName());
-            var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.SetBasePath(testDir);
-
-            // Act
-            var actualPath = configurationBuilder.GetBasePath();
-
-            // Assert
-            Assert.Equal(testDir, actualPath);
-        }
-
-        [Fact]
-        public void GetBasePath_ReturnBaseDirectoryIfNotSet()
-        {
-            // Arrange
-            var configurationBuilder = new ConfigurationBuilder();
-
-            // Act
-            var actualPath = configurationBuilder.GetBasePath();
-
-            string expectedPath;
-
-#if NETSTANDARDAPP1_5
-            expectedPath = AppContext.BaseDirectory;
-#else
-            expectedPath = Path.GetFullPath(AppDomain.CurrentDomain.GetData("APP_CONTEXT_BASE_DIRECTORY") as string ?? 
-                AppDomain.CurrentDomain.BaseDirectory);
-#endif
-
-            // Assert
-            Assert.Equal(expectedPath, actualPath);
-        }
-
-        [Fact]
         public void SetFileProvider_ThrowsIfBasePathIsNull()
         {
             // Arrange
