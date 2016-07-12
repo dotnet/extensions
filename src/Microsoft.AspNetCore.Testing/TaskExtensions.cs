54f96084e78f988e70e9a04365983ce53b903380
Cesar Blum Silveira <cesars@microsoft.com>
2016-07-11 17:00:26 -0700

    Add Task extensions to timeout after a given time period.

---
+++ src/Microsoft.AspNetCore.Testing/TaskExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Testing
+{
+    public static class TaskExtensions
+    {
+        public static async Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)
+        {
+            if (task == await Task.WhenAny(task, Task.Delay(timeout)))
+            {
+                return await task;
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        public static async Task TimeoutAfter(this Task task, TimeSpan timeout)
+        {
+            if (task == await Task.WhenAny(task, Task.Delay(timeout)))
+            {
+                await task;
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Testing.Tests/TaskExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Testing
+{
+    public class TaskExtensionsTest
+    {
+        [Fact]
+        public async Task TimeoutAfterTest()
+        {
+            const double timeoutMilliseconds = 100;
+            var sw = new Stopwatch();
+            sw.Start();
+            await Assert.ThrowsAsync<TimeoutException>(async () => await Task.Run(() =>
+            {
+                while (true) { };
+            }).TimeoutAfter(TimeSpan.FromMilliseconds(timeoutMilliseconds)));
+            Assert.True(sw.ElapsedMilliseconds >= timeoutMilliseconds);
+        }
+    }
+}
