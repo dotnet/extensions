cc814c61435491917804bb4502c87eb93efd3960
David Fowler <davidfowl@gmail.com>
2016-08-08 16:45:41 -0700

    Add support for IServiceProviderFactory<TContainerBuilder> - A new abstraction that allows the DI container author to describe how to translate an IServiceCollection to a specific container and then to an IServiceProvider. - Hosting will use this abstraction to swap out the default container

---
+++ src/Microsoft.Extensions.DependencyInjection/DefaultServiceProviderFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class DefaultServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
+    {
+        public IServiceCollection CreateBuilder(IServiceCollection services)
+        {
+            return services;
+        }
+
+        public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder)
+        {
+            return containerBuilder.BuildServiceProvider();
+        }
+    }
+}
