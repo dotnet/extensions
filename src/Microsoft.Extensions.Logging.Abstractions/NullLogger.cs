2affd93f72139efbfd8b6f359e357f371903688a
BrennanConroy <brecon@microsoft.com>
2016-08-22 09:00:54 -0700

    Move NullLogger out of Internal

--- src/Microsoft.Extensions.Logging.Abstractions/NullLogger.cs
+++ src/Microsoft.Extensions.Logging.Abstractions/NullLogger.cs
@@ -2,9 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Extensions.Logging.Abstractions.Internal;
 
-namespace Microsoft.Extensions.Logging.Abstractions.Internal
+namespace Microsoft.Extensions.Logging.Abstractions
 {
+    /// <summary>
+    /// Minimalistic logger that does nothing.
+    /// </summary>
     public class NullLogger : ILogger
     {
         public static NullLogger Instance { get; } = new NullLogger();
@@ -13,16 +17,19 @@ namespace Microsoft.Extensions.Logging.Abstractions.Internal
         {
         }
 
+        /// <inheritdoc />
         public IDisposable BeginScope<TState>(TState state)
         {
             return NullScope.Instance;
         }
 
+        /// <inheritdoc />
         public bool IsEnabled(LogLevel logLevel)
         {
             return false;
         }
 
+        /// <inheritdoc />
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
         }
--- src/Microsoft.Extensions.Logging.Abstractions/NullLoggerProvider.cs
+++ src/Microsoft.Extensions.Logging.Abstractions/NullLoggerProvider.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-namespace Microsoft.Extensions.Logging.Abstractions.Internal
+namespace Microsoft.Extensions.Logging.Abstractions
 {
+    /// <summary>
+    /// Provider for the <see cref="NullLogger"/>.
+    /// </summary>
     public class NullLoggerProvider : ILoggerProvider
     {
         public static NullLoggerProvider Instance { get; } = new NullLoggerProvider();
@@ -11,11 +14,13 @@ namespace Microsoft.Extensions.Logging.Abstractions.Internal
         {
         }
 
+        /// <inheritdoc />
         public ILogger CreateLogger(string categoryName)
         {
             return NullLogger.Instance;
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
         }
