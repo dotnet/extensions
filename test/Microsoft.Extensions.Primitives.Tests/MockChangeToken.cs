b5274f38e6a03e88d9b76602081a170ae4482500
Jass Bagga <jbagga93@live.ca>
2016-12-05 17:32:58 -0800

    Copyright license added

--- test/Microsoft.Extensions.Primitives.Tests/MockChangeToken.cs
+++ test/Microsoft.Extensions.Primitives.Tests/MockChangeToken.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.Primitives.Tests
 {
