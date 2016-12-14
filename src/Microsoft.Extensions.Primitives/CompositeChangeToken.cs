80670c7248da31de91dda064925ccbabbf39088a
Jass Bagga <jbagga93@live.ca>
2016-12-13 18:21:23 -0800

    Callback proxy added

--- src/Microsoft.Extensions.Primitives/CompositeChangeToken.cs
+++ src/Microsoft.Extensions.Primitives/CompositeChangeToken.cs
@@ -7,10 +7,15 @@ using System.Collections.Generic;
 namespace Microsoft.Extensions.Primitives
 {
     /// <summary>
-    /// Represents a composition of <see cref="IChangeToken"/>.
+    /// An <see cref="IChangeToken" /> which represents one or more <see cref="IChangeToken" /> instances.
     /// </summary>
     public class CompositeChangeToken : IChangeToken
     {
+        private readonly object _callbackLock = new object();
+        private List<CallbackState> _callbacks = new List<CallbackState>();
+        private bool _registeredCallbackProxy;
+        private bool _canBeChanged = true;
+
         /// <summary>
         /// Creates a new instance of <see cref="CompositeChangeToken"/>.
         /// </summary>
@@ -25,19 +30,105 @@ namespace Microsoft.Extensions.Primitives
             ChangeTokens = changeTokens;
         }
 
+        /// <summary>
+        /// Returns the list of <see cref="IChangeToken"/> which compose the current <see cref="CompositeChangeToken"/>.
+        /// </summary>
         public IReadOnlyList<IChangeToken> ChangeTokens { get; }
 
-        public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+        /// <inheritdoc />
+        public virtual IDisposable RegisterChangeCallback(Action<object> callback, object state)
         {
-            var disposables = new IDisposable[ChangeTokens.Count];
-            for (var i = 0; i < ChangeTokens.Count; i++)
+            if (!_canBeChanged)
             {
-                var disposable = ChangeTokens[i].RegisterChangeCallback(callback, state);
-                disposables[i] = disposable;
+                return NullDisposable.Singleton;
             }
-            return new CompositeDisposable(disposables);
+
+            var changeTokenState = new CallbackState(callback, state, new CallbackDisposable());
+            _callbacks.Add(changeTokenState);
+
+            EnsureCallbacksRegistered();
+
+            return changeTokenState.Disposable;
         }
 
+        private void EnsureCallbacksRegistered()
+        {
+            lock (_callbackLock)
+            {
+                if (_registeredCallbackProxy)
+                {
+                    return;
+                }
+
+                for (var i = 0; i < ChangeTokens.Count; i++)
+                {
+                    ChangeTokens[i].RegisterChangeCallback(OnChange, null);
+                }
+
+                _registeredCallbackProxy = true;
+            }
+        }
+
+        private void OnChange(object state)
+        {
+            if (!_canBeChanged)
+            {
+                return;
+            }
+
+            _canBeChanged = false;
+            lock (_callbackLock)
+            {
+                for (var i = 0; i < _callbacks.Count; i++)
+                {
+                    var callbackState = _callbacks[i];
+                    if (callbackState.Disposable.Disposed)
+                    {
+                        continue;
+                    }
+
+                    callbackState.Callback(callbackState.State);
+                }
+            }
+        }
+
+        private class NullDisposable : IDisposable
+        {
+            public static readonly NullDisposable Singleton = new NullDisposable();
+
+            public bool Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+        }
+
+        private class CallbackDisposable : IDisposable
+        {
+            public bool Disposed { get; private set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+        }
+
+        private struct CallbackState
+        {
+            public CallbackState(Action<object> callback, object state, CallbackDisposable disposable)
+            {
+                Callback = callback;
+                State = state;
+                Disposable = disposable;
+            }
+
+            public Action<object> Callback { get; }
+            public object State { get; }
+            public CallbackDisposable Disposable { get; }
+        }
+
+        /// <inheritdoc />
         public bool HasChanged
         {
             get
@@ -54,6 +145,7 @@ namespace Microsoft.Extensions.Primitives
             }
         }
 
+        /// <inheritdoc />
         public bool ActiveChangeCallbacks
         {
             get
@@ -67,24 +159,6 @@ namespace Microsoft.Extensions.Primitives
                 }
 
                 return false;
-            }
-        }
-
-        private class CompositeDisposable : IDisposable
-        {
-            private readonly IDisposable[] _disposables;
-
-            public CompositeDisposable(IDisposable[] disposables)
-            {
-                _disposables = disposables;
-            }
-
-            public void Dispose()
-            {
-                for (var i = 0; i < _disposables.Length; i++)
-                {
-                    _disposables[i].Dispose();
-                }
             }
         }
     }
--- test/Microsoft.Extensions.Primitives.Tests/CompositeChangeTokenTest.cs
+++ test/Microsoft.Extensions.Primitives.Tests/CompositeChangeTokenTest.cs
@@ -2,61 +2,82 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using Moq;
 using Xunit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.Primitives.Tests
 {
     public class CompositeChangeTokenTest
     {
         [Fact]
+        public void RegisterChangeCallback_IsInvokedExactlyOnce()
+        {
+            // Arrange 
+            var firstChangeToken = new MockChangeToken();
+            var secondChangeToken = new MockChangeToken();
+            var thirdChangeToken = new MockChangeToken();
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken, thirdChangeToken });
+            var count1 = 0;
+            var count2 = 0;
+
+            compositeChangeToken.RegisterChangeCallback(_ => count1++, null);
+            compositeChangeToken.RegisterChangeCallback(_ => count2++, null);
+
+            // Act
+            firstChangeToken.RaiseCallback(null);
+            secondChangeToken.RaiseCallback(null);
+            thirdChangeToken.RaiseCallback(null);
+
+            // Assert
+            Assert.Equal(1, count1);
+            Assert.Equal(1, count2);
+        }
+
+        [Fact]
         public void RegisterChangeCallback_ReturnsACompositeDisposable()
         {
             // Arrange
-            var firstChangeToken = new MockChangeToken();
-            var secondChangeToken = new MockChangeToken();
-
-            var changeTokens = new List<IChangeToken>();
-            changeTokens.Add(firstChangeToken);
-            changeTokens.Add(secondChangeToken);
+            var firstChangeToken = new Mock<IChangeToken>();
+            var secondChangeToken = new Mock<IChangeToken>();
 
             object result = null;
             object state = new object();
+            Action<object> callback = item => result = item;
+
+            var compositeDisposable = new MockDisposable();
+
+            var compositeChangeToken = new Mock<CompositeChangeToken>(new List<IChangeToken> { firstChangeToken.Object, secondChangeToken.Object });
+            compositeChangeToken.Setup(t => t.RegisterChangeCallback(callback, state))
+              .Returns(() =>
+              {
+                  var disposable = new MockDisposable();
+                  disposable.Dispose();
+                  compositeDisposable = disposable;
+                  return disposable;
+              });
 
             // Act
-            var compositeChangeToken = new CompositeChangeToken(changeTokens);
-            var disposable = compositeChangeToken.RegisterChangeCallback(item =>
-            {
-                result = item;
-            }, state);
+            compositeChangeToken.Object.RegisterChangeCallback(callback, state);
+            compositeDisposable.Dispose();
 
             // Assert
-            Assert.Equal(1, firstChangeToken.Callbacks.Count);
-            Assert.False(firstChangeToken.Callbacks[0].Item3.Disposed);
-            Assert.Equal(1, secondChangeToken.Callbacks.Count);
-            Assert.False(secondChangeToken.Callbacks[0].Item3.Disposed);
-
-            disposable.Dispose();
-            Assert.Equal(1, firstChangeToken.Callbacks.Count);
-            Assert.True(firstChangeToken.Callbacks[0].Item3.Disposed);
-            Assert.Equal(1, secondChangeToken.Callbacks.Count);
-            Assert.True(secondChangeToken.Callbacks[0].Item3.Disposed);
+            Assert.True(compositeDisposable.Disposed);
         }
 
         [Fact]
         public void HasChanged_IsTrue_IfAnyTokenHasChanged()
         {
             // Arrange
-            var firstChangeToken = new MockChangeToken { HasChanged = true };
-            var secondChangeToken = new MockChangeToken();
+            var firstChangeToken = new MockChangeToken();
+            var secondChangeToken = new MockChangeToken { HasChanged = true };
+            var thirdChangeToken = new MockChangeToken();
 
-            var changeTokens = new List<IChangeToken>();
-            changeTokens.Add(firstChangeToken);
-            changeTokens.Add(secondChangeToken);
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken, thirdChangeToken });
 
-            // Act
-            var compositeChangeToken = new CompositeChangeToken(changeTokens);
-
-            // Assert
+            // Act & Assert
             Assert.True(compositeChangeToken.HasChanged);
         }
 
@@ -67,14 +88,9 @@ namespace Microsoft.Extensions.Primitives.Tests
             var firstChangeToken = new MockChangeToken();
             var secondChangeToken = new MockChangeToken();
 
-            var changeTokens = new List<IChangeToken>();
-            changeTokens.Add(firstChangeToken);
-            changeTokens.Add(secondChangeToken);
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken });
 
-            // Act
-            var compositeChangeToken = new CompositeChangeToken(changeTokens);
-
-            // Assert
+            // Act & Assert
             Assert.False(compositeChangeToken.HasChanged);
         }
 
@@ -82,17 +98,13 @@ namespace Microsoft.Extensions.Primitives.Tests
         public void ActiveChangeCallbacks_IsTrue_IfAnyTokenHasActiveChangeCallbacks()
         {
             // Arrange
-            var firstChangeToken = new MockChangeToken { ActiveChangeCallbacks = true };
-            var secondChangeToken = new MockChangeToken();
+            var firstChangeToken = new MockChangeToken();
+            var secondChangeToken = new MockChangeToken { ActiveChangeCallbacks = true };
+            var thirdChangeToken = new MockChangeToken();
 
-            var changeTokens = new List<IChangeToken>();
-            changeTokens.Add(firstChangeToken);
-            changeTokens.Add(secondChangeToken);
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken, thirdChangeToken });
 
-            // Act
-            var compositeChangeToken = new CompositeChangeToken(changeTokens);
-
-            // Assert
+            // Act & Assert
             Assert.True(compositeChangeToken.ActiveChangeCallbacks);
         }
 
@@ -103,15 +115,92 @@ namespace Microsoft.Extensions.Primitives.Tests
             var firstChangeToken = new MockChangeToken();
             var secondChangeToken = new MockChangeToken();
 
-            var changeTokens = new List<IChangeToken>();
-            changeTokens.Add(firstChangeToken);
-            changeTokens.Add(secondChangeToken);
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken });
+
+            // Act & Assert
+            Assert.False(compositeChangeToken.ActiveChangeCallbacks);
+        }
+
+        [Fact]
+        public async Task CallbackInvokedOnce_ConcurrentThreadsBlocked()
+        {
+            // Arrange
+            var event1 = new AutoResetEvent(false);
+            var event2 = new AutoResetEvent(false);
+            var event3 = new AutoResetEvent(false);
+
+            var firstChangeToken = new MockChangeToken();
+            var secondChangeToken = new MockChangeToken();
+            var count = 0;
+            Action<object> callback = _ =>
+            {
+                count++;
+                event3.Set();
+                event1.WaitOne(5000);
+            };
+
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken });
+            compositeChangeToken.RegisterChangeCallback(callback, null);
 
             // Act
-            var compositeChangeToken = new CompositeChangeToken(changeTokens);
+            var firstChange = Task.Run(() =>
+            {
+                event2.WaitOne(5000);
+                firstChangeToken.RaiseCallback(null);
+            });
+            var secondChange = Task.Run(() =>
+            {
+                event3.WaitOne(5000);
+                secondChangeToken.RaiseCallback(null);
+            }).ContinueWith(x => event1.Set());
+
+            event2.Set();
+
+            await Task.WhenAll(firstChange, secondChange);
 
             // Assert
-            Assert.False(compositeChangeToken.ActiveChangeCallbacks);
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public async Task RegisterChangeCallbackOnce_ConcurrentThreadsBlocked()
+        {
+            // Arrange
+            var event1 = new AutoResetEvent(false);
+            var event2 = new AutoResetEvent(false);
+            var event3 = new AutoResetEvent(false);
+
+            var firstChangeToken = new MockChangeToken();
+            var secondChangeToken = new MockChangeToken();
+            var count = 0;
+            Action<object> callback = _ =>
+            {
+                count++;
+                event3.Set();
+                event1.WaitOne();
+            };
+
+            var compositeChangeToken = new CompositeChangeToken(new List<IChangeToken> { firstChangeToken, secondChangeToken });
+            compositeChangeToken.RegisterChangeCallback(callback, null);
+
+            // Act
+            var firstChange = Task.Run(() =>
+            {
+                event2.WaitOne(5000);
+                firstChangeToken.RaiseCallback(null);
+            });
+            var secondChange = Task.Run(() =>
+            {
+                event3.WaitOne();
+                compositeChangeToken.RegisterChangeCallback(callback, null);
+            }).ContinueWith(x => event1.Set());
+
+            event2.Set();
+
+            await Task.WhenAll(firstChange, secondChange);
+
+            // Assert
+            Assert.Equal(1, count);
         }
     }
 }
