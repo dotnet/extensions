a91f16ee10b85f7fc2ad7f4a59c2f1d951fb3a9f
Nate McMaster <nate.mcmaster@microsoft.com>
2017-01-05 17:36:26 -0800

    Remove redundant references and fixup MSBuild target test

--- test/Microsoft.Extensions.Configuration.UserSecrets.Test/MsBuildTargetTest.cs
+++ test/Microsoft.Extensions.Configuration.UserSecrets.Test/MsBuildTargetTest.cs
@@ -42,10 +42,12 @@ namespace Microsoft.Extensions.Configuration.UserSecrets
             }
             var target = Path.Combine(_solutionRoot.FullName, "src", "Microsoft.Extensions.Configuration.UserSecrets", "build", "netstandard1.0", "Microsoft.Extensions.Configuration.UserSecrets.targets");
             Directory.CreateDirectory(Path.Combine(_tempDir, "obj"));
+            var libName = "Microsoft.Extensions.Configuration.UserSecrets.dll";
+            File.Copy(Path.Combine(AppContext.BaseDirectory, libName), Path.Combine(_tempDir, libName));
             File.Copy(target, Path.Combine(_tempDir, "obj", "test.csproj.usersecretstest.targets")); // imitates how NuGet will import this target
             var testProj = Path.Combine(_tempDir, "test.csproj");
             // should represent a 'dotnet new' project
-            File.WriteAllText(testProj, @"
+            File.WriteAllText(testProj, $@"
 <Project Sdk=""Microsoft.NET.Sdk"" ToolsVersion=""15.0"">
     <PropertyGroup>
         <OutputType>Exe</OutputType>
@@ -55,6 +57,7 @@ namespace Microsoft.Extensions.Configuration.UserSecrets
     <ItemGroup>
         <Compile Include=""Program.cs""/>
         <PackageReference Include=""Microsoft.NETCore.App"" Version=""1.0.1"" />
+        <Reference Include=""$(MSBuildThisFileDirectory){libName}"" />
     </ItemGroup>
 </Project>
 ");
@@ -82,16 +85,18 @@ namespace Microsoft.Extensions.Configuration.UserSecrets
                 UseShellExecute = false,
                 WorkingDirectory = _tempDir
             };
-            Process.Start(buildInfo).WaitForExit();
-            // build will fail because the test proj doesn't reference UserSecrets.dll
-            // but that's okay. We just want to verify the target generates code correctly
+            var build = Process.Start(buildInfo);
+            build.WaitForExit();
+            Assert.Equal(0, build.ExitCode);
 
             Assert.True(File.Exists(assemblyInfoFile));
             var contents = File.ReadAllText(assemblyInfoFile);
             Assert.Contains("[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute(\"xyz123\")]", contents);
             var lastWrite = new FileInfo(assemblyInfoFile).LastWriteTimeUtc;
 
-            Process.Start(buildInfo).WaitForExit();
+            var rebuild = Process.Start(buildInfo);
+            rebuild.WaitForExit();
+            Assert.Equal(0, rebuild.ExitCode);
             // asserts that the target doesn't re-generate assembly file. Important for incremental build.
             Assert.Equal(lastWrite, new FileInfo(assemblyInfoFile).LastWriteTimeUtc);
         }
@@ -100,7 +105,7 @@ namespace Microsoft.Extensions.Configuration.UserSecrets
         {
             try
             {
-                Directory.Delete(_tempDir, recursive: true);
+          //      Directory.Delete(_tempDir, recursive: true);
             }
             catch
             {
