831ad485ccb35d3bd775149c15d680ccefc09e8c
David Fowler <davidfowl@gmail.com>
2017-03-11 22:33:36 -0800

    Added benchmarks project and basic benchmark  (#502)
    * Added initial benchmarks for DI

---
+++ test/Microsoft.Extensions.DependencyInjection.Performance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.Extensions.DependencyInjection.Performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
---
+++ test/Microsoft.Extensions.DependencyInjection.Performance/configs/CoreConfig.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Validators;
+
+namespace Microsoft.Extensions.DependencyInjection.Performance
+{
+    public class CoreConfig : ManualConfig
+    {
+        public CoreConfig()
+        {
+            Add(JitOptimizationsValidator.FailOnError);
+            Add(MemoryDiagnoser.Default);
+
+            Add(Job.Default
+                .With(BenchmarkDotNet.Environments.Runtime.Core)
+                .WithRemoveOutliers(false)
+                .With(RunStrategy.Throughput)
+                .WithLaunchCount(3)
+                .WithWarmupCount(5)
+                .WithTargetCount(10));
+        }
+    }
+}
