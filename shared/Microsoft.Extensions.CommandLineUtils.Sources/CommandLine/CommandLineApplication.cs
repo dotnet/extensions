48418a24184a5b91bfde9b6c68c9d7893eebf448
Brar Piening <brar@gmx.de>
2017-03-16 12:11:42 +0100

    Move CommandArgumentEnumerator into ArgsProcessor

--- shared/Microsoft.Extensions.CommandLineUtils.Sources/CommandLine/CommandLineApplication.cs
+++ shared/Microsoft.Extensions.CommandLineUtils.Sources/CommandLine/CommandLineApplication.cs
@@ -369,48 +369,6 @@ namespace Microsoft.Extensions.CommandLineUtils
             return option != null;
         }
 
-        private class CommandArgumentEnumerator : IEnumerator<CommandArgument>
-        {
-            private readonly IEnumerator<CommandArgument> _enumerator;
-
-            public CommandArgumentEnumerator(IEnumerator<CommandArgument> enumerator)
-            {
-                _enumerator = enumerator;
-            }
-
-            public CommandArgument Current
-            {
-                get { return _enumerator.Current; }
-            }
-
-            object IEnumerator.Current
-            {
-                get { return Current; }
-            }
-
-            public void Dispose()
-            {
-                _enumerator.Dispose();
-            }
-
-            public bool MoveNext()
-            {
-                if (Current == null || !Current.MultipleValues)
-                {
-                    return _enumerator.MoveNext();
-                }
-
-                // If current argument allows multiple values, we don't move forward and
-                // all later values will be added to current CommandArgument.Values
-                return true;
-            }
-
-            public void Reset()
-            {
-                _enumerator.Reset();
-            }
-        }
-
         private class ArgsProcessor
         {
             private enum ArgType
@@ -515,6 +473,48 @@ namespace Microsoft.Extensions.CommandLineUtils
                 {
                     _current = null;
                     _argsEnumerator.Reset();
+                }
+            }
+
+            private class CommandArgumentEnumerator : IEnumerator<CommandArgument>
+            {
+                private readonly IEnumerator<CommandArgument> _enumerator;
+
+                public CommandArgumentEnumerator(IEnumerator<CommandArgument> enumerator)
+                {
+                    _enumerator = enumerator;
+                }
+
+                public CommandArgument Current
+                {
+                    get { return _enumerator.Current; }
+                }
+
+                object IEnumerator.Current
+                {
+                    get { return Current; }
+                }
+
+                public void Dispose()
+                {
+                    _enumerator.Dispose();
+                }
+
+                public bool MoveNext()
+                {
+                    if (Current == null || !Current.MultipleValues)
+                    {
+                        return _enumerator.MoveNext();
+                    }
+
+                    // If current argument allows multiple values, we don't move forward and
+                    // all later values will be added to current CommandArgument.Values
+                    return true;
+                }
+
+                public void Reset()
+                {
+                    _enumerator.Reset();
                 }
             }
 
