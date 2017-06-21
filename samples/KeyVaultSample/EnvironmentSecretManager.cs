c0fa2c35993295ee9670a3ac14fda56a66c12150
Rob Lindley <Rob@coderrob.com>
2017-06-21 13:32:03 -0400

    Add ConfigurationPath.KeyDelimiter replacement for "--" to GetKey (#679)

--- samples/KeyVaultSample/EnvironmentSecretManager.cs
+++ samples/KeyVaultSample/EnvironmentSecretManager.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace ConsoleApplication
 {
-    public class EnvironmentSecretManager : IKeyVaultSecretManager
+    public class EnvironmentSecretManager : DefaultKeyVaultSecretManager
     {
         private readonly string _environmentPrefix;
 
@@ -12,14 +12,23 @@ namespace ConsoleApplication
             _environmentPrefix = environment + "-";
         }
 
-        public bool Load(SecretItem secret)
+        public override bool Load(SecretItem secret)
         {
-            return secret.Identifier.Name.StartsWith(_environmentPrefix);
+            return HasEnvironmentPrefix(secret.Identifier.Name);
         }
 
-        public string GetKey(SecretBundle secret)
+        public override string GetKey(SecretBundle secret)
         {
-            return secret.SecretIdentifier.Name.Substring(_environmentPrefix.Length);
+            var keyName = base.GetKey(secret);
+
+            return HasEnvironmentPrefix(keyName)
+                ? keyName.Substring(_environmentPrefix.Length)
+                : keyName;
+        }
+
+        private bool HasEnvironmentPrefix(string name)
+        {
+            return name.StartsWith(_environmentPrefix);
         }
     }
 }
