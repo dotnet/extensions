d7e26a0d61d2eda05289fac30c983a79ed36dca2
Javier Calvarro Nelson <jacalvar@microsoft.com>
2017-09-21 13:37:53 -0700

    Add a shared sources library with code to generate certificates for development

---
+++ shared/Microsoft.AspNetCore.Certificates.Generation.Sources/CertificateManager.cs
@@ -0,0 +1,439 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if NETCOREAPP2_0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Certificates.Generation
+{
+    internal class CertificateManager
+    {
+        public const string AspNetHttpsOid = "1.3.6.1.4.1.311.84.1.1";
+        public const string AspNetHttpsOidFriendlyName = "ASP.NET Core HTTPS development certificate";
+
+        public const string AspNetIdentityOid = "1.3.6.1.4.1.311.84.1.2";
+        public const string AspNetIdentityOidFriendlyName = "ASP.NET Core Identity Json Web Token signing development certificate";
+
+        private const string ServerAuthenticationEnhancedKeyUsageOid = "1.3.6.1.5.5.7.3.1";
+        private const string ServerAuthenticationEnhancedKeyUsageOidFriendlyName = "Server Authentication";
+
+        private const string LocalhostHttpsDnsName = "localhost";
+        private const string LocalhostHttpsDistinguishedName = "CN=" + LocalhostHttpsDnsName;
+
+        private const string IdentityDistinguishedName = "CN=Microsoft.AspNetCore.Identity.Signing";
+
+        public const int RSAMinimumKeySizeInBits = 2048;
+
+        public IList<X509Certificate2> ListCertificates(
+            CertificatePurpose purpose,
+            StoreName storeName,
+            StoreLocation location,
+            bool isValid,
+            bool requireExportable = true)
+        {
+            var certificates = new List<X509Certificate2>();
+            try
+            {
+                using (var store = new X509Store(storeName, location))
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    certificates.AddRange(store.Certificates.OfType<X509Certificate2>());
+                    IEnumerable<X509Certificate2> matchingCertificates = certificates;
+                    switch (purpose)
+                    {
+                        case CertificatePurpose.All:
+                            matchingCertificates = matchingCertificates
+                                .Where(c => HasOid(c, AspNetHttpsOid) || HasOid(c, AspNetIdentityOid));
+                            break;
+                        case CertificatePurpose.HTTPS:
+                            matchingCertificates = matchingCertificates
+                                .Where(c => HasOid(c, AspNetHttpsOid));
+                            break;
+                        case CertificatePurpose.Signing:
+                            matchingCertificates = matchingCertificates
+                                .Where(c => HasOid(c, AspNetIdentityOid));
+                            break;
+                        default:
+                            break;
+                    }
+                    if (isValid)
+                    {
+                        // Ensure the certificate hasn't expired, has a private key and its exportable
+                        // (for container/unix scenarios).
+                        var now = DateTimeOffset.Now;
+                        matchingCertificates = matchingCertificates
+                            .Where(c => c.NotBefore <= now &&
+                                now <= c.NotAfter &&
+                                (!requireExportable || !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || IsExportable(c)));
+                    }
+
+                    // We need to enumerate the certificates early to prevent dispoisng issues.
+                    matchingCertificates = matchingCertificates.ToList();
+
+                    var certificatesToDispose = certificates.Except(matchingCertificates);
+                    DisposeCertificates(certificatesToDispose);
+
+                    store.Close();
+
+                    return (IList<X509Certificate2>)matchingCertificates;
+                }
+            }
+            catch
+            {
+                DisposeCertificates(certificates);
+                certificates.Clear();
+                return certificates;
+            }
+
+            bool HasOid(X509Certificate2 certificate, string oid) =>
+                certificate.Extensions.OfType<X509Extension>()
+                    .Any(e => string.Equals(oid, e.Oid.Value, StringComparison.Ordinal));
+
+            bool IsExportable(X509Certificate2 c) =>
+                ((c.GetRSAPrivateKey() is RSACryptoServiceProvider rsaPrivateKey &&
+                    rsaPrivateKey.CspKeyContainerInfo.Exportable) ||
+                (c.GetRSAPrivateKey() is RSACng cngPrivateKey &&
+                    cngPrivateKey.Key.ExportPolicy == CngExportPolicies.AllowExport));
+        }
+
+        private void DisposeCertificates(IEnumerable<X509Certificate2> disposables)
+        {
+            foreach (var disposable in disposables)
+            {
+                try
+                {
+                    disposable.Dispose();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public X509Certificate2 CreateAspNetCoreHttpsDevelopmentCertificate(DateTimeOffset notBefore, DateTimeOffset notAfter, string subjectOverride)
+        {
+            var subject = new X500DistinguishedName(subjectOverride ?? LocalhostHttpsDistinguishedName);
+            var extensions = new List<X509Extension>();
+            var sanBuilder = new SubjectAlternativeNameBuilder();
+            sanBuilder.AddDnsName(LocalhostHttpsDnsName);
+
+            var keyUsage = new X509KeyUsageExtension(X509KeyUsageFlags.KeyEncipherment, critical: true);
+            var enhancedKeyUsage = new X509EnhancedKeyUsageExtension(
+                new OidCollection() {
+                    new Oid(
+                        ServerAuthenticationEnhancedKeyUsageOid,
+                        ServerAuthenticationEnhancedKeyUsageOidFriendlyName)
+                },
+                critical: true);
+
+            var basicConstraints = new X509BasicConstraintsExtension(
+                certificateAuthority: false,
+                hasPathLengthConstraint: false,
+                pathLengthConstraint: 0,
+                critical: true);
+
+            var aspNetHttpsExtension = new X509Extension(
+                new AsnEncodedData(
+                    new Oid(AspNetHttpsOid, AspNetHttpsOidFriendlyName),
+                    Encoding.ASCII.GetBytes(AspNetHttpsOidFriendlyName)),
+                critical: false);
+
+            extensions.Add(basicConstraints);
+            extensions.Add(keyUsage);
+            extensions.Add(enhancedKeyUsage);
+            extensions.Add(sanBuilder.Build(critical: true));
+            extensions.Add(aspNetHttpsExtension);
+
+            var certificate = CreateSelfSignedCertificate(subject, extensions, notBefore, notAfter);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                certificate.FriendlyName = AspNetHttpsOidFriendlyName;
+            }
+
+            return certificate;
+        }
+
+        public X509Certificate2 CreateApplicationTokenSigningDevelopmentCertificate(DateTimeOffset notBefore, DateTimeOffset notAfter, string subjectOverride)
+        {
+            var subject = new X500DistinguishedName(subjectOverride ?? IdentityDistinguishedName);
+            var extensions = new List<X509Extension>();
+
+            var keyUsage = new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, critical: true);
+            var enhancedKeyUsage = new X509EnhancedKeyUsageExtension(
+                new OidCollection() {
+                    new Oid(
+                        ServerAuthenticationEnhancedKeyUsageOid,
+                        ServerAuthenticationEnhancedKeyUsageOidFriendlyName)
+                },
+                critical: true);
+
+            var basicConstraints = new X509BasicConstraintsExtension(
+                certificateAuthority: false,
+                hasPathLengthConstraint: false,
+                pathLengthConstraint: 0,
+                critical: true);
+
+            var aspNetIdentityExtension = new X509Extension(
+                new AsnEncodedData(
+                    new Oid(AspNetIdentityOid, AspNetIdentityOidFriendlyName),
+                    Encoding.ASCII.GetBytes(AspNetIdentityOidFriendlyName)),
+                critical: false);
+
+            extensions.Add(basicConstraints);
+            extensions.Add(keyUsage);
+            extensions.Add(enhancedKeyUsage);
+            extensions.Add(aspNetIdentityExtension);
+
+            var certificate = CreateSelfSignedCertificate(subject, extensions, notBefore, notAfter);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                certificate.FriendlyName = AspNetIdentityOidFriendlyName;
+            }
+
+            return certificate;
+        }
+
+        public X509Certificate2 CreateSelfSignedCertificate(
+            X500DistinguishedName subject,
+            IEnumerable<X509Extension> extensions,
+            DateTimeOffset notBefore,
+            DateTimeOffset notAfter)
+        {
+            var key = CreateKeyMaterial(RSAMinimumKeySizeInBits);
+
+            var request = new CertificateRequest(subject, key, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            foreach (var extension in extensions)
+            {
+                request.CertificateExtensions.Add(extension);
+            }
+
+            return request.CreateSelfSigned(notBefore, notAfter);
+
+            RSA CreateKeyMaterial(int minimumKeySize)
+            {
+                var rsa = RSA.Create(minimumKeySize);
+                if (rsa.KeySize < minimumKeySize)
+                {
+                    throw new InvalidOperationException($"Failed to create a key with a size of {minimumKeySize} bits");
+                }
+
+                return rsa;
+            }
+        }
+
+        public X509Certificate2 SaveCertificateInStore(X509Certificate2 certificate, StoreName name, StoreLocation location)
+        {
+            var imported = certificate;
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // On non OSX systems we need to export the certificate and import it so that the transient
+                // key that we generated gets persisted.
+                var export = certificate.Export(X509ContentType.Pkcs12, "");
+                imported = new X509Certificate2(export, "", X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+                Array.Clear(export, 0, export.Length);
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                imported.FriendlyName = certificate.FriendlyName;
+            }
+
+            using (var store = new X509Store(name, location))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(imported);
+                store.Close();
+            };
+
+            return imported;
+        }
+
+        public void ExportCertificate(X509Certificate2 certificate, string path, bool includePrivateKey, string password)
+        {
+            if (Path.GetDirectoryName(path) != "")
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+
+            if (includePrivateKey)
+            {
+                var bytes = certificate.Export(X509ContentType.Pkcs12, password);
+                try
+                {
+                    File.WriteAllBytes(path, bytes);
+                }
+                finally
+                {
+                    Array.Clear(bytes, 0, bytes.Length);
+                }
+            }
+            else
+            {
+                var bytes = certificate.Export(X509ContentType.Cert);
+                File.WriteAllBytes(path, bytes);
+            }
+        }
+
+        public void TrustCertificate(X509Certificate2 certificate)
+        {
+            // Strip certificate of the private key if any.
+            var publicCertificate = new X509Certificate2(certificate.Export(X509ContentType.Cert));
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                publicCertificate.FriendlyName = certificate.FriendlyName;
+            }
+
+            using (var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(publicCertificate);
+                store.Close();
+            };
+        }
+
+        public void RemoveAllCertificates(CertificatePurpose purpose, StoreName storeName, StoreLocation storeLocation, string subject = null)
+        {
+            var certificates = ListCertificates(purpose, storeName, storeLocation, isValid: false);
+            var certificatesWithName = subject == null ? certificates : certificates.Where(c => c.Subject == subject);
+
+            RemoveAllCertificatesCore(certificatesWithName, storeName, storeLocation);
+            DisposeCertificates(certificates);
+        }
+
+        public void RemoveAllCertificatesCore(IEnumerable<X509Certificate2> certificates, StoreName storeName, StoreLocation storeLocation)
+        {
+            using (var store = new X509Store(storeName, storeLocation))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                foreach (var certificate in certificates)
+                {
+                    var matching = store.Certificates.OfType<X509Certificate2>().Single(c => c.SerialNumber == certificate.SerialNumber);
+                    store.Remove(matching);
+                }
+                store.Close();
+            }
+        }
+
+        public EnsureCertificateResult EnsureAspNetCoreHttpsDevelopmentCertificate(
+            DateTimeOffset notBefore,
+            DateTimeOffset notAfter,
+            string path = null,
+            bool trust = false,
+            bool includePrivateKey = false,
+            string password = null,
+            string subject = LocalhostHttpsDistinguishedName)
+        {
+            return EnsureValidCertificateExists(notBefore, notAfter, CertificatePurpose.HTTPS, path, trust, includePrivateKey, password, subject);
+        }
+
+        public EnsureCertificateResult EnsureAspNetCoreApplicationTokensDevelopmentCertificate(
+            DateTimeOffset notBefore,
+            DateTimeOffset notAfter,
+            string path = null,
+            bool trust = false,
+            bool includePrivateKey = false,
+            string password = null,
+            string subject = IdentityDistinguishedName)
+        {
+            return EnsureValidCertificateExists(notBefore, notAfter, CertificatePurpose.Signing, path, trust, includePrivateKey, password, subject);
+        }
+
+        public EnsureCertificateResult EnsureValidCertificateExists(
+            DateTimeOffset notBefore,
+            DateTimeOffset notAfter,
+            CertificatePurpose purpose,
+            string path = null,
+            bool trust = false,
+            bool includePrivateKey = false,
+            string password = null,
+            string subjectOverride = null)
+        {
+            if (purpose == CertificatePurpose.All)
+            {
+                throw new ArgumentException("The certificate must have a specific purpose.");
+            }
+
+            var certificates = ListCertificates(purpose, StoreName.My, StoreLocation.CurrentUser, isValid: true).Concat(
+                ListCertificates(purpose, StoreName.My, StoreLocation.LocalMachine, isValid: true));
+
+            certificates = subjectOverride == null ? certificates : certificates.Where(c => c.Subject == subjectOverride);
+
+            var result = EnsureCertificateResult.Succeeded;
+
+            X509Certificate2 certificate = null;
+            if (certificates.Count() > 0)
+            {
+                certificate = certificates.FirstOrDefault();
+                result = EnsureCertificateResult.ValidCertificatePresent;
+            }
+            else
+            {
+                try
+                {
+                    switch (purpose)
+                    {
+                        case CertificatePurpose.All:
+                            throw new InvalidOperationException("The certificate must have a specific purpose.");
+                        case CertificatePurpose.HTTPS:
+                            certificate = CreateAspNetCoreHttpsDevelopmentCertificate(notBefore, notAfter, subjectOverride);
+                            break;
+                        case CertificatePurpose.Signing:
+                            certificate = CreateApplicationTokenSigningDevelopmentCertificate(notBefore, notAfter, subjectOverride);
+                            break;
+                        default:
+                            throw new InvalidOperationException("The certificate must have a purpose.");
+                    }
+                }
+                catch
+                {
+                    return EnsureCertificateResult.ErrorCreatingTheCertificate;
+                }
+
+                try
+                {
+                    certificate = SaveCertificateInStore(certificate, StoreName.My, StoreLocation.CurrentUser);
+                }
+                catch
+                {
+                    return EnsureCertificateResult.ErrorSavingTheCertificateIntoTheCurrentUserPersonalStore;
+                }
+            }
+            if (path != null)
+            {
+                try
+                {
+                    ExportCertificate(certificate, path, includePrivateKey, password);
+                }
+                catch
+                {
+                    return EnsureCertificateResult.ErrorExportingTheCertificate;
+                }
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && trust)
+            {
+                try
+                {
+                    TrustCertificate(certificate);
+                }
+                catch
+                {
+                    return EnsureCertificateResult.FailedToTrustTheCertificate;
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
+#endif
---
+++ shared/Microsoft.AspNetCore.Certificates.Generation.Sources/EnsureCertificateResult.cs
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if NETCOREAPP2_0
+
+namespace Microsoft.AspNetCore.Certificates.Generation
+{
+    internal enum EnsureCertificateResult
+    {
+        Succeeded = 1,
+        ValidCertificatePresent,
+        ErrorCreatingTheCertificate,
+        ErrorSavingTheCertificateIntoTheCurrentUserPersonalStore,
+        ErrorExportingTheCertificate,
+        FailedToTrustTheCertificate
+    }
+}
+
+#endif
