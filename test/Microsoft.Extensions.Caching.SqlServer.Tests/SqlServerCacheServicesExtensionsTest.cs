3ac69c681a3a8f8557018c950348a074ad882cfe
Nate McMaster <nate.mcmaster@microsoft.com>
2017-10-05 13:15:59 -0700

    Minor change to test code to resolve the xUnit2013 build warning

--- test/Microsoft.Extensions.Caching.SqlServer.Tests/SqlServerCacheServicesExtensionsTest.cs
+++ test/Microsoft.Extensions.Caching.SqlServer.Tests/SqlServerCacheServicesExtensionsTest.cs
@@ -1,10 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -24,8 +21,7 @@ namespace Microsoft.Extensions.Caching.SqlServer
             SqlServerCachingServicesExtensions.AddSqlServerCacheServices(services);
 
             // Assert
-            Assert.Equal(1, services.Count);
-            var serviceDescriptor = services[0];
+            var serviceDescriptor = Assert.Single(services);
             Assert.Equal(typeof(IDistributedCache), serviceDescriptor.ServiceType);
             Assert.Equal(typeof(SqlServerCache), serviceDescriptor.ImplementationType);
             Assert.Equal(ServiceLifetime.Singleton, serviceDescriptor.Lifetime);
