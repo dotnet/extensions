1a8cddf7a5002b410f2274d00c291784e0f8002a
Pavel Krymets <pavel@krymets.com>
2017-10-11 13:15:19 -0700

    Validations

--- shared/Microsoft.AspNetCore.BenchmarkRunner.Sources/Program.cs
+++ shared/Microsoft.AspNetCore.BenchmarkRunner.Sources/Program.cs
@@ -14,11 +14,45 @@ namespace Microsoft.AspNetCore.BenchmarkDotNet.Runner
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             CheckValidate(ref args);
-            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly)
+            var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly)
                 .Run(args, ManualConfig.CreateEmpty());
+
+            foreach (var summary in summaries)
+            {
+                if (summary.HasCriticalValidationErrors)
+                {
+                    return Fail(summary, nameof(summary.HasCriticalValidationErrors));
+                }
+
+                foreach (var report in summary.Reports)
+                {
+                    if (!report.BuildResult.IsGenerateSuccess)
+                    {
+                        return Fail(report, nameof(report.BuildResult.IsGenerateSuccess));
+                    }
+
+                    if (!report.BuildResult.IsBuildSuccess)
+                    {
+                        return Fail(report, nameof(report.BuildResult.IsBuildSuccess));
+                    }
+
+                    if (!report.AllMeasurements.Any())
+                    {
+                        return Fail(report, nameof(report.AllMeasurements));
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        static int Fail(object o, string message)
+        {
+            Console.WriteLine("'{0}' failed, reason: '{1}'", o, message);
+            return 1;
         }
 
         static void CheckValidate(ref string[] args)
@@ -26,12 +60,12 @@ namespace Microsoft.AspNetCore.BenchmarkDotNet.Runner
             var argsList = args.ToList();
             if (argsList.Remove("--validate"))
             {
-                ConditionalJobConfig.Job = Job.Dry;
+                ParameterizedJobConfigAttribute.Job = Job.Dry;
             }
 
             if (argsList.Remove("--validate-fast"))
             {
-                ConditionalJobConfig.Job = Job.Dry.With(InProcessToolchain.Instance);
+                ParameterizedJobConfigAttribute.Job = Job.Dry.With(InProcessToolchain.Instance);
             }
 
             args = argsList.ToArray();
