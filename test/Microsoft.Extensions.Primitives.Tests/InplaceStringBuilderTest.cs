abdc3a5f00991b11c7464ab8ba22d77c8e67605a
Pranav K <prkrishn@hotmail.com>
2017-11-28 13:55:48 -0800

    Fixup namespace and missing license header

--- test/Microsoft.Extensions.Primitives.Tests/InplaceStringBuilderTest.cs
+++ test/Microsoft.Extensions.Primitives.Tests/InplaceStringBuilderTest.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using Microsoft.Extensions.Primitives;
 using Xunit;
 
-namespace Microsoft.AspNetCore.Http.Tests.Internal
+namespace Microsoft.Extensions.Primitives
 {
     public class InplaceStringBuilderTest
     {
