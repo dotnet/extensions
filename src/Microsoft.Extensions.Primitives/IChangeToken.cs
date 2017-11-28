d7afeffc51f71860ca0928bab5c48cbd14fa3a0f
Chris Ross <Tratcher@Outlook.com>
2017-11-28 09:51:20 -0800

    Clarify ActiveChangeCallbacks comments (#300)

--- src/Microsoft.Extensions.Primitives/IChangeToken.cs
+++ src/Microsoft.Extensions.Primitives/IChangeToken.cs
@@ -16,7 +16,8 @@ namespace Microsoft.Extensions.Primitives
         bool HasChanged { get; }
 
         /// <summary>
-        /// Indicates if this token will pro-actively raise callbacks. Callbacks are still guaranteed to fire, eventually.
+        /// Indicates if this token will pro-actively raise callbacks. If <c>false</c>, the token consumer must
+        /// poll <see cref="HasChanged" /> to detect changes.
         /// </summary>
         bool ActiveChangeCallbacks { get; }
 
