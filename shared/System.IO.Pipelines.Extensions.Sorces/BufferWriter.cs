dd8ea3c630af665e1252ba2e7befe9b25ec047b2
Pavel Krymets <pavel@krymets.com>
2018-02-20 15:49:05 -0800

    Add BufferReader/Writer

---
+++ shared/System.IO.Pipelines.Extensions.Sorces/BufferWriter.cs
@@ -0,0 +1,67 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+
+namespace System.IO.Pipelines.Extensions
+{
+    public ref struct BufferWriter
+    {
+        private PipeWriter _writer;
+        private Span<byte> _span;
+
+        public BufferWriter(PipeWriter writer)
+        {
+            _writer = writer;
+            _span = writer.GetSpan();
+        }
+
+        public Span<byte> Span => _span;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Advance(int count)
+        {
+            _span = _span.Slice(count);
+            _writer.Advance(count);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Write(ReadOnlySpan<byte> source)
+        {
+            if (_span.Length >= source.Length)
+            {
+                source.CopyTo(_span);
+                Advance(source.Length);
+            }
+            else
+            {
+                WriteMultiBuffer(source);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Ensure(int count = 1)
+        {
+            _writer.GetMemory(count);
+            _span = _writer.GetSpan();
+        }
+
+        private void WriteMultiBuffer(ReadOnlySpan<byte> source)
+        {
+            while (source.Length > 0)
+            {
+                if (_span.Length == 0)
+                {
+                    Ensure();
+                }
+
+                var writable = Math.Min(source.Length, _span.Length);
+                source.Slice(0, writable).CopyTo(_span);
+                source = source.Slice(writable);
+                Advance(writable);
+            }
+        }
+    }
+}
