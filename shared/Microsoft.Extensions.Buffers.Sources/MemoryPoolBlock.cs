cf9ea199c32d2f846bfbeebebc4e6507424ec6ba
Pavel Krymets <pavel@krymets.com>
2018-02-28 12:41:56 -0800

    Add BufferReader/Writer (#316)

---
+++ shared/Microsoft.Extensions.Buffers.Sources/MemoryPoolBlock.cs
@@ -0,0 +1,136 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.IO.Pipelines;
+
+namespace System.Buffers
+{
+    /// <summary>
+    /// Block tracking object used by the byte buffer memory pool. A slab is a large allocation which is divided into smaller blocks. The
+    /// individual blocks are then treated as independent array segments.
+    /// </summary>
+    internal class MemoryPoolBlock : OwnedMemory<byte>
+    {
+        private readonly int _offset;
+        private readonly int _length;
+        private int _referenceCount;
+        private bool _disposed;
+
+        /// <summary>
+        /// This object cannot be instantiated outside of the static Create method
+        /// </summary>
+        protected MemoryPoolBlock(SlabMemoryPool pool, MemoryPoolSlab slab, int offset, int length)
+        {
+            _offset = offset;
+            _length = length;
+
+            Pool = pool;
+            Slab = slab;
+        }
+
+        /// <summary>
+        /// Back-reference to the memory pool which this block was allocated from. It may only be returned to this pool.
+        /// </summary>
+        public SlabMemoryPool Pool { get; }
+
+        /// <summary>
+        /// Back-reference to the slab from which this block was taken, or null if it is one-time-use memory.
+        /// </summary>
+        public MemoryPoolSlab Slab { get; }
+
+        public override int Length => _length;
+
+        public override Span<byte> Span
+        {
+            get
+            {
+                if (IsDisposed) ThrowHelper.ThrowObjectDisposedException(ExceptionArgument.MemoryPoolBlock);
+                return new Span<byte>(Slab.Array, _offset, _length);
+            }
+        }
+
+#if BLOCK_LEASE_TRACKING
+        public bool IsLeased { get; set; }
+        public string Leaser { get; set; }
+#endif
+
+        ~MemoryPoolBlock()
+        {
+            if (Slab != null && Slab.IsActive)
+            {
+#if DEBUG
+                Debug.Assert(false, $"{Environment.NewLine}{Environment.NewLine}*** Block being garbage collected instead of returned to pool" +
+#if BLOCK_LEASE_TRACKING
+                    $": {Leaser}" +
+#endif
+                    $" ***{ Environment.NewLine}");
+#endif
+
+                // Need to make a new object because this one is being finalized
+                Pool.Return(new MemoryPoolBlock(Pool, Slab, _offset, _length));
+            }
+        }
+
+        internal static MemoryPoolBlock Create(
+            int offset,
+            int length,
+            SlabMemoryPool pool,
+            MemoryPoolSlab slab)
+        {
+            return new MemoryPoolBlock(pool, slab, offset, length);
+        }
+
+        protected void OnZeroReferences()
+        {
+            Pool.Return(this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _disposed = true;
+        }
+
+        public override void Retain()
+        {
+            if (IsDisposed) ThrowHelper.ThrowObjectDisposedException(ExceptionArgument.MemoryPoolBlock);
+            Interlocked.Increment(ref _referenceCount);
+        }
+
+        public override bool Release()
+        {
+            int newRefCount = Interlocked.Decrement(ref _referenceCount);
+            if (newRefCount < 0) ThrowHelper.ThrowInvalidOperationException_ReferenceCountZero();
+            if (newRefCount == 0)
+            {
+                OnZeroReferences();
+                return false;
+            }
+            return true;
+        }
+
+        protected override bool IsRetained => _referenceCount > 0;
+        public override bool IsDisposed => _disposed;
+
+        // In kestrel both MemoryPoolBlock and OwnedMemory end up in the same assembly so
+        // this method access modifiers need to be `protected internal`
+        protected override bool TryGetArray(out ArraySegment<byte> arraySegment)
+        {
+            if (IsDisposed) ThrowHelper.ThrowObjectDisposedException(ExceptionArgument.MemoryPoolBlock);
+            arraySegment = new ArraySegment<byte>(Slab.Array, _offset, _length);
+            return true;
+        }
+
+        public override MemoryHandle Pin(int byteOffset = 0)
+        {
+            Retain();   // checks IsDisposed
+            if (byteOffset < 0 || byteOffset > _length) ThrowHelper.ThrowArgumentOutOfRangeException(_length, byteOffset);
+            unsafe
+            {
+                return new MemoryHandle(this, (Slab.NativePointer + _offset + byteOffset).ToPointer());
+            }
+        }
+    }
+}
---
+++ shared/Microsoft.Extensions.Buffers.Testing.Sources/CustomMemoryForTest.cs
@@ -0,0 +1,106 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace System.Buffers
+{
+    internal class CustomMemoryForTest<T> : OwnedMemory<T>
+    {
+        private bool _disposed;
+        private int _referenceCount;
+        private int _noReferencesCalledCount;
+        private T[] _array;
+        private readonly int _offset;
+        private readonly int _length;
+
+        public CustomMemoryForTest(T[] array): this(array, 0, array.Length)
+        {
+        }
+
+        public CustomMemoryForTest(T[] array, int offset, int length)
+        {
+            _array = array;
+            _offset = offset;
+            _length = length;
+        }
+
+        public int OnNoRefencesCalledCount => _noReferencesCalledCount;
+
+        public override int Length => _length;
+
+        public override bool IsDisposed => _disposed;
+
+        protected override bool IsRetained => _referenceCount > 0;
+
+        public override Span<T> Span
+        {
+            get
+            {
+                if (IsDisposed)
+                    throw new ObjectDisposedException(nameof(CustomMemoryForTest<T>));
+                return new Span<T>(_array, _offset, _length);
+            }
+        }
+
+        public override MemoryHandle Pin(int byteOffset = 0)
+        {
+            unsafe
+            {
+                Retain();
+                if (byteOffset != 0 && (((uint)byteOffset) - 1) / Unsafe.SizeOf<T>() >= _array.Length)
+                    throw new ArgumentOutOfRangeException(nameof(byteOffset));
+                var handle = GCHandle.Alloc(_array, GCHandleType.Pinned);
+                return new MemoryHandle(this, Unsafe.Add<byte>((void*)handle.AddrOfPinnedObject(), _offset + byteOffset), handle);
+            }
+        }
+
+        protected override bool TryGetArray(out ArraySegment<T> arraySegment)
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(CustomMemoryForTest<T>));
+            arraySegment = new ArraySegment<T>(_array, _offset, _length);
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _array = null;
+            }
+
+            _disposed = true;
+
+        }
+
+        public override void Retain()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(CustomMemoryForTest<T>));
+            Interlocked.Increment(ref _referenceCount);
+        }
+
+        public override bool Release()
+        {
+            int newRefCount = Interlocked.Decrement(ref _referenceCount);
+
+            if (newRefCount < 0)
+                throw new InvalidOperationException();
+
+            if (newRefCount == 0)
+            {
+                _noReferencesCalledCount++;
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
