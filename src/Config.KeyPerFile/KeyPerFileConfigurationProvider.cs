b4e14caba529518fd6984e6af046c8a170abb7c4
Hao Kung <HaoK@users.noreply.github.com>
2018-03-27 13:06:39 -0700

    Rename DockerSecrets => KeyPerFile (#784)

--- src/Config.KeyPerFile/KeyPerFileConfigurationProvider.cs
+++ src/Config.KeyPerFile/KeyPerFileConfigurationProvider.cs
@@ -1,30 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Microsoft.Extensions.FileProviders;
 
-namespace Microsoft.Extensions.Configuration.DockerSecrets
+namespace Microsoft.Extensions.Configuration.KeyPerFile
 {
     /// <summary>
-    /// An docker secrets based <see cref="ConfigurationProvider"/>.
+    /// A <see cref="ConfigurationProvider"/> that uses a directory's files as configuration key/values.
     /// </summary>
-    public class DockerSecretsConfigurationProvider : ConfigurationProvider
+    public class KeyPerFileConfigurationProvider : ConfigurationProvider
     {
-        DockerSecretsConfigurationSource Source { get; set; }
+        KeyPerFileConfigurationSource Source { get; set; }
 
         /// <summary>
         /// Initializes a new instance.
         /// </summary>
         /// <param name="source">The settings.</param>
-        public DockerSecretsConfigurationProvider(DockerSecretsConfigurationSource source)
-        {
-            Source = source ?? throw new ArgumentNullException(nameof(source));
-        }
+        public KeyPerFileConfigurationProvider(KeyPerFileConfigurationSource source)
+            => Source = source ?? throw new ArgumentNullException(nameof(source));
 
         private static string NormalizeKey(string key)
-        {
-            return key.Replace("__", ConfigurationPath.KeyDelimiter);
-        }
+            => key.Replace("__", ConfigurationPath.KeyDelimiter);
 
         /// <summary>
         /// Loads the docker secrets.
@@ -35,27 +30,23 @@ namespace Microsoft.Extensions.Configuration.DockerSecrets
 
             if (Source.FileProvider == null)
             {
-                if (Directory.Exists(Source.SecretsDirectory))
-                {
-                    Source.FileProvider = new PhysicalFileProvider(Source.SecretsDirectory);
-                }
-                else if (Source.Optional)
+                if (Source.Optional)
                 {
                     return;
                 }
                 else
                 {
-                    throw new DirectoryNotFoundException("DockerSecrets directory doesn't exist and is not optional.");
+                    throw new DirectoryNotFoundException("A non-null file provider for the directory is required when this source is not optional.");
                 }
             }
 
-            var secretsDir = Source.FileProvider.GetDirectoryContents("/");
-            if (!secretsDir.Exists && !Source.Optional)
+            var directory = Source.FileProvider.GetDirectoryContents("/");
+            if (!directory.Exists && !Source.Optional)
             {
-                throw new DirectoryNotFoundException("DockerSecrets directory doesn't exist and is not optional.");
+                throw new DirectoryNotFoundException("The root directory for the FileProvider doesn't exist and is not optional.");
             }
 
-            foreach (var file in secretsDir)
+            foreach (var file in directory)
             {
                 if (file.IsDirectory)
                 {
