d55ce0cc13050a781efef5d08c79f332e52fa385
Pavel Krymets <pavel@krymets.com>
2018-04-13 10:12:56 -0700

    Add azure logging change token source unconditionally (#806)

--- src/Microsoft.Extensions.Logging.AzureAppServices/AzureAppServicesLoggerFactoryExtensions.cs
+++ src/Microsoft.Extensions.Logging.AzureAppServices/AzureAppServicesLoggerFactoryExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging.AzureAppServices;
 using Microsoft.Extensions.Logging.AzureAppServices.Internal;
@@ -22,6 +23,13 @@ namespace Microsoft.Extensions.Logging
         public static ILoggingBuilder AddAzureWebAppDiagnostics(this ILoggingBuilder builder)
         {
             var context = WebAppContext.Default;
+
+            // Only add the provider if we're in Azure WebApp. That cannot change once the apps started
+            return AddAzureWebAppDiagnostics(builder, context);
+        }
+
+        internal static ILoggingBuilder AddAzureWebAppDiagnostics(this ILoggingBuilder builder, IWebAppContext context)
+        {
             if (!context.IsRunningInAzureWebApp)
             {
                 return builder;
@@ -30,28 +38,40 @@ namespace Microsoft.Extensions.Logging
             var config = SiteConfigurationProvider.GetAzureLoggingConfiguration(context);
             var services = builder.Services;
 
-            services.TryAddEnumerable(Singleton<IConfigureOptions<LoggerFilterOptions>>(CreateFileFilterConfigureOptions(config)));
-            services.TryAddEnumerable(Singleton<IConfigureOptions<LoggerFilterOptions>>(CreateBlobFilterConfigureOptions(config)));
+            var addedFileLogger = TryAddEnumerable(services, Singleton<ILoggerProvider, FileLoggerProvider>());
+            var addedBlobLogger = TryAddEnumerable(services, Singleton<ILoggerProvider, BlobLoggerProvider>());
 
-            services.TryAddEnumerable(Singleton<IOptionsChangeTokenSource<LoggerFilterOptions>>(
-                    new ConfigurationChangeTokenSource<LoggerFilterOptions>(config)));
+            if (addedFileLogger || addedBlobLogger)
+            {
+                services.AddSingleton(context);
+                services.AddSingleton<IOptionsChangeTokenSource<LoggerFilterOptions>>(
+                    new ConfigurationChangeTokenSource<LoggerFilterOptions>(config));
+            }
 
-            services.TryAddEnumerable(Singleton<IConfigureOptions<AzureBlobLoggerOptions>>(
-                new BlobLoggerConfigureOptions(config, context)));
-            services.TryAddEnumerable(Singleton<IOptionsChangeTokenSource<AzureBlobLoggerOptions>>(
-                new ConfigurationChangeTokenSource<AzureBlobLoggerOptions>(config)));
+            if (addedFileLogger)
+            {
+                services.AddSingleton<IConfigureOptions<LoggerFilterOptions>>(CreateFileFilterConfigureOptions(config));
+                services.AddSingleton<IConfigureOptions<AzureFileLoggerOptions>>(new FileLoggerConfigureOptions(config, context));
+                services.AddSingleton<IOptionsChangeTokenSource<AzureFileLoggerOptions>>(
+                    new ConfigurationChangeTokenSource<AzureFileLoggerOptions>(config));
+            }
 
-            services.TryAddEnumerable(Singleton<IConfigureOptions<AzureFileLoggerOptions>>(new FileLoggerConfigureOptions(config, context)));
-            services.TryAddEnumerable(Singleton<IOptionsChangeTokenSource<AzureFileLoggerOptions>>(
-                new ConfigurationChangeTokenSource<AzureFileLoggerOptions>(config)));
-
-            services.TryAddEnumerable(Singleton<IWebAppContext>(context));
-
-            // Only add the provider if we're in Azure WebApp. That cannot change once the apps started
-            services.TryAddEnumerable(Singleton<ILoggerProvider, FileLoggerProvider>());
-            services.TryAddEnumerable(Singleton<ILoggerProvider, BlobLoggerProvider>());
+            if (addedBlobLogger)
+            {
+                services.AddSingleton<IConfigureOptions<LoggerFilterOptions>>(CreateBlobFilterConfigureOptions(config));
+                services.AddSingleton<IConfigureOptions<AzureBlobLoggerOptions>>(new BlobLoggerConfigureOptions(config, context));
+                services.AddSingleton<IOptionsChangeTokenSource<AzureBlobLoggerOptions>>(
+                    new ConfigurationChangeTokenSource<AzureBlobLoggerOptions>(config));
+            }
 
             return builder;
+        }
+
+        private static bool TryAddEnumerable(IServiceCollection collection, ServiceDescriptor descriptor)
+        {
+            var beforeCount = collection.Count;
+            collection.TryAddEnumerable(descriptor);
+            return beforeCount != collection.Count;
         }
 
         private static ConfigurationBasedLevelSwitcher CreateBlobFilterConfigureOptions(IConfiguration config)
