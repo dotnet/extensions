43a6d7b56bdd7fa175e503707c92ce0cae17852b
Pavel Krymets <pavel@krymets.com>
2018-05-03 09:25:24 -0700

    cleanup

--- test/Microsoft.AspNetCore.Testing.Tests/TestPathUtilitiesTest.cs
+++ test/Microsoft.AspNetCore.Testing.Tests/TestPathUtilitiesTest.cs
@@ -18,7 +18,7 @@ namespace Microsoft.AspNetCore.Testing
             // Testing\test\Microsoft.AspNetCore.Testing.Tests\bin\Debug\net46
             var expectedPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", ".."));
 
-            Assert.Equal(expectedPath, TestPathUtilities.GetSolutionRootDirectory("Testing"));
+            Assert.Equal(expectedPath, TestPathUtilities.GetSolutionRootDirectory("Common"));
         }
 
         [Fact]
