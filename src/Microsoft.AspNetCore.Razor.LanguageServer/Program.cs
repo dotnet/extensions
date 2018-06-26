ac9875968cfa2b8bf45d42cf0f75723464712c4e
N. Taylor Mullen <nimullen@microsoft.com>
2018-06-26 15:16:15 -0700

    Add language server sample.

---
+++ src/Microsoft.AspNetCore.Razor.LanguageServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Server;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var server = await OmniSharp.Extensions.LanguageServer.Server.LanguageServer.From(options =>
+                options
+                    .WithInput(Console.OpenStandardInput())
+                    .WithOutput(Console.OpenStandardOutput())
+                    .WithLoggerFactory(new LoggerFactory())
+                    .AddDefaultLoggingProvider()
+                    .WithMinimumLogLevel(LogLevel.Trace)
+                    .WithHandler<TextDocumentHandler>());
+
+            await server.WaitForExit;
+        }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Razor.LanguageServer/TextDocumentHandler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer
+{
+    public class TextDocumentHandler : ITextDocumentSyncHandler
+    {
+        private readonly ILanguageServer _router;
+
+        private readonly DocumentSelector _documentSelector = new DocumentSelector(
+            new DocumentFilter()
+            {
+                Pattern = "**/*.cshtml"
+            }
+        );
+
+        private SynchronizationCapability _capability;
+
+        public TextDocumentHandler(ILanguageServer router)
+        {
+            _router = router;
+        }
+
+        public TextDocumentSyncKind Change { get; } = TextDocumentSyncKind.Full;
+
+        public Task Handle(DidChangeTextDocumentParams notification, CancellationToken token)
+        {
+            _router.Window.LogMessage(new LogMessageParams()
+            {
+                Type = MessageType.Log,
+                Message = "Hello World!!!!"
+            });
+            return Task.CompletedTask;
+        }
+
+        TextDocumentChangeRegistrationOptions IRegistration<TextDocumentChangeRegistrationOptions>.GetRegistrationOptions()
+        {
+            return new TextDocumentChangeRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                SyncKind = Change
+            };
+        }
+
+        public void SetCapability(SynchronizationCapability capability)
+        {
+            _capability = capability;
+        }
+
+        public async Task Handle(DidOpenTextDocumentParams notification, CancellationToken token)
+        {
+            await Task.Yield();
+            _router.Window.LogMessage(new LogMessageParams()
+            {
+                Type = MessageType.Log,
+                Message = "Hello World!!!!"
+            });
+        }
+
+        TextDocumentRegistrationOptions IRegistration<TextDocumentRegistrationOptions>.GetRegistrationOptions()
+        {
+            return new TextDocumentRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+        public Task Handle(DidCloseTextDocumentParams notification, CancellationToken token)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(DidSaveTextDocumentParams notification, CancellationToken token)
+        {
+            return Task.CompletedTask;
+        }
+
+        TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions>.GetRegistrationOptions()
+        {
+            return new TextDocumentSaveRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                IncludeText = true
+            };
+        }
+
+        public TextDocumentAttributes GetTextDocumentAttributes(Uri uri)
+        {
+            return new TextDocumentAttributes(uri, "razor");
+        }
+    }
+}
