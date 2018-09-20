168c8a203c7211311b3d47ad198994b5cbefcc12
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-09-20 11:10:44 +0100

    Re-add tslint to client project

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,10 +4,10 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as fs from 'fs';
+import * as razorExtensionPackage from 'microsoft.aspnetcore.razor.vscode';
 import * as os from 'os';
 import * as path from 'path';
 import * as vscode from 'vscode';
-import * as razorExtensionPackage from 'microsoft.aspnetcore.razor.vscode';
 
 let activationResolver: (value?: any) => void;
 export const extensionActivated = new Promise(resolve => {
@@ -17,16 +17,18 @@ export const extensionActivated = new Promise(resolve => {
 export async function activate(context: vscode.ExtensionContext) {
     const ridDir = getPlatformRidDir();
     if (!ridDir) {
-        throw new Error("Unsupported Razor platform.");
+        throw new Error('Unsupported Razor platform.');
     }
-    
+
     // Because this extension is only used for local development and tests in CI,
     // we know the Razor Language Server is at a specific path within this repo
     const languageServerDir = path.join(
-        __dirname, '..', '..', '..', 'src', 'Microsoft.AspNetCore.Razor.LanguageServer', 'bin', 'Debug', 'netcoreapp2.0', ridDir);
+        __dirname, '..', '..', '..', 'src', 'Microsoft.AspNetCore.Razor.LanguageServer',
+        'bin', 'Debug', 'netcoreapp2.0', ridDir);
 
     if (!fs.existsSync(languageServerDir)) {
-        throw new Error(`The Razor Language Server project has not yet been built - could not find ${languageServerDir}`);
+        throw new Error('The Razor Language Server project has not yet been built - '
+            + `could not find ${languageServerDir}`);
     }
 
     await razorExtensionPackage.activate(context, languageServerDir);
@@ -35,15 +37,15 @@ export async function activate(context: vscode.ExtensionContext) {
 
 function getPlatformRidDir() {
     if (!!os.platform().match(/^win/)) {
-        return "win10-x64";
+        return 'win10-x64';
     }
 
     if (!!os.platform().match(/^linux/)) {
-        return "linux-x64";
+        return 'linux-x64';
     }
 
     if (!!os.platform().match(/^darwin/)) {
-        return "osx-x64";
+        return 'osx-x64';
     }
 
     return undefined;
