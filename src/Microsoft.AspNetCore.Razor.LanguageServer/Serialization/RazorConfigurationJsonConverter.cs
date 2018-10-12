0f635895fef1129f91d147c89ec0a7030c5e7ff8
N. Taylor Mullen <nimullen@microsoft.com>
2018-10-12 16:56:32 -0700

    Add Razor serialization converters.
    - These converters are nearly direct copies from the Razor repo and are necessary to serialize project level information.

---
+++ src/Microsoft.AspNetCore.Razor.LanguageServer/Serialization/RazorConfigurationJsonConverter.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+// This class is a copy from the Razor repo.
+
+using System;
+using Microsoft.AspNetCore.Razor.Language;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Serialization
+{
+    internal class RazorConfigurationJsonConverter : JsonConverter
+    {
+        public static readonly RazorConfigurationJsonConverter Instance = new RazorConfigurationJsonConverter();
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(RazorConfiguration).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                return null;
+            }
+
+            var obj = JObject.Load(reader);
+            var configurationName = obj[nameof(RazorConfiguration.ConfigurationName)].Value<string>();
+            var languageVersion = obj[nameof(RazorConfiguration.LanguageVersion)].Value<string>();
+            var extensions = obj[nameof(RazorConfiguration.Extensions)].ToObject<RazorExtension[]>(serializer);
+
+            return RazorConfiguration.Create(RazorLanguageVersion.Parse(languageVersion), configurationName, extensions);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var configuration = (RazorConfiguration)value;
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName(nameof(RazorConfiguration.ConfigurationName));
+            writer.WriteValue(configuration.ConfigurationName);
+
+            writer.WritePropertyName(nameof(RazorConfiguration.LanguageVersion));
+            writer.WriteValue(configuration.LanguageVersion.ToString());
+
+            writer.WritePropertyName(nameof(RazorConfiguration.Extensions));
+            serializer.Serialize(writer, configuration.Extensions);
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Razor.LanguageServer/Serialization/RazorExtensionJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+// This class is a copy from the Razor repo.
+
+using System;
+using Microsoft.AspNetCore.Razor.Language;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Serialization
+{
+    internal class RazorExtensionJsonConverter : JsonConverter
+    {
+        public static readonly RazorExtensionJsonConverter Instance = new RazorExtensionJsonConverter();
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(RazorExtension).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                return null;
+            }
+
+            var obj = JObject.Load(reader);
+            var extensionName = obj[nameof(RazorExtension.ExtensionName)].Value<string>();
+
+            return new SerializedRazorExtension(extensionName);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var extension = (RazorExtension)value;
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName(nameof(RazorExtension.ExtensionName));
+            writer.WriteValue(extension.ExtensionName);
+
+            writer.WriteEndObject();
+        }
+    }
+}
