deca3b1f27bac494d5f12f9859a218f216e7eaad
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-12-04 10:47:15 -0800

    Update test/vstest/runtests.cmd
    Co-Authored-By: HaoK <HaoK@users.noreply.github.com>

--- test/vstest/runtests.cmd
+++ test/vstest/runtests.cmd
@@ -1,6 +1,6 @@
 set target=%1
 set version=%2
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Version %version% -Channel 2.0 -NoCdn -InstallDir %HELIX_CORRELATION_PAYLOAD%\sdk"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))) -Version %version% -InstallDir %HELIX_CORRELATION_PAYLOAD%\sdk"
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 %HELIX_CORRELATION_PAYLOAD%\sdk\dotnet vstest %target% --logger:trx
 
