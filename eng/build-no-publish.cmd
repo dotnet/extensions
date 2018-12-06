847b2874b07d8ab0d9cddd6f9fd587d70882561a
Ryan Nowak <nowakra@gmail.com>
2018-12-05 19:15:44 -0800

    Update Arcade and remove workaround
    Removes the workaround we needed to fix symbol publishing when running
    on full MSBuild

--- eng/build-no-publish.cmd
+++
@@ -1,4 +0,0 @@
-rem workaround for dotnet/arcade#1425
-@echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0common\Build.ps1""" -restore -build -test -sign -pack -ci %*"
-exit /b %ErrorLevel%
--- eng/publish.cmd
+++
@@ -1,4 +0,0 @@
-rem workaround for dotnet/arcade#1425
-@echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0common\Build.ps1""" -publish -ci %*"
-exit /b %ErrorLevel%
