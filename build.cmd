7f180bc8dccccc242850eb3fc68e1a51769b5c2b
Ryan Brandenburg <rybrande@microsoft.com>
2018-12-10 12:38:07 -0800

    Reorder build targets

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -build -restore -pack -test %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -test -pack %*"
 exit /b %ErrorLevel%
