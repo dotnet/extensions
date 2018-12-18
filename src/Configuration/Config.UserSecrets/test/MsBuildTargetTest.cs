bc359563bd75339e392c0c573f3b1c5d7dd314a9
Hao Kung <HaoK@users.noreply.github.com>
2018-12-18 12:52:34 -0800

    Revert "Skip last failing test"
    This reverts commit e93c62c3356c0bdaa8b5c48eb1a23fcfb8aeec2a.

--- src/Configuration/Config.UserSecrets/test/MsBuildTargetTest.cs
+++ src/Configuration/Config.UserSecrets/test/MsBuildTargetTest.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Extensions.Configuration.UserSecrets
             }
         }
 
-        [Fact(Skip = "https://github.com/aspnet/Extensions/issues/624")]
+        [Fact]
         public void GeneratesAssemblyAttributeFile()
         {
             var target = Path.Combine(_solutionRoot.FullName, "src", "Configuration", "Config.UserSecrets", "src", "build", "netstandard2.0", "Microsoft.Extensions.Configuration.UserSecrets.targets");
