12acf96e0587697957e490eb1616a9632f40544e
David Fowler <davidfowl@gmail.com>
2018-12-24 10:23:48 -0800

    Add a test to verify dispose isn't called again on the underlying change token.

--- src/Primitives/test/ChangeTokenTest.cs
+++ src/Primitives/test/ChangeTokenTest.cs
@@ -148,7 +148,7 @@ namespace Microsoft.Extensions.Primitives
             {
                 provider.Changed();
             }
-            
+
             Assert.Equal(5, count);
         }
 
@@ -202,6 +202,100 @@ namespace Microsoft.Extensions.Primitives
             provider.Changed();
 
             Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void DoubleDisposeDisposesOnce()
+        {
+            var provider = new TrackableChangeTokenProvider();
+            var count = 0;
+
+            IDisposable reg = null;
+
+            reg = ChangeToken.OnChange<object>(provider.GetChangeToken, state =>
+            {
+                count++;
+                reg.Dispose();
+            },
+            null);
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+            Assert.Equal(1, provider.RegistrationCalls);
+            Assert.Equal(1, provider.DisposeCalls);
+
+            reg.Dispose();
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+            Assert.Equal(2, provider.RegistrationCalls);
+            Assert.Equal(2, provider.DisposeCalls);
+        }
+
+        public class TrackableChangeTokenProvider
+        {
+            private TrackableChangeToken _cts = new TrackableChangeToken();
+
+            public int RegistrationCalls { get; set; }
+            public int DisposeCalls { get; set; }
+
+            public IChangeToken GetChangeToken() => _cts;
+
+            public void Changed()
+            {
+                var previous = _cts;
+                _cts = new TrackableChangeToken();
+                previous.Execute();
+
+                RegistrationCalls += previous.RegistrationCalls;
+                DisposeCalls += previous.DisposeCalls;
+            }
+        }
+
+        public class TrackableChangeToken : IChangeToken
+        {
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+
+            public int RegistrationCalls { get; set; }
+            public int DisposeCalls { get; set; }
+
+            public bool HasChanged => _cts.IsCancellationRequested;
+
+            public bool ActiveChangeCallbacks => true;
+
+            public void Execute()
+            {
+                _cts.Cancel();
+            }
+
+            public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+            {
+                var registration = _cts.Token.Register(callback, state);
+                RegistrationCalls++;
+
+                return new DisposableAction(() =>
+                {
+                    DisposeCalls++;
+                    registration.Dispose();
+                });
+            }
+
+            private class DisposableAction : IDisposable
+            {
+                private Action _action;
+
+                public DisposableAction(Action action)
+                {
+                    _action = action;
+                }
+
+                public void Dispose()
+                {
+                    _action?.Invoke();
+                }
+            }
         }
 
         public class ResettableChangeTokenProvider
