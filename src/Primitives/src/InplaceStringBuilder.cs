f3061ed72fe6baefb2229393cdd9d60193de2ee8
Pavel Krymets <pavel@krymets.com>
2019-01-08 12:16:28 -0800

    Update InplaceStringBuilder.cs

--- src/Primitives/src/InplaceStringBuilder.cs
+++ src/Primitives/src/InplaceStringBuilder.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 namespace Microsoft.Extensions.Primitives
 {
     [DebuggerDisplay("Value = {_value}")]
-    [Obsolete("This type is obsolete and will be removed in a future version. The recommended alternative is string.Create.")]
+    [Obsolete("This type is obsolete and will be removed in a future version.")]
     public struct InplaceStringBuilder
     {
         private int _offset;
