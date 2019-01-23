b123ee87cabe1e99676b61fc1c39967c33f8e77f
N. Taylor Mullen <nimullen@microsoft.com>
2019-01-23 11:00:11 -0800

    Convert HtmlBlockTest to use ParseDocument
    aspnet/AspNetCore#6470

--- src/Razor/test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlBlockTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlBlockTest.cs
@@ -36,112 +36,111 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
         [Fact]
         public void TagWithoutCloseAngleDoesNotTerminateBlock()
         {
-            ParseBlockTest("<                      " + Environment.NewLine
+            ParseDocumentTest("@{<                      " + Environment.NewLine
                          + "   ");
         }
 
         [Fact]
         public void AllowsStartAndEndTagsToDifferInCase()
         {
-            ParseBlockTest("<li><p>Foo</P></lI>");
+            ParseDocumentTest("@{<li><p>Foo</P></lI>}");
         }
 
         [Fact]
         public void ReadsToEndOfLineIfFirstCharacterAfterTransitionIsColon()
         {
-            ParseBlockTest("@:<li>Foo Bar Baz" + Environment.NewLine
-                         + "bork",
-                         expectedParseLength: 19);
+            ParseDocumentTest("@{@:<li>Foo Bar Baz" + Environment.NewLine
+                         + "bork}");
         }
 
         [Fact]
         public void StopsParsingSingleLineBlockAtEOFIfNoEOLReached()
         {
-            ParseBlockTest("@:foo bar");
+            ParseDocumentTest("@{@:foo bar");
         }
 
         [Fact]
         public void StopsAtMatchingCloseTagToStartTag()
         {
-            ParseBlockTest("<a><b></b></a><c></c>", expectedParseLength: 14);
+            ParseDocumentTest("@{<a><b></b></a><c></c>}");
         }
 
         [Fact]
         public void ParsesUntilMatchingEndTagIfFirstNonWhitespaceCharacterIsStartTag()
         {
-            ParseBlockTest("<baz><boz><biz></biz></boz></baz>");
+            ParseDocumentTest("@{<baz><boz><biz></biz></boz></baz>}");
         }
 
         [Fact]
         public void AllowsUnclosedTagsAsLongAsItCanRecoverToAnExpectedEndTag()
         {
-            ParseBlockTest("<foo><bar><baz></foo>");
+            ParseDocumentTest("@{<foo><bar><baz></foo>}");
         }
 
         [Fact]
         public void WithSelfClosingTagJustEmitsTag()
         {
-            ParseBlockTest("<foo />");
+            ParseDocumentTest("@{<foo />}");
         }
 
         [Fact]
         public void CanHandleSelfClosingTagsWithinBlock()
         {
-            ParseBlockTest("<foo><bar /></foo>");
+            ParseDocumentTest("@{<foo><bar /></foo>}");
         }
 
         [Fact]
         public void SupportsTagsWithAttributes()
         {
-            ParseBlockTest("<foo bar=\"baz\"><biz><boz zoop=zork/></biz></foo>");
+            ParseDocumentTest("@{<foo bar=\"baz\"><biz><boz zoop=zork/></biz></foo>}");
         }
 
         [Fact]
         public void AllowsCloseAngleBracketInAttributeValueIfDoubleQuoted()
         {
-            ParseBlockTest("<foo><bar baz=\">\" /></foo>");
+            ParseDocumentTest("@{<foo><bar baz=\">\" /></foo>}");
         }
 
         [Fact]
         public void AllowsCloseAngleBracketInAttributeValueIfSingleQuoted()
         {
-            ParseBlockTest("<foo><bar baz=\'>\' /></foo>");
+            ParseDocumentTest("@{<foo><bar baz=\'>\' /></foo>}");
         }
 
         [Fact]
         public void AllowsSlashInAttributeValueIfDoubleQuoted()
         {
-            ParseBlockTest("<foo><bar baz=\"/\"></bar></foo>");
+            ParseDocumentTest("@{<foo><bar baz=\"/\"></bar></foo>}");
         }
 
         [Fact]
         public void AllowsSlashInAttributeValueIfSingleQuoted()
         {
-            ParseBlockTest("<foo><bar baz=\'/\'></bar></foo>");
+            ParseDocumentTest("@{<foo><bar baz=\'/\'></bar></foo>}");
         }
 
         [Fact]
         public void TerminatesAtEOF()
         {
-            ParseBlockTest("<foo>");
+            ParseDocumentTest("@{<foo>");
         }
 
         [Fact]
         public void SupportsCommentAsBlock()
         {
-            ParseBlockTest("<!-- foo -->");
+            ParseDocumentTest("@{<!-- foo -->}");
         }
 
         [Fact]
         public void SupportsCommentWithExtraDashAsBlock()
         {
-            ParseBlockTest("<!-- foo --->");
+            ParseDocumentTest("@{<!-- foo --->}");
         }
 
         [Fact]
         public void SupportsCommentWithinBlock()
         {
-            ParseBlockTest("<foo>bar<!-- zoop -->baz</foo>");
+            ParseDocumentTest("@{<foo>bar<!-- zoop -->baz</foo>}");
         }
 
         [Fact]
@@ -158,63 +157,63 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
         [Fact]
         public void ProperlyBalancesCommentStartAndEndTags()
         {
-            ParseBlockTest("<!--<foo></bar>-->");
+            ParseDocumentTest("@{<!--<foo></bar>-->}");
         }
 
         [Fact]
         public void TerminatesAtEOFWhenParsingComment()
         {
-            ParseBlockTest("<!--<foo>");
+            ParseDocumentTest("@{<!--<foo>");
         }
 
         [Fact]
         public void OnlyTerminatesCommentOnFullEndSequence()
         {
-            ParseBlockTest("<!--<foo>--</bar>-->");
+            ParseDocumentTest("@{<!--<foo>--</bar>-->}");
         }
 
         [Fact]
         public void TerminatesCommentAtFirstOccurrenceOfEndSequence()
         {
-            ParseBlockTest("<foo><!--<foo></bar-->--></foo>");
+            ParseDocumentTest("@{<foo><!--<foo></bar-->--></foo>}");
         }
 
         [Fact]
         public void TreatsMalformedTagsAsContent()
         {
-            ParseBlockTest("<foo></!-- bar --></foo>", expectedParseLength: 18);
+            ParseDocumentTest("@{<foo></!-- bar --></foo>}");
         }
 
 
         [Fact]
         public void ParsesSGMLDeclarationAsEmptyTag()
         {
-            ParseBlockTest("<foo><!DOCTYPE foo bar baz></foo>");
+            ParseDocumentTest("@{<foo><!DOCTYPE foo bar baz></foo>}");
         }
 
         [Fact]
         public void TerminatesSGMLDeclarationAtFirstCloseAngle()
         {
-            ParseBlockTest("<foo><!DOCTYPE foo bar> baz></foo>");
+            ParseDocumentTest("@{<foo><!DOCTYPE foo bar> baz></foo>}");
         }
 
         [Fact]
         public void ParsesXMLProcessingInstructionAsEmptyTag()
         {
-            ParseBlockTest("<foo><?xml foo bar baz?></foo>");
+            ParseDocumentTest("@{<foo><?xml foo bar baz?></foo>}");
         }
 
         [Fact]
         public void TerminatesXMLProcessingInstructionAtQuestionMarkCloseAnglePair()
         {
-            ParseBlockTest("<foo><?xml foo bar baz?> baz</foo>");
+            ParseDocumentTest("@{<foo><?xml foo bar baz?> baz</foo>}");
         }
 
         [Fact]
         public void DoesNotTerminateXMLProcInstrAtCloseAngleUnlessPreceededByQuestionMark()
         {
             // ParseBlockDoesNotTerminateXMLProcessingInstructionAtCloseAngleUnlessPreceededByQuestionMark
-            ParseBlockTest("<foo><?xml foo bar> baz?></foo>");
+            ParseDocumentTest("@{<foo><?xml foo bar> baz?></foo>}");
         }
 
         [Fact]
@@ -232,37 +231,37 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
         [Fact]
         public void AcceptsEmptyTextTag()
         {
-            ParseBlockTest("<text/>");
+            ParseDocumentTest("@{<text/>}");
         }
 
         [Fact]
         public void AcceptsTextTagAsOuterTagButDoesNotRender()
         {
-            ParseBlockTest("<text>Foo Bar <foo> Baz</text> zoop", expectedParseLength: 30);
+            ParseDocumentTest("@{<text>Foo Bar <foo> Baz</text> zoop}");
         }
 
         [Fact]
         public void RendersLiteralTextTagIfDoubled()
         {
-            ParseBlockTest("<text><text>Foo Bar <foo> Baz</text></text> zoop", expectedParseLength: 43);
+            ParseDocumentTest("@{<text><text>Foo Bar <foo> Baz</text></text> zoop}");
         }
 
         [Fact]
         public void DoesNotConsiderPsuedoTagWithinMarkupBlock()
         {
-            ParseBlockTest("<foo><text><bar></bar></foo>");
+            ParseDocumentTest("@{<foo><text><bar></bar></foo>}");
         }
 
         [Fact]
         public void StopsParsingMidEmptyTagIfEOFReached()
         {
-            ParseBlockTest("<br/");
+            ParseDocumentTest("@{<br/}");
         }
 
         [Fact]
         public void CorrectlyHandlesSingleLineOfMarkupWithEmbeddedStatement()
         {
-            ParseBlockTest("<div>Foo @if(true) {} Bar</div>");
+            ParseDocumentTest("@{<div>Foo @if(true) {} Bar</div>}");
         }
 
         [Fact]
