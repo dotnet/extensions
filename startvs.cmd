15745cc5f939e085a8c0e4ad8a5f72fc7fd17f4b
Nate McMaster <nate.mcmaster@microsoft.com>
2019-01-29 15:14:01 -0800

    Update documentation, and put workarounds into a common file

---
+++ startvs.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+:: This command launches a Visual Studio solution with environment variables required to use a local version of the .NET Core SDK.
+
+:: This tells .NET Core to use .dotnet\dotnet.exe
+SET DOTNET_ROOT=%~dp0.dotnet\
+
+:: This tells .NET Core not to go looking for .NET Core in other places
+SET DOTNET_MULTILEVEL_LOOKUP=0
+
+:: Put our local dotnet.exe on PATH first so Visual Studio knows which one to use
+SET PATH=%DOTNET_ROOT%;%PATH%
+
+SET sln=%1
+
+IF NOT EXIST "%DOTNET_ROOT%\dotnet.exe" (
+    restore.cmd
+)
+
+start %~dp0Extensions.sln
