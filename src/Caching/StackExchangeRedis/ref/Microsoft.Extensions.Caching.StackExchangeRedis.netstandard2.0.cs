e7332cbc938cdee8a76d513bc2ee7258c553cc5e
Pavel Krymets <pavel@krymets.com>
2019-02-08 16:35:42 -0800

    Headers

--- src/Caching/StackExchangeRedis/ref/Microsoft.Extensions.Caching.StackExchangeRedis.netstandard2.0.cs
+++ src/Caching/StackExchangeRedis/ref/Microsoft.Extensions.Caching.StackExchangeRedis.netstandard2.0.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace Microsoft.Extensions.Caching.StackExchangeRedis
 {
     public partial class RedisCache : Microsoft.Extensions.Caching.Distributed.IDistributedCache, System.IDisposable
--- src/FileProviders/Abstractions/ref/Microsoft.Extensions.FileProviders.Abstractions.netstandard2.0.cs
+++ src/FileProviders/Abstractions/ref/Microsoft.Extensions.FileProviders.Abstractions.netstandard2.0.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace Microsoft.Extensions.FileProviders
 {
     public partial interface IDirectoryContents : System.Collections.Generic.IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo>, System.Collections.IEnumerable
