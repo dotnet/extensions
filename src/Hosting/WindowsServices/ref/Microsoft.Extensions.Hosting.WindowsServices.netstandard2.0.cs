43435a80d007d568aa17d891ea32ca87363716fc
Chris Ross <Tratcher@Outlook.com>
2019-02-13 14:05:48 -0800

    Add Hosting.WindowsServices package #809 (#1083)

---
+++ src/Hosting/WindowsServices/ref/Microsoft.Extensions.Hosting.WindowsServices.netstandard2.0.cs
@@ -0,0 +1,22 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static partial class ServiceBaseLifetimeHostBuilderExtensions
+    {
+        public static Microsoft.Extensions.Hosting.IHostBuilder UseServiceBaseLifetime(this Microsoft.Extensions.Hosting.IHostBuilder hostBuilder) { throw null; }
+    }
+}
+namespace Microsoft.Extensions.Hosting.WindowsServices
+{
+    public partial class ServiceBaseLifetime : System.ServiceProcess.ServiceBase, Microsoft.Extensions.Hosting.IHostLifetime
+    {
+        public ServiceBaseLifetime(Microsoft.Extensions.Hosting.IHostingEnvironment environment, Microsoft.Extensions.Hosting.IApplicationLifetime applicationLifetime, Microsoft.Extensions.Logging.ILoggerFactory loggerFactory) { }
+        public Microsoft.Extensions.Hosting.IHostingEnvironment Environment { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } }
+        protected override void OnStart(string[] args) { }
+        protected override void OnStop() { }
+        public System.Threading.Tasks.Task StopAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
+        public System.Threading.Tasks.Task WaitForStartAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
+    }
+}
