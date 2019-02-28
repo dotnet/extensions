90fb93a95ca7b39a3d54d1d8ef80a54fe1e2178a
Jonathan Sheely <jsheely@twentytech.net>
2019-02-28 16:03:10 -0500

    Added request to context so you find the RequestUri when logging onRetry attempts

--- src/HttpClientFactory/Polly/src/PolicyHttpMessageHandler.cs
+++ src/HttpClientFactory/Polly/src/PolicyHttpMessageHandler.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -119,6 +119,7 @@ namespace Microsoft.Extensions.Http
             if (context == null)
             {
                 context = new Context();
+                context.Add("request", request);
                 request.SetPolicyExecutionContext(context);
                 cleanUpContext = true;
             }
