3885348eadb81f0c4f2c3fbb7beccbc2c192fe92
HobbyProjects <HobbyProjects@users.noreply.github.com>
2019-03-05 10:25:27 -0800

    Provide a reloadOnChange setting to AddAzureKeyVault (#1054)

--- src/Configuration/Config.AzureKeyVault/ref/Microsoft.Extensions.Configuration.AzureKeyVault.netstandard2.0.cs
+++ src/Configuration/Config.AzureKeyVault/ref/Microsoft.Extensions.Configuration.AzureKeyVault.netstandard2.0.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Extensions.Configuration
 {
     public static partial class AzureKeyVaultConfigurationExtensions
     {
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, Microsoft.Extensions.Configuration.AzureKeyVault.AzureKeyVaultConfigurationOptions options) { throw null; }
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, string vault) { throw null; }
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, string vault, Microsoft.Azure.KeyVault.KeyVaultClient client, Microsoft.Extensions.Configuration.AzureKeyVault.IKeyVaultSecretManager manager) { throw null; }
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, string vault, Microsoft.Extensions.Configuration.AzureKeyVault.IKeyVaultSecretManager manager) { throw null; }
@@ -16,6 +17,17 @@ namespace Microsoft.Extensions.Configuration
 }
 namespace Microsoft.Extensions.Configuration.AzureKeyVault
 {
+    public partial class AzureKeyVaultConfigurationOptions
+    {
+        public AzureKeyVaultConfigurationOptions() { }
+        public AzureKeyVaultConfigurationOptions(string vault) { }
+        public AzureKeyVaultConfigurationOptions(string vault, string clientId, System.Security.Cryptography.X509Certificates.X509Certificate2 certificate) { }
+        public AzureKeyVaultConfigurationOptions(string vault, string clientId, string clientSecret) { }
+        public Microsoft.Azure.KeyVault.KeyVaultClient Client { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public Microsoft.Extensions.Configuration.AzureKeyVault.IKeyVaultSecretManager Manager { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public System.TimeSpan? ReloadInterval { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public string Vault { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+    }
     public partial class DefaultKeyVaultSecretManager : Microsoft.Extensions.Configuration.AzureKeyVault.IKeyVaultSecretManager
     {
         public DefaultKeyVaultSecretManager() { }
