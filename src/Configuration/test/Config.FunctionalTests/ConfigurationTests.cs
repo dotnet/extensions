2ece3a4c659d53fb5ffd14076e5b23ad75408f2a
Hao Kung <HaoK@users.noreply.github.com>
2019-04-05 11:01:24 -0700

    Update ConfigurationTests.cs

--- src/Configuration/test/Config.FunctionalTests/ConfigurationTests.cs
+++ src/Configuration/test/Config.FunctionalTests/ConfigurationTests.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.Configuration.Ini;
 using Microsoft.Extensions.Configuration.Json;
