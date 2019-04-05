fdcf78cf3993bf8f4aa55cae44f8a79e732bcf85
Hiral Desai <hidesai@microsoft.com>
2019-04-04 17:40:22 -0700

    Adding refs

---
+++ src/Configuration/Config.AzureTableStorage/ref/Microsoft.Extensions.Configuration.AzureTableStorage.netstandard2.0.cs
@@ -0,0 +1,10 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static partial class AzureTableStorageConfigurationExtensions
+    {
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureTableStorage(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, string connectionString, string tableName = null, string partitionKey = null) { throw null; }
+    }
+}
