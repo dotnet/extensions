d319735d0572b4a9cf99ed9de91c0e9b1d03feb9
Tom Deseyn <tom.deseyn@gmail.com>
2019-06-06 22:16:07 +0200

    Update ref assembly

--- src/Hosting/Systemd/ref/Microsoft.Extensions.Hosting.Systemd.netstandard2.1.cs
+++ src/Hosting/Systemd/ref/Microsoft.Extensions.Hosting.Systemd.netstandard2.1.cs
@@ -13,32 +13,20 @@ namespace Microsoft.Extensions.Hosting.Systemd
     public partial interface ISystemdNotifier
     {
         bool IsEnabled { get; }
-        void Notify(Microsoft.Extensions.Hosting.Systemd.ServiceState state, params Microsoft.Extensions.Hosting.Systemd.ServiceState[] states);
-    }
-    public static partial class ServiceHelpers
-    {
-        public static bool IsSystemdService () { throw null; }
+        void Notify(Microsoft.Extensions.Hosting.Systemd.ServiceState state);
     }
     [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
     public partial struct ServiceState
     {
         private object _dummy;
+        public static readonly Microsoft.Extensions.Hosting.Systemd.ServiceState Ready;
+        public static readonly Microsoft.Extensions.Hosting.Systemd.ServiceState Stopping;
         public ServiceState(string state) { throw null; }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Ready { get { throw null; } }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Reloading { get { throw null; } }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Stopping { get { throw null; } }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Watchdog { get { throw null; } }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState BusError(string value) { throw null; }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Errno(int value) { throw null; }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState MainPid(int value) { throw null; }
-        public static Microsoft.Extensions.Hosting.Systemd.ServiceState Status(string value) { throw null; }
         public override string ToString() { throw null; }
     }
-    public partial class SystemdNotifier : Microsoft.Extensions.Hosting.Systemd.ISystemdNotifier
+    public static partial class SystemdHelpers
     {
-        public SystemdNotifier() { }
-        public bool IsEnabled { get { throw null; } }
-        public void Notify(Microsoft.Extensions.Hosting.Systemd.ServiceState state, Microsoft.Extensions.Hosting.Systemd.ServiceState[] states) { }
+        public static bool IsSystemdService() { throw null; }
     }
     public partial class SystemdLifetime : Microsoft.Extensions.Hosting.IHostLifetime, System.IDisposable
     {
@@ -46,5 +34,11 @@ namespace Microsoft.Extensions.Hosting.Systemd
         public void Dispose() { }
         public System.Threading.Tasks.Task StopAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
         public System.Threading.Tasks.Task WaitForStartAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
+    }
+    public partial class SystemdNotifier : Microsoft.Extensions.Hosting.Systemd.ISystemdNotifier
+    {
+        public SystemdNotifier() { }
+        public bool IsEnabled { get { throw null; } }
+        public void Notify(Microsoft.Extensions.Hosting.Systemd.ServiceState state) { }
     }
 }
