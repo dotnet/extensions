a1704360b392a8f08fc5d5cecd31490522d54dfe
Marcell Toth <marcell@marcelltoth.net>
2019-06-09 15:19:10 +0200

    Remove unnecessary using

--- src/Primitives/src/StringValues.cs
+++ src/Primitives/src/StringValues.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Internal;
 
