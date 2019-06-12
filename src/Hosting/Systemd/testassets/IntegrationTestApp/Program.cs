f7503dd3bcbe523c4f8cefdbae0e0b2c0072f794
Tom Deseyn <tom.deseyn@gmail.com>
2019-06-12 17:10:43 +0200

    PR feedback

--- src/Hosting/Systemd/testassets/IntegrationTestApp/Program.cs
+++ src/Hosting/Systemd/testassets/IntegrationTestApp/Program.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,10 +20,6 @@ namespace IntegrationTestApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSystemd()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .UseSystemd();
     }
 }
