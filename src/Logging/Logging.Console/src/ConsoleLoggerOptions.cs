5b706d22a88850062c8e6414beee8c885b22d1bf
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-06-27 11:49:34 +0200

    Provide capability to use UTC time notation for console logs
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Logging/Logging.Console/src/ConsoleLoggerOptions.cs
+++ src/Logging/Logging.Console/src/ConsoleLoggerOptions.cs
@@ -37,5 +37,10 @@ namespace Microsoft.Extensions.Logging.Console
         /// Gets or sets format string used to format timestamp in logging messages. Defaults to <c>null</c>.
         /// </summary>
         public string TimestampFormat { get; set; }
+
+        /// <summary>
+        /// Gets or sets indication whether or not UTC timezone should be used to for timestamps in logging messages. Defaults to <c>false</c>.
+        /// </summary>
+        public bool UseUtcTimezone { get; set; } = false;
     }
 }
