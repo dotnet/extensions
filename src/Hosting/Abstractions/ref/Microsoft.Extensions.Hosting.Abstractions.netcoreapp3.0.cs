702fe690cd1825e6307f3d85885256b73f7c6e8c
Grahame Horner <grahamehorner@users.noreply.github.com>
2019-07-17 22:44:13 +0100

    Update Microsoft.Extensions.Hosting.Abstractions.netcoreapp3.0.cs

--- src/Hosting/Abstractions/ref/Microsoft.Extensions.Hosting.Abstractions.netcoreapp3.0.cs
+++ src/Hosting/Abstractions/ref/Microsoft.Extensions.Hosting.Abstractions.netcoreapp3.0.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Extensions.Hosting
         public virtual System.Threading.Tasks.Task StartAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
         [System.Diagnostics.DebuggerStepThroughAttribute]
         public virtual System.Threading.Tasks.Task StopAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
+        public TaskStatus Status { get; }
     }
     [System.ObsoleteAttribute("This type is obsolete and will be removed in a future version. The recommended alternative is Microsoft.Extensions.Hosting.Environments.", false)]
     public static partial class EnvironmentName
