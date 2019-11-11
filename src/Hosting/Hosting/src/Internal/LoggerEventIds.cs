ddf541a02232909a2e34cdd765e44e134ff57034
Kahbazi <A.Kahbazi@gmail.com>
2019-11-11 20:41:04 +0330

    Update LoggerEventIds.cs

--- src/Hosting/Hosting/src/Internal/LoggerEventIds.cs
+++ src/Hosting/Hosting/src/Internal/LoggerEventIds.cs
@@ -7,13 +7,13 @@ namespace Microsoft.Extensions.Hosting.Internal
 {
     internal static class LoggerEventIds
     {
-        public static EventId Starting = new EventId(1, "Starting");
-        public static EventId Started = new EventId(2, "Started");
-        public static EventId Stopping = new EventId(3, "Stopping");
-        public static EventId Stopped = new EventId(4, "Stopped");
-        public static EventId StoppedWithException = new EventId(5, "StoppedWithException");
-        public static EventId ApplicationStartupException = new EventId(6, "ApplicationStartupException");
-        public static EventId ApplicationStoppingException = new EventId(7, "ApplicationStoppingException");
-        public static EventId ApplicationStoppedException = new EventId(8, "ApplicationStoppedException");
+        public static readonly EventId Starting = new EventId(1, "Starting");
+        public static readonly EventId Started = new EventId(2, "Started");
+        public static readonly EventId Stopping = new EventId(3, "Stopping");
+        public static readonly EventId Stopped = new EventId(4, "Stopped");
+        public static readonly EventId StoppedWithException = new EventId(5, "StoppedWithException");
+        public static readonly EventId ApplicationStartupException = new EventId(6, "ApplicationStartupException");
+        public static readonly EventId ApplicationStoppingException = new EventId(7, "ApplicationStoppingException");
+        public static readonly EventId ApplicationStoppedException = new EventId(8, "ApplicationStoppedException");
     }
 }
