2bf3b80ca2bdf7f539d2adfbb3a2e94aceec37c3
N. Taylor Mullen <nimullen@microsoft.com>
2019-12-02 14:17:15 -0800

    Add grammar support for the model directive.
    - Ensured the new model directive grammar applied scopes that were compatible with the pre-existing Razor grammar.
    - Add tests to validate the various forms of `@model`.
    
    aspnet/AspNetCore#14287

--- src/Razor/test/Microsoft.AspNetCore.Razor.VSCode.Grammar.Test/tests/GrammarTests.test.ts
+++ src/Razor/test/Microsoft.AspNetCore.Razor.VSCode.Grammar.Test/tests/GrammarTests.test.ts
@@ -9,6 +9,7 @@ import { RunCodeDirectiveSuite } from './CodeDirective';
 import { RunExplicitExpressionSuite } from './ExplicitExpressions';
 import { RunFunctionsDirectiveSuite } from './FunctionsDirective';
 import { RunImplicitExpressionSuite } from './ImplicitExpressions';
+import { RunModelDirectiveSuite } from './ModelDirective';
 import { RunPageDirectiveSuite } from './PageDirective';
 import { RunRemoveTagHelperDirectiveSuite } from './RemoveTagHelperDirective';
 import { RunTagHelperPrefixDirectiveSuite } from './TagHelperPrefixDirective';
@@ -32,4 +33,5 @@ describe('Grammar tests', () => {
     RunAddTagHelperDirectiveSuite();
     RunRemoveTagHelperDirectiveSuite();
     RunTagHelperPrefixDirectiveSuite();
+    RunModelDirectiveSuite();
 });
