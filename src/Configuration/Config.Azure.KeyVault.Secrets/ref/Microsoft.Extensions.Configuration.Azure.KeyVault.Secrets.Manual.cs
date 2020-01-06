07584151d56621e1398c13fe0c43596036ea2ec9
Brennan Conroy <brecon@microsoft.com>
2020-01-06 13:15:45 -0800

    global

--- src/Configuration/Config.Azure.KeyVault.Secrets/ref/Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.Manual.cs
+++ src/Configuration/Config.Azure.KeyVault.Secrets/ref/Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.Manual.cs
@@ -5,26 +5,26 @@ namespace Microsoft.Extensions.Configuration
 {
     public static partial class AzureKeyVaultConfigurationExtensions
     {
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, Azure.Security.KeyVault.Secrets.SecretClient client, Microsoft.Extensions.Configuration.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault, Azure.Core.TokenCredential credential) { throw null; }
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault, Azure.Core.TokenCredential credential, Microsoft.Extensions.Configuration.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::Azure.Security.KeyVault.Secrets.SecretClient client, Microsoft.Extensions.Configuration.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault, global::Azure.Core.TokenCredential credential) { throw null; }
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault, global::Azure.Core.TokenCredential credential, Microsoft.Extensions.Configuration.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
     }
 }
 namespace Microsoft.Extensions.Configuration.KeyVault.Secrets
 {
     public partial class AzureKeyVaultConfigurationOptions
     {
-        public AzureKeyVaultConfigurationOptions(System.Uri vault, Azure.Core.TokenCredential credential) { }
-        public Azure.Security.KeyVault.Secrets.SecretClient Client { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public AzureKeyVaultConfigurationOptions(System.Uri vault, global::Azure.Core.TokenCredential credential) { }
+        public global::Azure.Security.KeyVault.Secrets.SecretClient Client { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
     }
     public partial class DefaultKeyVaultSecretManager : Microsoft.Extensions.Configuration.KeyVault.Secrets.IKeyVaultSecretManager
     {
-        public virtual string GetKey(Azure.Security.KeyVault.Secrets.KeyVaultSecret secret) { throw null; }
-        public virtual bool Load(Azure.Security.KeyVault.Secrets.SecretProperties secret) { throw null; }
+        public virtual string GetKey(global::Azure.Security.KeyVault.Secrets.KeyVaultSecret secret) { throw null; }
+        public virtual bool Load(global::Azure.Security.KeyVault.Secrets.SecretProperties secret) { throw null; }
     }
     public partial interface IKeyVaultSecretManager
     {
-        string GetKey(Azure.Security.KeyVault.Secrets.KeyVaultSecret secret);
-        bool Load(Azure.Security.KeyVault.Secrets.SecretProperties secret);
+        string GetKey(global::Azure.Security.KeyVault.Secrets.KeyVaultSecret secret);
+        bool Load(global::Azure.Security.KeyVault.Secrets.SecretProperties secret);
     }
 }
