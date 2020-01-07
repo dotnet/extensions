e5c0e25e0adf474063b1c2f34351437351c5b7bf
Brennan Conroy <brecon@microsoft.com>
2020-01-07 15:20:53 -0800

    alternative global

--- src/Configuration/Config.Azure.KeyVault.Secrets/ref/Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.netstandard2.0.cs
+++ src/Configuration/Config.Azure.KeyVault.Secrets/ref/Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.netstandard2.0.cs
@@ -5,9 +5,12 @@ namespace Microsoft.Extensions.Configuration
 {
     public static partial class AzureKeyVaultConfigurationExtensions
     {
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.AzureKeyVaultConfigurationOptions options) { throw null; }
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault) { throw null; }
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, System.Uri vault, Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::Azure.Security.KeyVault.Secrets.SecretClient client, global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.AzureKeyVaultConfigurationOptions options) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::System.Uri vault) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::System.Uri vault, global::Azure.Core.TokenCredential credential) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::System.Uri vault, global::Azure.Core.TokenCredential credential, global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
+        public static global::Microsoft.Extensions.Configuration.IConfigurationBuilder AddAzureKeyVault(this global::Microsoft.Extensions.Configuration.IConfigurationBuilder configurationBuilder, global::System.Uri vault, global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager manager) { throw null; }
     }
 }
 namespace Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets
@@ -15,11 +18,20 @@ namespace Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets
     public partial class AzureKeyVaultConfigurationOptions
     {
         public AzureKeyVaultConfigurationOptions() { }
-        public Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager Manager { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
-        public System.TimeSpan? ReloadInterval { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public AzureKeyVaultConfigurationOptions(global::System.Uri vault, global::Azure.Core.TokenCredential credential) { }
+        public global::Azure.Security.KeyVault.Secrets.SecretClient Client { [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager Manager { [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
+        public global::System.TimeSpan? ReloadInterval { [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
     }
-    public partial class DefaultKeyVaultSecretManager
+    public partial class DefaultKeyVaultSecretManager : global::Microsoft.Extensions.Configuration.Azure.KeyVault.Secrets.IKeyVaultSecretManager
     {
         public DefaultKeyVaultSecretManager() { }
+        public virtual string GetKey(global::Azure.Security.KeyVault.Secrets.KeyVaultSecret secret) { throw null; }
+        public virtual bool Load(global::Azure.Security.KeyVault.Secrets.SecretProperties secret) { throw null; }
+    }
+    public partial interface IKeyVaultSecretManager
+    {
+        string GetKey(global::Azure.Security.KeyVault.Secrets.KeyVaultSecret secret);
+        bool Load(global::Azure.Security.KeyVault.Secrets.SecretProperties secret);
     }
 }
