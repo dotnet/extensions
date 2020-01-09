0dad311b53066673ff71232edc141905214d4709
N. Taylor Mullen <nimullen@microsoft.com>
2020-01-09 13:07:27 -0800

    Add feature switch to use our new Razor LSP editor.
    - Added a new Razor LSP content type to identify our new Razor editor experience.
    - Given that there are already content type associations for the .cshtml/.razor file paths I had to build a RazorEditorFactory that takes precedence over WTE's editor factories and if our feature flags are `true` to change the content type on created editors.
    - Added a `README.md` to the LanguageServerClient project to indicate how to use the new Razor LSP editor (no functionality currently).
    - For a feature flag I took a two pronged approach of allowing environment variables or VS' first class feature flag support. We haven't yet made the changes to Visual Studio necessary to surface our feature flag as a properly settable feature flag. Once that is done our feature flag will be toggable via the `Enable Preview Features` dialog.
    - Didn't feel there was a ton of value in adding tests for our EditorFactory since it acts on things like environment variables and internal VS feature flag functionalities.
    
    aspnet/AspNetCore#17787

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorLSPContentTypeDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    internal sealed class RazorLSPContentTypeDefinition
+    {
+        public const string Name = "RazorLSP";
+        public const string CSHTMLFileExtension = ".cshtml";
+        public const string RazorFileExtension = ".razor";
+
+        /// <summary>
+        /// Exports the Razor LSP content type
+        /// </summary>
+        [Export]
+        [Name(Name)]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        public ContentTypeDefinition RazorLSPContentType { get; set; }
+
+        // We can't assocaite the Razor LSP content type with the above file extensions because there's already a content type
+        // associated with them. Instead, we utilize our RazorEditorFactory to assign the RazorLSPContentType to .razor/.cshtml
+        // files.
+    }
+}
--- src/Razor/src/Microsoft.VisualStudio.RazorExtension/RazorPackage.cs
+++ src/Razor/src/Microsoft.VisualStudio.RazorExtension/RazorPackage.cs
@@ -4,21 +4,32 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using Microsoft.VisualStudio.LanguageServerClient.Razor;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.VisualStudio.RazorExtension
 {
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    // We attach to the 51st priority order because the traditional Web + XML editors have priority 50. We need to be loaded prior to them
+    // since we want to have the option to own the experience for Razor files
+    [ProvideEditorExtension(typeof(RazorEditorFactory), RazorLSPContentTypeDefinition.CSHTMLFileExtension, 52, NameResourceID = 101)]
+    [ProvideEditorExtension(typeof(RazorEditorFactory), RazorLSPContentTypeDefinition.RazorFileExtension, 52, NameResourceID = 101)]
+    [ProvideEditorFactory(typeof(RazorEditorFactory), 101)]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
     [AboutDialogInfo(PackageGuidString, "ASP.NET Core Razor Language Services", "#110", "#112", IconResourceID = "#400")]
     [Guid(PackageGuidString)]
     public sealed class RazorPackage : AsyncPackage
     {
         public const string PackageGuidString = "13b72f58-279e-49e0-a56d-296be02f0805";
 
+        private RazorEditorFactory _editorFactory;
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            _editorFactory = new RazorEditorFactory(this);
+            RegisterEditorFactory(_editorFactory);
         }
     }
 }
