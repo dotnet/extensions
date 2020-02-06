28dd096536a5c2dac086d3619f5d46248bbd60d5
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-02-05 16:13:48 -0800

    Missed a spot

--- src/TestingUtils/Microsoft.AspNetCore.Testing/test/MaximumOSVersionTest.cs
+++ src/TestingUtils/Microsoft.AspNetCore.Testing/test/MaximumOSVersionTest.cs
@@ -56,7 +56,7 @@ namespace Microsoft.AspNetCore.Testing
             Assert.NotNull(versionKey);
             var currentVersion = (string)versionKey.GetValue("CurrentBuildNumber");
             Assert.NotNull(currentVersion);
-            Assert.True(18363 >= int.Parse(currentVersion), $"Unexpected build number {currentVersion} on {Environment.OSVersion}.");
+            Assert.True(18363 >= int.Parse(currentVersion), $"Unexpected build number {currentVersion} on {Environment.OSVersion.Version}.");
         }
     }
 
