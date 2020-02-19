ce4ba9bcb869d4f3b7163b2ded096c88443f25dc
N. Taylor Mullen <nimullen@microsoft.com>
2020-02-18 23:06:25 -0800

    Add tests for the VS LSP document manager.
    - This changeset adds the Razor LanguageServerClient testing project as well as some tests for the VS document  manager APIs.
    - Inherited the `src` LanguageServerClient's ruleset to avoid unimportant VS rules from applying to our test project.
    - Added tests for https://github.com/dotnet/aspnetcore-tooling/pull/1602
    
    dotnet/aspnetcore#17791
    dotnet/aspnetcore#17748

---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/DefaultFileUriProviderTest.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.Text;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    public class DefaultFileUriProviderTest
+    {
+        [Fact]
+        public void GetOrCreate_NoTextDocument_Creates()
+        {
+            // Arrange
+            var uriProvider = new DefaultFileUriProvider(Mock.Of<ITextDocumentFactoryService>());
+            var textBuffer = Mock.Of<ITextBuffer>();
+
+            // Act
+            var uri = uriProvider.GetOrCreate(textBuffer);
+
+            // Assert
+            Assert.NotNull(uri);
+        }
+
+        [Fact]
+        public void GetOrCreate_TurnsTextDocumentFilePathIntoUri()
+        {
+            // Arrange
+            var factory = new Mock<ITextDocumentFactoryService>();
+            var textBuffer = Mock.Of<ITextBuffer>();
+            var expectedFilePath = "C:/path/to/file.razor";
+            var textDocument = Mock.Of<ITextDocument>(document => document.FilePath == expectedFilePath);
+            factory.Setup(f => f.TryGetTextDocument(textBuffer, out textDocument))
+                .Returns(true);
+            var uriProvider = new DefaultFileUriProvider(factory.Object);
+
+            // Act
+            var uri = uriProvider.GetOrCreate(textBuffer);
+
+            // Assert
+            Assert.Equal(expectedFilePath, uri.OriginalString);
+        }
+    }
+}
---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/RazorLSPTextViewConnectionListenerTest.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    public class RazorLSPTextViewConnectionListenerTest
+    {
+        public RazorLSPTextViewConnectionListenerTest()
+        {
+            var razorLSPContentType = Mock.Of<IContentType>(contentType => contentType.IsOfType(RazorLSPContentTypeDefinition.Name) == true);
+            RazorLSPBuffer = Mock.Of<ITextBuffer>(textBuffer => textBuffer.ContentType == razorLSPContentType);
+
+            var nonRazorLSPContentType = Mock.Of<IContentType>(contentType => contentType.IsOfType(It.IsAny<string>()) == false);
+            NonRazorLSPBuffer = Mock.Of<ITextBuffer>(textBuffer => textBuffer.ContentType == nonRazorLSPContentType);
+        }
+
+        private ITextBuffer NonRazorLSPBuffer { get; }
+
+        private ITextBuffer RazorLSPBuffer { get; }
+
+        [Fact]
+        public void SubjectBuffersConnected_NoLSPRazorBuffers_Noops()
+        {
+            // Arrange
+            var lspDocumentManager = new Mock<LSPDocumentManagerBase>(MockBehavior.Strict);
+            var listener = new RazorLSPTextViewConnectionListener(lspDocumentManager.Object);
+            var subjectBuffers = new List<ITextBuffer>() { NonRazorLSPBuffer };
+            var textView = Mock.Of<ITextView>();
+
+            // Act
+            listener.SubjectBuffersConnected(textView, ConnectionReason.TextViewLifetime, subjectBuffers);
+
+            // Assert
+            lspDocumentManager.VerifyAll();
+        }
+
+        [Fact]
+        public void SubjectBuffersConnected_TracksLSPRazorBuffers()
+        {
+            // Arrange
+            var textView = Mock.Of<ITextView>();
+            var lspDocumentManager = new Mock<LSPDocumentManagerBase>(MockBehavior.Strict);
+            lspDocumentManager.Setup(manager => manager.TrackDocumentView(RazorLSPBuffer, textView))
+                .Verifiable();
+            var listener = new RazorLSPTextViewConnectionListener(lspDocumentManager.Object);
+            var subjectBuffers = new List<ITextBuffer>() { NonRazorLSPBuffer, RazorLSPBuffer };
+
+            // Act
+            listener.SubjectBuffersConnected(textView, ConnectionReason.TextViewLifetime, subjectBuffers);
+
+            // Assert
+            lspDocumentManager.VerifyAll();
+        }
+
+        [Fact]
+        public void SubjectBuffersDisconnected_NoLSPRazorBuffers_Noops()
+        {
+            // Arrange
+            var lspDocumentManager = new Mock<LSPDocumentManagerBase>(MockBehavior.Strict);
+            var listener = new RazorLSPTextViewConnectionListener(lspDocumentManager.Object);
+            var subjectBuffers = new List<ITextBuffer>() { NonRazorLSPBuffer };
+            var textView = Mock.Of<ITextView>();
+
+            // Act
+            listener.SubjectBuffersDisconnected(textView, ConnectionReason.TextViewLifetime, subjectBuffers);
+
+            // Assert
+            lspDocumentManager.VerifyAll();
+        }
+
+        [Fact]
+        public void SubjectBuffersDisconnected_UntracksLSPRazorBuffers()
+        {
+            // Arrange
+            var textView = Mock.Of<ITextView>();
+            var lspDocumentManager = new Mock<LSPDocumentManagerBase>(MockBehavior.Strict);
+            lspDocumentManager.Setup(manager => manager.UntrackDocumentView(RazorLSPBuffer, textView))
+                .Verifiable();
+            var listener = new RazorLSPTextViewConnectionListener(lspDocumentManager.Object);
+            var subjectBuffers = new List<ITextBuffer>() { NonRazorLSPBuffer, RazorLSPBuffer };
+
+            // Act
+            listener.SubjectBuffersDisconnected(textView, ConnectionReason.TextViewLifetime, subjectBuffers);
+
+            // Assert
+            lspDocumentManager.VerifyAll();
+        }
+    }
+}
