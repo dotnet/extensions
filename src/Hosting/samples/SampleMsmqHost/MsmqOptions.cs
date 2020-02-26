cec6e75f0ce3145c76acd4c8a8c14093696a03ba
Sam Harwell <Sam.Harwell@microsoft.com>
2020-02-26 10:27:22 -0800

    Normalize all file headers to the expected Apache 2.0 license

--- src/Hosting/samples/SampleMsmqHost/MsmqOptions.cs
+++ src/Hosting/samples/SampleMsmqHost/MsmqOptions.cs
@@ -1,4 +1,7 @@
-﻿using System.Messaging;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Messaging;
 
 namespace SampleMsmqHost
 {
