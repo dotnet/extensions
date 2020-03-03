e85f6c7a1e3407a78691ddc6ee27c9ac7c28f26c
N. Taylor Mullen <nimullen@microsoft.com>
2020-03-02 23:57:47 -0800

    Add LSP document synchronizer.
    - The purpose of an LSP document synchronizer is to synchronize a virtual document and an LSP documents buffers. Given a `VirtualDocumentSnapshot` and an `LSPDocumentSnapshot` the synchronizer will wait until a new `VirtualDocumentSnapshot` becomes available that matches the original `LSPDocumentSnapshot`'s version number. This ensures that any requests done after synchronization will be for the "correct" document.
    - By default the synchronizer has a 2s timeout in waiting for virtual documents and host documents to synchronize.
    - Synchronizations are memoized, meaning if multiple synchronizations happen for the same virtual / host document combo then we will return a single task object representing the success or failure of those synchronizations.
    - I made an assumption that synchronization requests for virtual documents will always have an ever increasing host document sync version. When we get a new synchronization request for a virtual document who's host document sync version is larger than the previous sync request we will cancel older requests.
    - Added tests to validate the synchronizer functionality.
    - Added a common test virtual and lsp document.
    
    Fixes dotnet/aspnetcore#17802

---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/TestVirtualDocumentSnapshot.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    public class TestVirtualDocumentSnapshot : VirtualDocumentSnapshot
+    {
+        private long? _hostDocumentSyncVersion;
+
+        public TestVirtualDocumentSnapshot(Uri uri, int hostDocumentVersion)
+        {
+            Uri = uri;
+            _hostDocumentSyncVersion = hostDocumentVersion;
+        }
+
+        public override Uri Uri { get; }
+
+        public override ITextSnapshot Snapshot { get; }
+
+        public override long? HostDocumentSyncVersion => _hostDocumentSyncVersion;
+
+        public TestVirtualDocumentSnapshot Fork(int hostDocumentVersion) => new TestVirtualDocumentSnapshot(Uri, hostDocumentVersion);
+    }
+}
