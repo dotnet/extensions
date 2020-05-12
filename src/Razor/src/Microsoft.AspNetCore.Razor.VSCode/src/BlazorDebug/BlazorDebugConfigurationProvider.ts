76fc7a948835bd4655fa9cbe6f9d7604032e284b
Safia Abdalla <safia@microsoft.com>
2020-05-12 12:31:59 -0700

    Run Blazor app using spawn

--- src/Razor/src/Microsoft.AspNetCore.Razor.VSCode/src/BlazorDebug/BlazorDebugConfigurationProvider.ts
+++ src/Razor/src/Microsoft.AspNetCore.Razor.VSCode/src/BlazorDebug/BlazorDebugConfigurationProvider.ts
@@ -3,55 +3,55 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
+import { spawn } from 'child_process';
 import * as vscode from 'vscode';
 
 import { RazorLogger } from '../RazorLogger';
 
 export class BlazorDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
 
-    constructor(private readonly logger: RazorLogger, private readonly vscodeType: typeof vscode) {}
+    constructor(private readonly logger: RazorLogger, private readonly vscodeType: typeof vscode) { }
 
     public async resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, configuration: vscode.DebugConfiguration): Promise<vscode.DebugConfiguration | undefined> {
-        const app = {
-            name: '.NET Core Launch (Blazor Standalone)',
-            type: 'coreclr',
-            request: 'launch',
-            program: 'dotnet',
-            args: ['run'],
-            cwd: configuration.cwd,
+        const output = this.vscodeType.window.createOutputChannel('Blazor WebAssembly App');
+        const app = spawn('dotnet', ['run'], {
+            cwd: configuration.cwd || folder && folder.uri && folder.uri.fsPath,
             env: {
+                ...process.env,
                 ASPNETCORE_ENVIRONMENT: 'Development',
                 ...configuration.env,
             },
-        };
+        });
+
+        app.stdout.on('data', (data) => output.append(data.toString()));
+        app.stderr.on('data', (data) => output.append(data.toString()));
+        app.on('error', (error) => {
+            output.append(error.toString());
+            this.logger.logError('[DEBUGGER] Error when launch app: ', error);
+        });
+
+        output.show();
+
         const browser = {
             name: '.NET Core Debug Blazor Web Assembly in Browser',
             type: configuration.browser === 'edge' ? 'pwa-msedge' : 'pwa-chrome',
             request: 'launch',
-            timeout: configuration.timeout,
-            url: configuration.url,
-            webRoot: configuration.webRoot,
+            timeout: configuration.timeout || 30000,
+            url: configuration.url || 'https://localhost:5001',
+            webRoot: configuration.webRoot || '${workspaceFolder}',
             inspectUri: '{wsProtocol}://{url.hostname}:{url.port}/_framework/debug/ws-proxy?browser={browserInspectUri}',
             trace: configuration.trace || false,
         };
+
         let showErrorInfo = false;
 
         try {
-            await this.vscodeType.debug.startDebugging(folder, app);
-            try {
-                await this.vscodeType.debug.startDebugging(folder, browser);
-                this.logger.logVerbose('[DEBUGGER] Launching browser debugger...');
-            } catch (error) {
-                this.logger.logError(
-                  '[DEBUGGER] Error when launching browser debugger: ',
-                  error,
-                );
-                showErrorInfo = true;
-            }
+            await this.vscodeType.debug.startDebugging(folder, browser);
+            this.logger.logVerbose('[DEBUGGER] Launching browser debugger...');
         } catch (error) {
             this.logger.logError(
-              '[DEBUGGER] Error when launching application: ',
-              error,
+                '[DEBUGGER] Error when launching browser debugger: ',
+                error,
             );
             showErrorInfo = true;
         }
