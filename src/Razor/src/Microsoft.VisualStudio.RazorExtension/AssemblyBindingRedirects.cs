2270bf9fa1c65523efc8de84ab8d2a421d42ded3
N. Taylor Mullen <nimullen@microsoft.com>
2020-06-03 13:43:02 -0700

    Fix VSIX insertion binaries.
    - When we migrated from project references to package references for the Razor compiler dlls stopped being published as part of our VSIX insertion. Previously when using project references the VSIXSourceItem + binding redirect auto-generation was inferred.

---
+++ src/Razor/src/Microsoft.VisualStudio.RazorExtension/AssemblyBindingRedirects.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.AspNetCore.Mvc.Razor.Extensions",
+    GenerateCodeBase = true,
+    PublicKeyToken = "adb9793829ddae60",
+    OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "5.0.0.0",
+    NewVersion = "5.0.0.0")]
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.AspNetCore.Mvc.Razor.Extensions.Version1_X",
+    GenerateCodeBase = true,
+    PublicKeyToken = "adb9793829ddae60",
+    OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "5.0.0.0",
+    NewVersion = "5.0.0.0")]
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.AspNetCore.Mvc.Razor.Extensions.Version2_X",
+    GenerateCodeBase = true,
+    PublicKeyToken = "adb9793829ddae60",
+    OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "5.0.0.0",
+    NewVersion = "5.0.0.0")]
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.AspNetCore.Razor.Language",
+    GenerateCodeBase = true,
+    PublicKeyToken = "adb9793829ddae60",
+    OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "5.0.0.0",
+    NewVersion = "5.0.0.0")]
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.CodeAnalysis.Razor",
+    GenerateCodeBase = true,
+    PublicKeyToken = "adb9793829ddae60",
+    OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "5.0.0.0",
+    NewVersion = "5.0.0.0")]
