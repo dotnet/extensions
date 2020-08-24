0a019975150b8fecc14823623563acc60443d10c
N. Taylor Mullen <nimullen@microsoft.com>
2020-08-24 09:14:26 -0700

    Revert "Revert "Log language server output to files (also)""
    This reverts commit 5ff2d0ab409f2e275b6eb7c78f06bc3f057dee31.

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Feedback/FeedbackFileLoggerProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor.Feedback
+{
+    internal abstract class FeedbackFileLoggerProviderFactory
+    {
+        /// <summary>
+        /// Returns a <see cref="FeedbackFileLoggerProvider"/>. This is an optimization to ensure we don't load an extra logging dll at MEF load time in Visual Studio.
+        /// </summary>
+        /// <returns>A created <see cref="FeedbackFileLoggerProvider"/>.</returns>
+        public abstract object GetOrCreate();
+    }
+}
