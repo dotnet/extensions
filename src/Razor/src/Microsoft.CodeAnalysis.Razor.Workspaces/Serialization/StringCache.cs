bb4dc96f953221fa76b33b8477cd653ef2c0524d
Ryan Brandenburg <rybrande@microsoft.com>
2021-01-04 16:37:27 -0800

    PR feedback on StringCache (#2871)
    PR feedback on StringCache

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Serialization/StringCache.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Serialization/StringCache.cs
@@ -57,10 +57,7 @@ namespace Microsoft.CodeAnalysis.Razor.Serialization.Internal
 
         private void Cleanup()
         {
-            _hashSet.RemoveWhere((weakRef) =>
-            {
-                return !weakRef.IsAlive;
-            });
+            _hashSet.RemoveWhere(weakRef => !weakRef.IsAlive);
         }
 
         private bool TryGetValue(string key, out string? value)
@@ -69,7 +66,7 @@ namespace Microsoft.CodeAnalysis.Razor.Serialization.Internal
             {
                 var exfiltrator = (ExfiltratingEqualityComparer)_hashSet.Comparer;
                 var val = exfiltrator.LastEqualValue!;
-                if (val.TryGetTarget(out var target))
+                if (val.Value.TryGetTarget(out var target))
                 {
                     value = target!;
                     return true;
@@ -110,24 +107,21 @@ namespace Microsoft.CodeAnalysis.Razor.Serialization.Internal
                 }
             }
 
-            public int GetHashCode(Entry obj)
-            {
-                return obj.TargetHashCode;
-            }
+            public int GetHashCode(Entry obj) => obj.GetHashCode();
         }
 
-        private class Entry
+        private struct Entry
         {
             // In order to use HashSet we need a stable HashCode, so we have to cache it as soon as it comes in.
             // If the HashCode is unstable then entries in the HashSet become unreachable/unremovable.
-            public readonly int TargetHashCode;
+            private readonly int _targetHashCode;
 
             private readonly WeakReference<string> _weakRef;
 
             public Entry(string target)
             {
                 _weakRef = new WeakReference<string>(target);
-                TargetHashCode = target.GetHashCode();
+                _targetHashCode = target.GetHashCode();
             }
 
             public bool IsAlive => _weakRef.TryGetTarget(out _);
@@ -146,16 +140,17 @@ namespace Microsoft.CodeAnalysis.Razor.Serialization.Internal
 
                 if (TryGetTarget(out var thisTarget) && entry.TryGetTarget(out var entryTarget))
                 {
-                    return thisTarget!.Equals(entryTarget, StringComparison.Ordinal);
+                    return thisTarget!.GetHashCode().Equals(entryTarget.GetHashCode()) && thisTarget!.Equals(entryTarget, StringComparison.Ordinal);
                 }
 
                 // We lost the reference, but we need to check RefEquals to ensure that HashSet can successfully Remove items.
-                return ReferenceEquals(this, obj);
+                // We can't compare the Entries themselves because as structs they would get Value-Boxed an RefEquals would always be false.
+                return ReferenceEquals(_weakRef, entry._weakRef);
             }
 
             public override int GetHashCode()
             {
-                return TargetHashCode;
+                return _targetHashCode;
             }
         }
     }
