a290f20dde2529043957fcfe779e104aaa90815d
Chris R <Tratcher@outlook.com>
2023-05-26 14:11:41 -0700

    More specific error handling

--- src/Shared/AzureSync/CopyToDotnet.cmd
+++ src/Shared/AzureSync/CopyToDotnet.cmd
@@ -11,8 +11,11 @@ IF [%remote_repo%] == [] (
 echo DOTNETEXTENSIONS_REPO: %remote_repo%
 
 REM https://superuser.com/questions/280425/getting-robocopy-to-return-a-proper-exit-code
-(robocopy ..\Data.Validation\ %remote_repo%\src\Shared\Data.Validation\ /MIR) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
-(robocopy ..\DotNetSync\ %remote_repo%\src\Shared\AzureSync\ /MIR) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
-(robocopy ..\EmptyCollections\ %remote_repo%\src\Shared\EmptyCollections\ /MIR) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
-(robocopy ..\NumericExtensions\ %remote_repo%\src\Shared\NumericExtensions\ /MIR) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
-(robocopy ..\Throw\ %remote_repo%\src\Shared\Throw\ /MIR) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+IF %ERRORLEVEL% LSS 8 (robocopy ..\DotNetSync\ %remote_repo%\src\Shared\AzureSync\ /MIR)
+IF %ERRORLEVEL% LSS 8 (robocopy ..\Data.Validation\ %remote_repo%\src\Shared\Data.Validation\ /MIR)
+IF %ERRORLEVEL% LSS 8 (robocopy ..\EmptyCollections\ %remote_repo%\src\Shared\EmptyCollections\ /MIR)
+IF %ERRORLEVEL% LSS 8 (robocopy ..\NumericExtensions\ %remote_repo%\src\Shared\NumericExtensions\ /MIR)
+IF %ERRORLEVEL% LSS 8 (robocopy ..\Throw\ %remote_repo%\src\Shared\Throw\ /MIR)
+
+rem 0x0-4 mean everythings good, files may have been copied. 0x08-10 mean are actual errors.
+IF %ERRORLEVEL% LSS 8 (exit /b 0) ELSE (exit /b 1)
