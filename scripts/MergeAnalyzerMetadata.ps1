12982be1667fc62d74df18b6f3f998b836423b9e
Igor Velikorossov <igor.velikorossov@microsoft.com>
2023-06-06 14:23:34 +1000

    Build on Linux

--- scripts/MergeAnalyzerMetadata.ps1
+++ scripts/MergeAnalyzerMetadata.ps1
@@ -4,5 +4,12 @@
     Builds and invokes the DiagConfig tool to extract metadata from analyzers and update our diagnostic config state accordingly.
 #>
 
-Set-Location $PSScriptRoot\..
-.\build.cmd -restore -build -projects .\eng\Tools\DiagConfig\DiagConfig.csproj -bl
+
+Set-Location $PSScriptRoot/..
+
+if ($IsWindows) {
+    .\build.cmd -restore -build -projects .\eng\Tools\DiagConfig\DiagConfig.csproj -bl
+}
+else {
+    ./build.sh --restore --build --projects ./eng/Tools/DiagConfig/DiagConfig.csproj --bl
+}
