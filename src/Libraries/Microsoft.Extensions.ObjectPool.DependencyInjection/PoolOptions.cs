001f2764cbd1518cf26cc3d975a233c1a8f23bbf
Sebastien Ros <sebastienros@gmail.com>
2023-06-09 15:44:21 -0700

    Using constant for DefaultCapacity

--- src/Libraries/Microsoft.Extensions.ObjectPool.DependencyInjection/PoolOptions.cs
+++ src/Libraries/Microsoft.Extensions.ObjectPool.DependencyInjection/PoolOptions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.Extensions.ObjectPool;
@@ -12,11 +11,11 @@ namespace Microsoft.Extensions.ObjectPool;
 [Experimental]
 public sealed class PoolOptions
 {
-    internal static readonly int DefaultCapacity = Environment.ProcessorCount * 2;
+    internal const int DefaultCapacity = 1024;
 
     /// <summary>
     /// Gets or sets the maximal capacity of the pool.
     /// </summary>
-    /// <remarks>The default is <c>Environment.ProcessorCount * 2</c>.</remarks>
+    /// <remarks>The default is 1024.</remarks>
     public int Capacity { get; set; } = DefaultCapacity;
 }
