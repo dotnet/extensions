53bc8acc407599d1415d925df7e94089978bce45
Martin Tomka <martintomka@microsoft.com>
2023-06-26 14:05:17 +0200

    FIxes

---
+++ test/Libraries/Microsoft.Extensions.Http.Resilience.Tests/Routing/EndpointTests.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Extensions.Http.Resilience.Test.Routing;
+
+public class EndpointTests
+{
+    [Fact]
+    public void Uri_OK()
+    {
+        var endpoint = new Endpoint
+        {
+            Uri = new Uri("https://localhost:5001")
+        };
+
+        endpoint.Uri.Should().Be(new Uri("https://localhost:5001"));
+    }
+}
