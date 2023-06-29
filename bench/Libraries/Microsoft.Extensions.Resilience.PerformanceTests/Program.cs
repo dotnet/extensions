f12f02b68fd53974d3e4e6646197873ba724bbb8
Martin Tomka <martintomka@microsoft.com>
2023-06-29 12:21:49 +0200

    Build fix

--- bench/Libraries/Microsoft.Extensions.Resilience.PerformanceTests/Program.cs
+++ bench/Libraries/Microsoft.Extensions.Resilience.PerformanceTests/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Jobs;
