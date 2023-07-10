2ac803e8f25a7cd68516443bd3f1d09c5a1fed4c
martintmk <103487740+martintmk@users.noreply.github.com>
2023-07-10 08:35:55 +0200

    Hedging now works without routing configuration (#4144)

--- bench/Libraries/Microsoft.Extensions.Http.Resilience.PerformanceTests/HttpResilienceBenchmark.cs
+++ bench/Libraries/Microsoft.Extensions.Http.Resilience.PerformanceTests/HttpResilienceBenchmark.cs
@@ -10,14 +10,17 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Extensions.Http.Resilience.Bench;
 
+#pragma warning disable R9A044 // Assign array of literal values to a static field for improved performance
+
 public class HttpResilienceBenchmark
 {
-    private static readonly Uri _uri = new("https://bogus");
+    private static readonly Uri _uri = new(HttpClientFactory.PrimaryEndpoint);
 
     private HttpClient _client = null!;
     private HttpClient _standardClient = null!;
     private HttpClient _singleHandlerClient = null!;
-    private HttpClient _hedgingClient = null!;
+    private HttpClient _hedgingClientOrdered = null!;
+    private HttpClient _hedgingClientNoRoutes = null!;
 
     private static HttpRequestMessage Request
     {
@@ -32,12 +35,13 @@ public class HttpResilienceBenchmark
     [GlobalSetup]
     public void GlobalSetup()
     {
-        var serviceProvider = HttpClientFactory.InitializeServiceProvider(HedgingClientType.Ordered);
+        var serviceProvider = HttpClientFactory.InitializeServiceProvider(HedgingClientType.Ordered, HedgingClientType.NoRoutes);
         var factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
         _client = factory.CreateClient(HttpClientFactory.EmptyClient);
         _standardClient = factory.CreateClient(HttpClientFactory.StandardClient);
         _singleHandlerClient = factory.CreateClient(HttpClientFactory.SingleHandlerClient);
-        _hedgingClient = factory.CreateClient(nameof(HedgingClientType.Ordered));
+        _hedgingClientNoRoutes = factory.CreateClient(nameof(HedgingClientType.NoRoutes));
+        _hedgingClientOrdered = factory.CreateClient(nameof(HedgingClientType.Ordered));
     }
 
     [Benchmark(Baseline = true)]
@@ -47,20 +51,26 @@ public class HttpResilienceBenchmark
     }
 
     [Benchmark]
+    public Task<HttpResponseMessage> SingleHandler()
+    {
+        return _singleHandlerClient.SendAsync(Request, CancellationToken.None);
+    }
+
+    [Benchmark]
     public Task<HttpResponseMessage> StandardResilienceHandler()
     {
         return _standardClient.SendAsync(Request, CancellationToken.None);
     }
 
     [Benchmark]
-    public Task<HttpResponseMessage> StandardHedgingHandler()
+    public Task<HttpResponseMessage> StandardHedgingHandler_RoutesFromRequest()
     {
-        return _hedgingClient.SendAsync(Request, CancellationToken.None);
+        return _hedgingClientNoRoutes.SendAsync(Request, CancellationToken.None);
     }
 
     [Benchmark]
-    public Task<HttpResponseMessage> SingleHandler()
+    public Task<HttpResponseMessage> StandardHedgingHandler_RoutesFromConfig()
     {
-        return _singleHandlerClient.SendAsync(Request, CancellationToken.None);
+        return _hedgingClientOrdered.SendAsync(Request, CancellationToken.None);
     }
 }
--- src/Libraries/Microsoft.Extensions.Http.Resilience/Routing/Internal/RequestRoutingOptions.cs
+++ src/Libraries/Microsoft.Extensions.Http.Resilience/Routing/Internal/RequestRoutingOptions.cs
@@ -2,12 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Microsoft.Extensions.Http.Resilience.Routing.Internal;
 
 internal class RequestRoutingOptions
 {
-    [Required]
     public Func<RequestRoutingStrategy>? RoutingStrategyProvider { get; set; }
 }
