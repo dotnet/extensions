f8a7a939cc834cb13b90f53a9d320b597a9236e2
Nikita Balabaev <xakep139@users.noreply.github.com>
2023-08-01 17:27:53 +0200

    Apply suggestions from code review

--- src/Libraries/Microsoft.Extensions.Telemetry.Abstractions/Logging/LoggerMessageState.EnrichmentPropertyBag.cs
+++ src/Libraries/Microsoft.Extensions.Telemetry.Abstractions/Logging/LoggerMessageState.EnrichmentPropertyBag.cs
@@ -1,11 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Compliance.Classification;
 using Microsoft.Extensions.Telemetry.Enrichment;
-using Microsoft.Shared.Diagnostics;
 
 namespace Microsoft.Extensions.Telemetry.Logging;
 
