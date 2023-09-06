b1e01ad6850de0261f60f501325099ed6b679aec
Martin Taillefer <mataille@microsoft.com>
2023-09-06 15:11:02 -0700

    Simplify IEnrichmentTagCollector definition

--- src/Libraries/Microsoft.Extensions.Telemetry.Abstractions/Logging/LogMethodHelper.cs
+++ src/Libraries/Microsoft.Extensions.Telemetry.Abstractions/Logging/LogMethodHelper.cs
@@ -177,34 +177,6 @@ public sealed class LogMethodHelper : List<KeyValuePair<string, object?>>, ITagC
         Add(new KeyValuePair<string, object?>(tagName, tagValue));
     }
 
-    /// <inheritdoc/>
-    void IEnrichmentTagCollector.Add(string tagName, string tagValue)
-    {
-        _ = Throw.IfNullOrEmpty(tagName);
-        Add(new KeyValuePair<string, object?>(tagName, tagValue));
-    }
-
-    /// <inheritdoc/>
-    void IEnrichmentTagCollector.Add(ReadOnlySpan<KeyValuePair<string, object>> tags)
-    {
-        foreach (var p in tags)
-        {
-            // we're going from KVP<string, object> to KVP<string, object?> which is strictly correct, so ignore the complaint
-#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
-            Add(p);
-#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
-        }
-    }
-
-    /// <inheritdoc/>
-    void IEnrichmentTagCollector.Add(ReadOnlySpan<KeyValuePair<string, string>> tags)
-    {
-        foreach (var p in tags)
-        {
-            Add(new KeyValuePair<string, object?>(p.Key, p.Value));
-        }
-    }
-
     /// <summary>
     /// Gets log define options configured to skip the log level enablement check.
     /// </summary>
