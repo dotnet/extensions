bd7a57c7859a8ca537f0e9750633b3ea700ed56c
Nikita Balabaev <nbalabaev@microsoft.com>
2023-10-04 18:46:03 +0200

    resilience enrichment tags

--- src/Libraries/Microsoft.Extensions.Resilience/Resilience/Internal/ResilienceTagNames.cs
+++ src/Libraries/Microsoft.Extensions.Resilience/Resilience/Internal/ResilienceTagNames.cs
@@ -5,13 +5,13 @@ namespace Microsoft.Extensions.Resilience.Internal;
 
 internal static class ResilienceTagNames
 {
-    public const string FailureSource = "failure-source";
+    public const string FailureSource = "failure.source";
 
-    public const string FailureReason = "failure-reason";
+    public const string FailureReason = "failure.reason";
 
-    public const string FailureSummary = "failure-summary";
+    public const string FailureSummary = "failure.summary";
 
-    public const string DependencyName = "dep-name";
+    public const string DependencyName = "dependency.name";
 
-    public const string RequestName = "req-name";
+    public const string RequestName = "request.name";
 }
