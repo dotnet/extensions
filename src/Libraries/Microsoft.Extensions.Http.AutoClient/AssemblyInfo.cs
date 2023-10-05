ea96d67aa8bc4069cfaa1e88aa3e237b24b295f0
Jose Perez Rodriguez <joperezr@microsoft.com>
2023-10-05 10:35:44 -0700

    Undo the changes that marked API as experimental

--- src/Libraries/Microsoft.Extensions.Http.AutoClient/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.Shared.DiagnosticIds;
-
-[assembly: Experimental(diagnosticId: Experiments.AutoClient, UrlFormat = Experiments.UrlFormat)]
