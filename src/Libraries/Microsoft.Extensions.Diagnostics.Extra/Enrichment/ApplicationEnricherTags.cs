c93c1a012a22197be5cfdef82565dda25d9a2dd5
Iliar Turdushev <iliarturdu@microsoft.com>
2023-10-10 13:46:04 +0200

    Renamed AppEnricher's dimensions

--- src/Libraries/Microsoft.Extensions.Diagnostics.Extra/Enrichment/ApplicationEnricherTags.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.Extra/Enrichment/ApplicationEnricherTags.cs
@@ -14,22 +14,22 @@ public static class ApplicationEnricherTags
     /// <summary>
     /// Application name.
     /// </summary>
-    public const string ApplicationName = "AppName";
+    public const string ApplicationName = "App.Name";
 
     /// <summary>
     /// Environment name.
     /// </summary>
-    public const string EnvironmentName = "CloudEnv";
+    public const string EnvironmentName = "Cloud.Env";
 
     /// <summary>
     /// Deployment ring.
     /// </summary>
-    public const string DeploymentRing = "CloudDeploymentRing";
+    public const string DeploymentRing = "Cloud.DeploymentRing";
 
     /// <summary>
     /// Build version.
     /// </summary>
-    public const string BuildVersion = "CloudRoleVer";
+    public const string BuildVersion = "Cloud.RoleVer";
 
     /// <summary>
     /// Gets a list of all dimension names.
