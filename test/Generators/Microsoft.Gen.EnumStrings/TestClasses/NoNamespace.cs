d447cdb9f701a597058b3297fc36fe2869e0490c
Nikita Balabaev <xakep139@users.noreply.github.com>
2023-10-13 16:58:53 +0200

    Update test/Generators/Microsoft.Gen.EnumStrings/TestClasses/NoNamespace.cs

--- test/Generators/Microsoft.Gen.EnumStrings/TestClasses/NoNamespace.cs
+++ test/Generators/Microsoft.Gen.EnumStrings/TestClasses/NoNamespace.cs
@@ -1,4 +1,5 @@
-// Copyright (c) Microsoft Corporation. All Rights Reserved.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Extensions.EnumStrings;
 
