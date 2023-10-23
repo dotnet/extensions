0d244ba9b307b2b7fc098900cd74ba27609a61b2
Nikita Balabaev <nbalabaev@microsoft.com>
2023-10-23 19:17:58 +0200

    fix test coverage

---
+++ test/Libraries/Microsoft.Extensions.Resilience.Tests/Resilience/FailureResultContextTests.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FluentAssertions;
+using Microsoft.Extensions.Http.Diagnostics;
+using Xunit;
+
+namespace Microsoft.Extensions.Resilience.Test.Resilience;
+
+public class FailureResultContextTests
+{
+    [Fact]
+    public void ContextFactory_CreateTheObject()
+    {
+        var context = FailureResultContext.Create();
+
+        context.FailureSource.Should().Be(TelemetryConstants.Unknown);
+        context.AdditionalInformation.Should().Be(TelemetryConstants.Unknown);
+        context.FailureReason.Should().Be(TelemetryConstants.Unknown);
+    }
+}
