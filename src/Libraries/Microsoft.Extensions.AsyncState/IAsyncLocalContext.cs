c3612b1b39ec877dede0250fbd003c77a115f956
Martin Obratil <maobrati@microsoft.com>
2024-01-03 22:29:52 +0100

    Remove unnecessary using directives in IAsyncLocalContext.cs

--- src/Libraries/Microsoft.Extensions.AsyncState/IAsyncLocalContext.cs
+++ src/Libraries/Microsoft.Extensions.AsyncState/IAsyncLocalContext.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.Shared.DiagnosticIds;
 
 namespace Microsoft.Extensions.AsyncState;
 
