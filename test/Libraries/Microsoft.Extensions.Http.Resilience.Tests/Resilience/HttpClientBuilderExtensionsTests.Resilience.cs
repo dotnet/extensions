f5f44b0a752b350725905b9aac93fba248b24b69
Martin Tomka <martintomka@microsoft.com>
2024-02-05 19:23:02 +0100

    PR comments

--- test/Libraries/Microsoft.Extensions.Http.Resilience.Tests/Resilience/HttpClientBuilderExtensionsTests.Resilience.cs
+++ test/Libraries/Microsoft.Extensions.Http.Resilience.Tests/Resilience/HttpClientBuilderExtensionsTests.Resilience.cs
@@ -92,6 +92,12 @@ public sealed partial class HttpClientBuilderExtensionsTests
         // add twice intentionally
         builder.AddResilienceHandler("test", ConfigureBuilder);
 
+        // We check that the count of existing services is not unnecessary increased.
+        //
+        // The additional 3 services that are registered are related to:
+        // - Configuration of HTTP client options
+        // - Configuration of resilience pipeline
+        // - Registration of keyed service for resilience pipeline
         builder.Services.Should().HaveCount(count + 3);
     }
 
