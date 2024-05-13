b635ca9c6e50e98436ba836f6230e29499dee23d
Safia Abdalla <safia@safia.rocks>
2024-05-13 20:02:44 +0000

    Fix license headers in source files

--- src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/SystemResources.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/SystemResources.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Shared.Diagnostics;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Shared.Diagnostics;
 
 namespace Microsoft.Extensions.Diagnostics.ResourceMonitoring;
 
