8af667516b9b9da760f595d17c1816f55eae6850
Nikita Balabaev <xakep139@users.noreply.github.com>
2024-06-10 16:29:37 +0200

    Fix #5207 (#5210)

--- test/Generators/Microsoft.Gen.Logging/TestClasses/NonSensitiveRecordExtensions.cs
+++ test/Generators/Microsoft.Gen.Logging/TestClasses/NonSensitiveRecordExtensions.cs
@@ -1,7 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using Microsoft.Extensions.Compliance.Classification;
 using Microsoft.Extensions.Logging;
 
-public static partial class Bug5188
+public static partial class NonSensitiveRecordExtensions
 {
     internal record User()
     {
@@ -11,6 +14,7 @@ public static partial class Bug5188
         public string? Email { get; set; }
     }
 
+    // This is specifically to address https://github.com/dotnet/extensions/issues/5188
     internal static partial class Logging
     {
         [LoggerMessage(6001, LogLevel.Information, "Logging User: {User}")]
