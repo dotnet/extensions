3486254cdacc03f764dc2679a8736b293893a9a3
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2024-07-16 11:00:03 +0200

    update copyright

--- src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/Windows/Interop/MemoryInfo.Native.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/Windows/Interop/MemoryInfo.Native.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Diagnostics.ResourceMonitoring.Windows.Interop;
