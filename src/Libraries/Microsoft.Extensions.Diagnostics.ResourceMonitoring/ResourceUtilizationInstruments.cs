932727be7e81c42dd7703faf38f36b8c2f4bae76
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2024-08-12 09:55:39 +0200

    Sort const in the order

--- src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/ResourceUtilizationInstruments.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/ResourceUtilizationInstruments.cs
@@ -15,22 +15,6 @@ namespace Microsoft.Extensions.Diagnostics.ResourceMonitoring;
 internal static class ResourceUtilizationInstruments
 {
     /// <summary>
-    /// Gets the CPU consumption share of the running process in range <c>[0, 1]</c>.
-    /// </summary>
-    /// <remarks>
-    /// The type of an instrument is <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
-    /// </remarks>
-    public const string ProcessCpuUtilization = "process.cpu.utilization";
-
-    /// <summary>
-    /// Gets the memory consumption share of the running process in range <c>[0, 1]</c>.
-    /// </summary>
-    /// <remarks>
-    /// The type of an instrument is <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
-    /// </remarks>
-    public const string ProcessMemoryUtilization = "dotnet.process.memory.virtual.utilization";
-
-    /// <summary>
     /// Gets the CPU limit consumption of all processes running inside a container or control group in range <c>[0, 1]</c>.
     /// </summary>
     /// <remarks>
@@ -53,4 +37,20 @@ internal static class ResourceUtilizationInstruments
     /// The type of an instrument is <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
     /// </remarks>
     public const string ContainerMemoryLimitUtilization = "container.memory.limit.utilization";
+
+    /// <summary>
+    /// Gets the CPU consumption share of the running process in range <c>[0, 1]</c>.
+    /// </summary>
+    /// <remarks>
+    /// The type of an instrument is <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
+    /// </remarks>
+    public const string ProcessCpuUtilization = "process.cpu.utilization";
+
+    /// <summary>
+    /// Gets the memory consumption share of the running process in range <c>[0, 1]</c>.
+    /// </summary>
+    /// <remarks>
+    /// The type of an instrument is <see cref="System.Diagnostics.Metrics.ObservableGauge{T}"/>.
+    /// </remarks>
+    public const string ProcessMemoryUtilization = "dotnet.process.memory.virtual.utilization";
 }
