8b898b24ae652c1309c1da6ae8a4f94e0b474da4
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2024-08-14 11:46:51 +0200

    update

--- src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/IResourceMonitorBuilder.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/IResourceMonitorBuilder.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,6 +22,7 @@ public interface IResourceMonitorBuilder
     /// </summary>
     /// <typeparam name="T">The publisher's implementation type.</typeparam>
     /// <returns>The value of the object instance.</returns>
+    [Obsolete("This method is obsolete. Instead of IResourceUtilizationPublisher use observable instruments from Microsoft.Extensions.Diagnostics.ResourceMonitoring.ResourceUtilizationInstruments.")]
     IResourceMonitorBuilder AddPublisher<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>()
         where T : class, IResourceUtilizationPublisher;
 }
