ed3758e5499ccbc0524cd8ab28e7fea04c54c6db
Darius Letterman <darius.letterman@microsoft.com>
2024-08-17 18:45:35 +0200

    remove using

--- src/Generators/Microsoft.Gen.Logging/Parsing/Parser.cs
+++ src/Generators/Microsoft.Gen.Logging/Parsing/Parser.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
