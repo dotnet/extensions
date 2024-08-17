3949074a2e084b68433cc2bef60af64b4b9cac48
Darius Letterman <darius.letterman@microsoft.com>
2024-08-17 20:19:18 +0200

    add generated tests for primary constructors

---
+++ test/Generators/Microsoft.Gen.Logging/Generated/LogMethodExtendedTests.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Logging.Testing;
+using TestClasses;
+using Xunit;
+
+using static TestClasses.LogPropertiesRedactionExtensions;
+
+namespace Microsoft.Gen.Logging.Test;
+
+public class LogMethodExtendedTests
+{
+    [Fact]
+    public void FindsLoggerInPrimaryConstructorParameter()
+    {
+        using var logger = Utils.GetLogger();
+
+        var collector = logger.FakeLogCollector;
+
+        new ClassWithPrimaryConstructor(logger).Test();
+        Assert.Equal("Test.", collector.LatestRecord.Message);
+    }
+
+    [Fact]
+    public void FindsLoggerInPrimaryConstructorParameterInDifferentPartialDeclaration()
+    {
+        using var logger = Utils.GetLogger();
+
+        var collector = logger.FakeLogCollector;
+
+        new ClassWithPrimaryConstructorInDifferentPartialDeclaration(logger).Test();
+        Assert.Equal("Test.", collector.LatestRecord.Message);
+    }
+
+    [Fact]
+    public void FindsLoggerInFieldInitializedFromPrimaryConstructorParameter()
+    {
+        using var logger = Utils.GetLogger();
+
+        var collector = logger.FakeLogCollector;
+
+        new ClassWithPrimaryConstructorAndField(logger).Test();
+        Assert.Equal("Test.", collector.LatestRecord.Message);
+    }
+}
