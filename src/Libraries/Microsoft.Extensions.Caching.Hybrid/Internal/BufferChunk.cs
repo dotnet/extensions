579b89e29d35f171d503b1c18d35c4178732e8be
Marc Gravell <marc.gravell@gmail.com>
2024-09-12 00:41:14 +0100

    Migrate HybridCache from aspnetcore to extensions (#5391)
    this is a direct snapshot from dotnet/aspnetcore, with modifications to:
    
    1. satisfy local repo style rules
    2. work with different build setup
    
    Minor global changes:
    
    1. support netstandard2.1 on some legacy shim includes
    2. add some additional required packages (for testing)

---
+++ src/Libraries/Microsoft.Extensions.Caching.Hybrid/Internal/BufferChunk.cs
@@ -0,0 +1,94 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Extensions.Caching.Hybrid.Internal;
+
+// Used to convey buffer status; like ArraySegment<byte>, but Offset is always
+// zero, and we use the most significant bit of the length (usually the sign flag,
+// but we do not need to support negative length) to track whether or not
+// to recycle this value.
+internal readonly struct BufferChunk
+{
+    private const int FlagReturnToPool = (1 << 31);
+
+    private readonly int _lengthAndPoolFlag;
+
+    public byte[]? Array { get; } // null for default
+
+    public int Length => _lengthAndPoolFlag & ~FlagReturnToPool;
+
+    public bool ReturnToPool => (_lengthAndPoolFlag & FlagReturnToPool) != 0;
+
+    public BufferChunk(byte[] array)
+    {
+        Debug.Assert(array is not null, "expected valid array input");
+        Array = array;
+        _lengthAndPoolFlag = array!.Length;
+
+        // assume not pooled, if exact-sized
+        // (we don't expect array.Length to be negative; we're really just saying
+        // "we expect the result of assigning array.Length to _lengthAndPoolFlag
+        // to give the expected Length *and* not have the MSB set; we're just
+        // checking that we haven't fat-fingered our MSB logic)
+        Debug.Assert(!ReturnToPool, "do not return right-sized arrays");
+        Debug.Assert(Length == array.Length, "array length not respected");
+    }
+
+    public BufferChunk(byte[] array, int length, bool returnToPool)
+    {
+        Debug.Assert(array is not null, "expected valid array input");
+        Debug.Assert(length >= 0, "expected valid length");
+        Array = array;
+        _lengthAndPoolFlag = length | (returnToPool ? FlagReturnToPool : 0);
+        Debug.Assert(ReturnToPool == returnToPool, "return-to-pool not respected");
+        Debug.Assert(Length == length, "length not respected");
+    }
+
+    public byte[] ToArray()
+    {
+        var length = Length;
+        if (length == 0)
+        {
+            return [];
+        }
+
+        var copy = new byte[length];
+        Buffer.BlockCopy(Array!, 0, copy, 0, length);
+        return copy;
+
+        // Note on nullability of Array; the usage here is that a non-null array
+        // is always provided during construction, so the only null scenario is for default(BufferChunk).
+        // Since the constructor explicitly accesses array.Length, any null array passed to the constructor
+        // will cause an exception, even in release (the Debug.Assert only covers debug) - although in
+        // reality we do not expect this to ever occur (internal type, usage checked, etc). In the case of
+        // default(BufferChunk), we know that Length will be zero, which means we will hit the [] case.
+    }
+
+    internal void RecycleIfAppropriate()
+    {
+        if (ReturnToPool)
+        {
+            ArrayPool<byte>.Shared.Return(Array!);
+        }
+
+        Unsafe.AsRef(in this) = default; // anti foot-shotgun double-return guard; not 100%, but worth doing
+        Debug.Assert(Array is null && !ReturnToPool, "expected clean slate after recycle");
+    }
+
+    // get the data as a ROS; for note on null-logic of Array!, see comment in ToArray
+    internal ReadOnlySequence<byte> AsSequence() => Length == 0 ? default : new ReadOnlySequence<byte>(Array!, 0, Length);
+
+    internal BufferChunk DoNotReturnToPool()
+    {
+        var copy = this;
+        Unsafe.AsRef(in copy._lengthAndPoolFlag) &= ~FlagReturnToPool;
+        Debug.Assert(copy.Length == Length, "same length expected");
+        Debug.Assert(!copy.ReturnToPool, "do not return to pool");
+        return copy;
+    }
+}
---
+++ src/Libraries/Microsoft.Extensions.Caching.Hybrid/Internal/ImmutableTypeCache.cs
@@ -0,0 +1,79 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+#if NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+using System.Runtime.CompilerServices;
+#else
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+#endif
+
+namespace Microsoft.Extensions.Caching.Hybrid.Internal;
+
+/// <summary>
+/// Utility type for determining whether a type is blittable; the logic for this is very
+/// TFM dependent.
+/// </summary>
+internal static class ImmutableTypeCache
+{
+    internal static bool IsBlittable<T>() // minimize the generic portion (twinned with IsTypeImmutable)
+    {
+#if NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        return !RuntimeHelpers.IsReferenceOrContainsReferences<T>();
+#else
+        // down-level: only blittable types can be pinned
+        try
+        {
+            // get a typed, zeroed, non-null boxed instance of the appropriate type
+            // (can't use (object)default(T), as that would box to null for nullable types)
+            var obj = FormatterServices.GetUninitializedObject(Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T));
+            GCHandle.Alloc(obj, GCHandleType.Pinned).Free();
+            return true;
+        }
+#pragma warning disable CA1031 // Do not catch general exception types: interpret any failure here as "nope"
+        catch
+        {
+            return false;
+        }
+#pragma warning restore CA1031
+
+#endif
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Blocker Code Smell", "S2178:Short-circuit logic should be used in boolean contexts",
+        Justification = "Non-short-circuiting intentional to remove unnecessary branch")]
+    internal static bool IsTypeImmutable(Type type)
+    {
+        // check for known types
+        if (type == typeof(string))
+        {
+            return true;
+        }
+
+        if (type.IsValueType)
+        {
+            // switch from Foo? to Foo if necessary
+            if (Nullable.GetUnderlyingType(type) is { } nullable)
+            {
+                type = nullable;
+            }
+        }
+
+        if (type.IsValueType || (type.IsClass & type.IsSealed))
+        {
+            // check for [ImmutableObject(true)]; note we're looking at this as a statement about
+            // the overall nullability; for example, a type could contain a private int[] field,
+            // where the field is mutable and the list is mutable; but if the type is annotated:
+            // we're trusting that the API and use-case is such that the type is immutable
+            return type.GetCustomAttribute<ImmutableObjectAttribute>() is { Immutable: true };
+        }
+
+        // don't trust interfaces and non-sealed types; we might have any concrete
+        // type that has different behaviour
+        return false;
+    }
+}
