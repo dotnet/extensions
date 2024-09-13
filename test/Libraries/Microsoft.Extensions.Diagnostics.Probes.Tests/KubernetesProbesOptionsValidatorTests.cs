7f098799e7253a9979cd3cf193fd2701b0a024fb
Makazeu <yeefan@outlook.com>
2024-09-14 01:22:54 +0800

    Add another tests that KubernetesProbesOptions are validated when Host is starting

--- test/Libraries/Microsoft.Extensions.Diagnostics.Probes.Tests/KubernetesProbesOptionsValidatorTests.cs
+++ test/Libraries/Microsoft.Extensions.Diagnostics.Probes.Tests/KubernetesProbesOptionsValidatorTests.cs
@@ -1,6 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace Microsoft.Extensions.Diagnostics.Probes.Test;
@@ -11,7 +16,7 @@ public class KubernetesProbesOptionsValidatorTests
     public void Validator_DefaultValues_Succeeds()
     {
         var options = new KubernetesProbesOptions();
-        var result = new KubernetesProbesOptionsValidator().Validate(nameof(options), options);
+        ValidateOptionsResult result = new KubernetesProbesOptionsValidator().Validate(nameof(options), options);
 
         Assert.True(result.Succeeded);
     }
@@ -24,7 +29,7 @@ public class KubernetesProbesOptionsValidatorTests
         options.StartupProbe.TcpPort = 2306;
         options.ReadinessProbe.TcpPort = 2307;
 
-        var result = new KubernetesProbesOptionsValidator().Validate(nameof(options), options);
+        ValidateOptionsResult result = new KubernetesProbesOptionsValidator().Validate(nameof(options), options);
 
         Assert.True(result.Succeeded);
     }
@@ -38,7 +43,7 @@ public class KubernetesProbesOptionsValidatorTests
         options.ReadinessProbe.TcpPort = 2307;
 
         var validator = new KubernetesProbesOptionsValidator();
-        var result = validator.Validate(nameof(options), options);
+        ValidateOptionsResult result = validator.Validate(nameof(options), options);
         Assert.True(result.Failed);
 
         options.LivenessProbe.TcpPort = 2305;
@@ -58,5 +63,62 @@ public class KubernetesProbesOptionsValidatorTests
         options.ReadinessProbe.TcpPort = 2305;
         result = validator.Validate(nameof(options), options);
         Assert.True(result.Failed);
+    }
+
+    [Fact]
+    public async Task Validator_WhenHostStarts_Succeeds()
+    {
+        using IHost host = CreateHostBuilder(services =>
+        {
+            services.AddKubernetesProbes(options =>
+            {
+                options.LivenessProbe.TcpPort = 22305;
+                options.StartupProbe.TcpPort = 22306;
+                options.ReadinessProbe.TcpPort = 22307;
+            }).AddHealthChecks();
+        });
+
+        try
+        {
+            host.Start();
+            await host.StopAsync();
+        }
+        catch (OptionsValidationException ex)
+        {
+            Assert.Fail("Unexpected OptionsValidationException: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail("Unexpected exception: " + ex.Message);
+            throw;
+        }
+    }
+
+    [Fact]
+    public void Validator_WhenHostStarts_Fails()
+    {
+        Action action = () =>
+        {
+            using IHost host = CreateHostBuilder(services =>
+            {
+                services.AddKubernetesProbes(options =>
+                {
+                    options.LivenessProbe.TcpPort = 22305;
+                    options.StartupProbe.TcpPort = 22305;
+                    options.ReadinessProbe.TcpPort = 22307;
+                }).AddHealthChecks();
+            });
+
+            host.Start();
+        };
+
+        Assert.Throws<OptionsValidationException>(action);
+    }
+
+    private static IHost CreateHostBuilder(Action<IServiceCollection> configureServices)
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices(configureServices)
+            .Build();
     }
 }
