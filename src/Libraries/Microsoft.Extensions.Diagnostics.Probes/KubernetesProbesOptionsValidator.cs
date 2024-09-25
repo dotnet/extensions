c799d768af96a37412f4231d3ab6ef96c6e1e7ed
Makazeu <makazeu@gmail.com>
2024-09-26 07:16:45 +0800

    Add KubernetesProbesOptionsValidator to ensure different ports for probes (#5400)
    * Add KubernetesProbesOptionsValidator to enforce different ports for probes
    
    * Add another tests that KubernetesProbesOptions are validated when Host is starting
    
    * update
    
    ---------
    
    Co-authored-by: Yifan Zhu <yifzhu@microsoft.com>

---
+++ src/Libraries/Microsoft.Extensions.Diagnostics.Probes/KubernetesProbesOptionsValidator.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.Diagnostics.Probes;
+
+internal sealed class KubernetesProbesOptionsValidator : IValidateOptions<KubernetesProbesOptions>
+{
+    public ValidateOptionsResult Validate(string? name, KubernetesProbesOptions options)
+    {
+        var builder = new ValidateOptionsResultBuilder();
+
+        if (options.LivenessProbe.TcpPort == options.StartupProbe.TcpPort
+            || options.LivenessProbe.TcpPort == options.ReadinessProbe.TcpPort
+            || options.StartupProbe.TcpPort == options.ReadinessProbe.TcpPort)
+        {
+            builder.AddError("Liveness, startup and readiness probes must use different ports.");
+        }
+
+        return builder.Build();
+    }
+}
