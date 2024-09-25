c32217a5d953807d892ecb5ebea3a1fd397abf68
joegoldman2 <147369450+joegoldman2@users.noreply.github.com>
2024-09-25 11:10:58 +0000

    Update HttpRetryStrategyOptions XML doc

--- src/Libraries/Microsoft.Extensions.Http.Resilience/Polly/HttpRetryStrategyOptions.cs
+++ src/Libraries/Microsoft.Extensions.Http.Resilience/Polly/HttpRetryStrategyOptions.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Http.Resilience.Internal;
 using Polly;
 using Polly.Retry;
+using Polly.Timeout;
 
 namespace Microsoft.Extensions.Http.Resilience;
 
@@ -21,8 +22,10 @@ public class HttpRetryStrategyOptions : RetryStrategyOptions<HttpResponseMessage
     /// Initializes a new instance of the <see cref="HttpRetryStrategyOptions"/> class.
     /// </summary>
     /// <remarks>
-    /// By default, the options are set to handle only transient failures,
-    /// that is, timeouts, 5xx responses, and <see cref="HttpRequestException"/> exceptions.
+    /// By default, the options are configured to handle only transient failures.
+    /// Specifically, this includes HTTP status codes 408, 429, 500 and above, 
+    /// as well as <see cref="HttpRequestException"/> and <see cref="TimeoutRejectedException"/> exceptions.
+    /// Additionally, if the response includes a <c>Retry-After</c> header, its value will be used to determine the delay before retrying.
     /// </remarks>
     public HttpRetryStrategyOptions()
     {
