5dc647614784e885c98525c2f321152b7537fbd9
joegoldman2 <147369450+joegoldman2@users.noreply.github.com>
2024-09-26 08:43:00 +0000

    Clarify HttpRetryStrategyOptions/HttpHedgingStrategyOptions XML doc (#5445)
    * Update HttpRetryStrategyOptions XML doc
    
    * Update HttpHedgingStrategyOptions XML doc
    
    * Update HttpHedgingStrategyOptions.cs
    
    * Update HttpRetryStrategyOptions.cs

--- src/Libraries/Microsoft.Extensions.Http.Resilience/Hedging/HttpHedgingStrategyOptions.cs
+++ src/Libraries/Microsoft.Extensions.Http.Resilience/Hedging/HttpHedgingStrategyOptions.cs
@@ -3,7 +3,9 @@
 
 using System.Net.Http;
 using System.Threading.Tasks;
+using Polly.CircuitBreaker;
 using Polly.Hedging;
+using Polly.Timeout;
 
 namespace Microsoft.Extensions.Http.Resilience;
 
@@ -16,8 +18,9 @@ public class HttpHedgingStrategyOptions : HedgingStrategyOptions<HttpResponseMes
     /// Initializes a new instance of the <see cref="HttpHedgingStrategyOptions"/> class.
     /// </summary>
     /// <remarks>
-    /// By default, the options are set to handle only transient failures,
-    /// that is, timeouts, 5xx responses, and <see cref="HttpRequestException"/> exceptions.
+    /// By default, the options are configured to handle only transient failures.
+    /// Specifically, this includes HTTP status codes 408, 429, 500 and above, 
+    /// as well as <see cref="HttpRequestException"/>, <see cref="BrokenCircuitException"/>, and <see cref="TimeoutRejectedException"/> exceptions.
     /// </remarks>
     public HttpHedgingStrategyOptions()
     {
