5631d0ddff26cb2fe6b81adddbd20dde42f92c77
Iliar Turdushev <iliarturdu@microsoft.com>
2024-09-30 13:33:19 +0200

    Fixes #4957
    Adds extension methods allowing retrieval of the request message from resilience context

---
+++ src/Libraries/Microsoft.Extensions.Http.Resilience/Resilience/HttpResilienceContextExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.Http.Resilience.Internal;
+using Microsoft.Shared.Diagnostics;
+using Polly;
+
+namespace Polly;
+
+/// <summary>
+/// Provides utility methods for working with <see cref="ResilienceContext"/>.
+/// </summary>
+public static class HttpResilienceContextExtensions
+{
+    /// <summary>
+    /// Gets the request message from the <see cref="ResilienceContext"/>.
+    /// </summary>
+    /// <param name="context">The resilience context.</param>
+    /// <returns>
+    /// The request message.
+    /// If the request message is not present in the <see cref="ResilienceContext"/> the method returns <see langword="null"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"><paramref name="context"/> is <see langword="null"/>.</exception>
+    public static HttpRequestMessage? GetRequestMessage(this ResilienceContext context)
+    {
+        _ = Throw.IfNull(context);
+        return context.Properties.GetValue(ResilienceKeys.RequestMessage, default);
+    }
+
+    /// <summary>
+    /// Sets the request message on the <see cref="ResilienceContext"/>.
+    /// </summary>
+    /// <param name="context">The resilience context.</param>
+    /// <param name="requestMessage">The request message.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="context"/> is <see langword="null"/>.</exception>
+    public static void SetRequestMessage(this ResilienceContext context, HttpRequestMessage? requestMessage)
+    {
+        _ = Throw.IfNull(context);
+        context.Properties.Set(ResilienceKeys.RequestMessage, requestMessage);
+    }
+}
---
+++ test/Libraries/Microsoft.Extensions.Http.Resilience.Tests/Resilience/HttpResilienceContextExtensionsTests.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.Http.Resilience.Internal;
+using Polly;
+using Xunit;
+
+namespace Microsoft.Extensions.Http.Resilience.Test.Resilience;
+
+public class HttpResilienceContextExtensionsTests
+{
+    [Fact]
+    public void GetRequestMessage_ResilienceContextIsNull_Throws()
+    {
+        ResilienceContext context = null!;
+        Assert.Throws<ArgumentNullException>(context.GetRequestMessage);
+    }
+
+    [Fact]
+    public void GetRequestMessage_RequestMessageIsMissing_ReturnsNull()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+
+        Assert.Null(context.GetRequestMessage());
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+
+    [Fact]
+    public void GetRequestMessage_RequestMessageIsNull_ReturnsNull()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+        context.Properties.Set(ResilienceKeys.RequestMessage, null);
+
+        Assert.Null(context.GetRequestMessage());
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+
+    [Fact]
+    public void GetRequestMessage_RequestMessageIsPresent_ReturnsRequestMessage()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+        using var request = new HttpRequestMessage();
+        context.Properties.Set(ResilienceKeys.RequestMessage, request);
+
+        Assert.Same(request, context.GetRequestMessage());
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+
+    [Fact]
+    public void SetRequestMessage_ResilienceContextIsNull_Throws()
+    {
+        ResilienceContext context = null!;
+        using var request = new HttpRequestMessage();
+        Assert.Throws<ArgumentNullException>(() => context.SetRequestMessage(request));
+    }
+
+    [Fact]
+    public void SetRequestMessage_RequestMessageIsNull_SetsNullRequestMessage()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+        context.SetRequestMessage(null);
+
+        Assert.True(context.Properties.TryGetValue(ResilienceKeys.RequestMessage, out HttpRequestMessage? request));
+        Assert.Null(request);
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+
+    [Fact]
+    public void SetRequestMessage_RequestMessageIsNotNull_SetsRequestMessage()
+    {
+        var context = ResilienceContextPool.Shared.Get();
+        using var request = new HttpRequestMessage();
+        context.SetRequestMessage(request);
+
+        Assert.True(context.Properties.TryGetValue(ResilienceKeys.RequestMessage, out HttpRequestMessage? actualRequest));
+        Assert.Same(request, actualRequest);
+
+        ResilienceContextPool.Shared.Return(context);
+    }
+}
