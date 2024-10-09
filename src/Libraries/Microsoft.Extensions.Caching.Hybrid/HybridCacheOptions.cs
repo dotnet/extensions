eb321bd00d6a8dd4cbff56d3e95e58295b0a96b9
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-10-09 09:49:04 -0700

    doc updates

--- src/Libraries/Microsoft.Extensions.Caching.Hybrid/HybridCacheOptions.cs
+++ src/Libraries/Microsoft.Extensions.Caching.Hybrid/HybridCacheOptions.cs
@@ -11,11 +11,13 @@ public class HybridCacheOptions
     private const int ShiftBytesToMibiBytes = 20;
 
     /// <summary>
-    /// Gets or sets the default global options to be applied to <see cref="HybridCache"/> operations; if options are
-    /// specified at the individual call level, the non-null values are merged (with the per-call
-    /// options being used in preference to the global options). If no value is specified for a given
-    /// option (globally or per-call), the implementation may choose a reasonable default.
+    /// Gets or sets the default global options to be applied to <see cref="HybridCache"/> operations.
     /// </summary>
+    /// <remarks>
+    /// If options are specified at the individual call level, the non-null values are merged
+    /// (with the per-call options being used in preference to the global options). If no value is
+    /// specified for a given option (globally or per-call), the implementation can choose a reasonable default.
+    /// </remarks>
     public HybridCacheEntryOptions? DefaultEntryOptions { get; set; }
 
     /// <summary>
@@ -24,21 +26,35 @@ public class HybridCacheOptions
     public bool DisableCompression { get; set; }
 
     /// <summary>
-    /// Gets or sets the maximum size of cache items; attempts to store values over this size will be logged
-    /// and the value will not be stored in cache.
+    /// Gets or sets the maximum size of cache items.
     /// </summary>
-    /// <remarks>The default value is 1 MiB.</remarks>
+    /// <value>
+    /// The maximum size of cache items. The default value is 1 MiB.
+    /// </value>
+    /// <remarks>
+    /// Attempts to store values over this size are logged,
+    /// and the value isn't stored in the cache.
+    /// </remarks>
     public long MaximumPayloadBytes { get; set; } = 1 << ShiftBytesToMibiBytes; // 1MiB
 
     /// <summary>
-    /// Gets or sets the maximum permitted length (in characters) of keys; attempts to use keys over this size will be logged.
+    /// Gets or sets the maximum permitted length (in characters) of keys.
     /// </summary>
-    /// <remark>The default value is 1024 characters.</remark>
+    /// <value>
+    /// The maximum permitted length of keys, in characters. The default value is 1024 characters.
+    /// </value>
+    /// <remarks>Attempts to use keys over this size are logged.</remarks>
     public int MaximumKeyLength { get; set; } = 1024; // characters
 
     /// <summary>
-    /// Gets or sets a value indicating whether to use "tags" data as dimensions on metric reporting; if enabled, care should be used to ensure that
-    /// tags do not contain data that should not be visible in metrics systems.
+    /// Gets or sets a value indicating whether to use "tags" data as dimensions on metric reporting.
     /// </summary>
+    /// <value>
+    /// <see langword="true"> to use "tags" data as dimensions on metric reporting; otherwise, <see langword="false"/>.
+    /// </value>
+    /// <remarks>
+    /// If enabled, take care to ensure that tags don't contain data that
+    /// should not be visible in metrics systems.
+    /// </remarks>
     public bool ReportTagMetrics { get; set; }
 }
