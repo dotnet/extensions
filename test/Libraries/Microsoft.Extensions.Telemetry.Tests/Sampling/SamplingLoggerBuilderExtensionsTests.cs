473bfcabd07e977917f41e64b558121fb5f02b90
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2024-10-26 14:21:28 +0200

    more tests

--- test/Libraries/Microsoft.Extensions.Telemetry.Tests/Sampling/SamplingLoggerBuilderExtensionsTests.cs
+++ test/Libraries/Microsoft.Extensions.Telemetry.Tests/Sampling/SamplingLoggerBuilderExtensionsTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.Sampling;
 using Microsoft.Extensions.Logging;
 using Xunit;
@@ -9,6 +10,85 @@ using Xunit;
 namespace Microsoft.Extensions.Telemetry.Sampling;
 public class SamplingLoggerBuilderExtensionsTests
 {
+    [Fact]
+    public void AddTraceBasedSampling_RegistersInDI()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddTraceBasedSampling();
+        });
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var sampler = serviceProvider.GetService<LoggerSampler>();
+
+        Assert.NotNull(sampler);
+        Assert.IsType<TraceBasedSampler>(sampler);
+    }
+
+    [Fact]
+    public void AddRatioBasedSampler_RegistersInDI()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddRatioBasedSampler(1.0);
+        });
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var sampler = serviceProvider.GetService<LoggerSampler>();
+
+        Assert.NotNull(sampler);
+        Assert.IsType<RatioBasedSampler>(sampler);
+    }
+
+    [Fact]
+    public void AddSampler_RegistersInDI()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddSampler<MySampler>();
+        });
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var sampler = serviceProvider.GetService<LoggerSampler>();
+
+        Assert.NotNull(sampler);
+        Assert.IsType<MySampler>(sampler);
+    }
+
+    [Fact]
+    public void AddSamplerInstance_RegistersInDI()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddSampler(new MySampler());
+        });
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var sampler = serviceProvider.GetService<LoggerSampler>();
+
+        Assert.NotNull(sampler);
+        Assert.IsType<MySampler>(sampler);
+    }
+
+    [Fact]
+    public void AddFuncSampler_RegistersInDI()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddSampler(_ => true);
+        });
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var sampler = serviceProvider.GetService<LoggerSampler>();
+
+        Assert.NotNull(sampler);
+        Assert.IsType<FuncBasedSampler>(sampler);
+    }
+
     [Fact]
     public void WhenArgumentIsNull_Throws()
     {
