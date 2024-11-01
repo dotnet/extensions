d51d8a2224a6d6fd1109d15e33902ca3873026bc
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-11-01 10:55:23 +0000

    Fix build warning

--- test/Shared/JsonSchemaExporter/JsonSchemaExporterTests.cs
+++ test/Shared/JsonSchemaExporter/JsonSchemaExporterTests.cs
@@ -9,7 +9,9 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Schema;
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
+#if !NET9_0_OR_GREATER
 using System.Xml.Linq;
+#endif
 using Xunit;
 
 #pragma warning disable SA1402 // File may only contain a single type
