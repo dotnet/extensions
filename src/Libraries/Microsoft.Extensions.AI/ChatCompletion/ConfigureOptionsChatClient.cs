8b320e2f70b0edbe38f72a7a8f5d209fca0b0374
Stephen Toub <stoub@microsoft.com>
2024-11-07 14:04:04 -0500

    Rework UseChatOptions as ConfigureOptions (#5606)
    * Update README to include a section on UseChatOptions
    
    * Rework UseChat/EmbeddingGenerationOptions to always clone
    
    The callbacks now configure the supplied instance.

--- src/Libraries/Microsoft.Extensions.AI/ChatCompletion/ConfigureOptionsChatClient.cs
+++ src/Libraries/Microsoft.Extensions.AI/ChatCompletion/ConfigureOptionsChatClient.cs
@@ -8,67 +8,54 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Shared.Diagnostics;
 
-#pragma warning disable SA1629 // Documentation text should end with a period
-
 namespace Microsoft.Extensions.AI;
 
-/// <summary>A delegating chat client that updates or replaces the <see cref="ChatOptions"/> used by the remainder of the pipeline.</summary>
-/// <remarks>
-/// <para>
-/// The configuration callback is invoked with the caller-supplied <see cref="ChatOptions"/> instance. To override the caller-supplied options
-/// with a new instance, the callback may simply return that new instance, for example <c>_ => new ChatOptions() { MaxTokens = 1000 }</c>. To provide
-/// a new instance only if the caller-supplied instance is <see langword="null"/>, the callback may conditionally return a new instance, for example
-/// <c>options => options ?? new ChatOptions() { MaxTokens = 1000 }</c>. Any changes to the caller-provided options instance will persist on the
-/// original instance, so the callback must take care to only do so when such mutations are acceptable, such as by cloning the original instance
-/// and mutating the clone, for example:
-/// <c>
-/// options =>
-/// {
-///     var newOptions = options?.Clone() ?? new();
-///     newOptions.MaxTokens = 1000;
-///     return newOptions;
-/// }
-/// </c>
-/// </para>
-/// <para>
-/// The callback may return <see langword="null"/>, in which case a <see langword="null"/> options will be passed to the next client in the pipeline.
-/// </para>
-/// <para>
-/// The provided implementation of <see cref="IChatClient"/> is thread-safe for concurrent use so long as the employed configuration
-/// callback is also thread-safe for concurrent requests. If callers employ a shared options instance, care should be taken in the
-/// configuration callback, as multiple calls to it may end up running in parallel with the same options instance.
-/// </para>
-/// </remarks>
+/// <summary>A delegating chat client that configures a <see cref="ChatOptions"/> instance used by the remainder of the pipeline.</summary>
 public sealed class ConfigureOptionsChatClient : DelegatingChatClient
 {
     /// <summary>The callback delegate used to configure options.</summary>
-    private readonly Func<ChatOptions?, ChatOptions?> _configureOptions;
+    private readonly Action<ChatOptions> _configureOptions;
 
-    /// <summary>Initializes a new instance of the <see cref="ConfigureOptionsChatClient"/> class with the specified <paramref name="configureOptions"/> callback.</summary>
+    /// <summary>Initializes a new instance of the <see cref="ConfigureOptionsChatClient"/> class with the specified <paramref name="configure"/> callback.</summary>
     /// <param name="innerClient">The inner client.</param>
-    /// <param name="configureOptions">
-    /// The delegate to invoke to configure the <see cref="ChatOptions"/> instance. It is passed the caller-supplied <see cref="ChatOptions"/>
-    /// instance and should return the configured <see cref="ChatOptions"/> instance to use.
+    /// <param name="configure">
+    /// The delegate to invoke to configure the <see cref="ChatOptions"/> instance. It is passed a clone of the caller-supplied <see cref="ChatOptions"/> instance
+    /// (or a newly-constructed instance if the caller-supplied instance is <see langword="null"/>).
     /// </param>
-    public ConfigureOptionsChatClient(IChatClient innerClient, Func<ChatOptions?, ChatOptions?> configureOptions)
+    /// <remarks>
+    /// The <paramref name="configure"/> delegate is passed either a new instance of <see cref="ChatOptions"/> if
+    /// the caller didn't supply a <see cref="ChatOptions"/> instance, or a clone (via <see cref="ChatOptions.Clone"/> of the caller-supplied
+    /// instance if one was supplied.
+    /// </remarks>
+    public ConfigureOptionsChatClient(IChatClient innerClient, Action<ChatOptions> configure)
         : base(innerClient)
     {
-        _configureOptions = Throw.IfNull(configureOptions);
+        _configureOptions = Throw.IfNull(configure);
     }
 
     /// <inheritdoc/>
     public override async Task<ChatCompletion> CompleteAsync(IList<ChatMessage> chatMessages, ChatOptions? options = null, CancellationToken cancellationToken = default)
     {
-        return await base.CompleteAsync(chatMessages, _configureOptions(options), cancellationToken).ConfigureAwait(false);
+        return await base.CompleteAsync(chatMessages, Configure(options), cancellationToken).ConfigureAwait(false);
     }
 
     /// <inheritdoc/>
     public override async IAsyncEnumerable<StreamingChatCompletionUpdate> CompleteStreamingAsync(
         IList<ChatMessage> chatMessages, ChatOptions? options = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        await foreach (var update in base.CompleteStreamingAsync(chatMessages, _configureOptions(options), cancellationToken).ConfigureAwait(false))
+        await foreach (var update in base.CompleteStreamingAsync(chatMessages, Configure(options), cancellationToken).ConfigureAwait(false))
         {
             yield return update;
         }
+    }
+
+    /// <summary>Creates and configures the <see cref="ChatOptions"/> to pass along to the inner client.</summary>
+    private ChatOptions Configure(ChatOptions? options)
+    {
+        options = options?.Clone() ?? new();
+
+        _configureOptions(options);
+
+        return options;
     }
 }
