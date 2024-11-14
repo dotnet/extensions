451c10574c9292906cdd593cc26ef4c0d21c624b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-11-13 23:17:53 -0800

    Remove generic DI registration methods

--- src/Libraries/Microsoft.Extensions.AI/ChatCompletion/ChatClientBuilderServiceCollectionExtensions.cs
+++ src/Libraries/Microsoft.Extensions.AI/ChatCompletion/ChatClientBuilderServiceCollectionExtensions.cs
@@ -21,16 +21,6 @@ public static class ChatClientBuilderServiceCollectionExtensions
         => AddChatClient(serviceCollection, _ => innerClient);
 
     /// <summary>Registers a singleton <see cref="IChatClient"/> in the <see cref="IServiceCollection"/>.</summary>
-    /// <typeparam name="T">The type of the inner <see cref="IChatClient"/> that represents the underlying backend. This will be resolved from the service provider.</typeparam>
-    /// <param name="serviceCollection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
-    /// <returns>A <see cref="ChatClientBuilder"/> that can be used to build a pipeline around the inner client.</returns>
-    /// <remarks>The client is registered as a singleton service.</remarks>
-    public static ChatClientBuilder AddChatClient<T>(
-        this IServiceCollection serviceCollection)
-        where T : IChatClient
-        => AddChatClient(serviceCollection, services => services.GetRequiredService<T>());
-
-    /// <summary>Registers a singleton <see cref="IChatClient"/> in the <see cref="IServiceCollection"/>.</summary>
     /// <param name="serviceCollection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
     /// <param name="innerClientFactory">A callback that produces the inner <see cref="IChatClient"/> that represents the underlying backend.</param>
     /// <returns>A <see cref="ChatClientBuilder"/> that can be used to build a pipeline around the inner client.</returns>
@@ -58,18 +48,6 @@ public static class ChatClientBuilderServiceCollectionExtensions
         object serviceKey,
         IChatClient innerClient)
         => AddKeyedChatClient(serviceCollection, serviceKey, _ => innerClient);
-
-    /// <summary>Registers a singleton <see cref="IChatClient"/> in the <see cref="IServiceCollection"/>.</summary>
-    /// <typeparam name="T">The type of the inner <see cref="IChatClient"/> that represents the underlying backend. This will be resolved from the service provider.</typeparam>
-    /// <param name="serviceCollection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
-    /// <param name="serviceKey">The key with which to associate the client.</param>
-    /// <returns>A <see cref="ChatClientBuilder"/> that can be used to build a pipeline around the inner client.</returns>
-    /// <remarks>The client is registered as a scoped service.</remarks>
-    public static ChatClientBuilder AddKeyedChatClient<T>(
-        this IServiceCollection serviceCollection,
-        object serviceKey)
-        where T : IChatClient
-        => AddKeyedChatClient(serviceCollection, serviceKey, services => services.GetRequiredService<T>());
 
     /// <summary>Registers a singleton <see cref="IChatClient"/> in the <see cref="IServiceCollection"/>.</summary>
     /// <param name="serviceCollection">The <see cref="IServiceCollection"/> to which the client should be added.</param>
--- test/Libraries/Microsoft.Extensions.AI.Tests/ChatCompletion/DependencyInjectionPatterns.cs
+++ test/Libraries/Microsoft.Extensions.AI.Tests/ChatCompletion/DependencyInjectionPatterns.cs
@@ -12,30 +12,6 @@ public class DependencyInjectionPatterns
     private IServiceCollection ServiceCollection { get; } = new ServiceCollection();
 
     [Fact]
-    public void CanRegisterSingletonUsingGenericType()
-    {
-        // Arrange/Act
-        ServiceCollection.AddSingleton(services => new TestChatClient { Services = services });
-        ServiceCollection.AddChatClient<TestChatClient>()
-            .UseSingletonMiddleware();
-
-        // Assert
-        var services = ServiceCollection.BuildServiceProvider();
-        using var scope1 = services.CreateScope();
-        using var scope2 = services.CreateScope();
-
-        var instance1 = scope1.ServiceProvider.GetRequiredService<IChatClient>();
-        var instance1Copy = scope1.ServiceProvider.GetRequiredService<IChatClient>();
-        var instance2 = scope2.ServiceProvider.GetRequiredService<IChatClient>();
-
-        // Each scope gets the same instance, because it's singleton
-        var instance = Assert.IsType<SingletonMiddleware>(instance1);
-        Assert.Same(instance, instance1Copy);
-        Assert.Same(instance, instance2);
-        Assert.IsType<TestChatClient>(instance.InnerClient);
-    }
-
-    [Fact]
     public void CanRegisterSingletonUsingFactory()
     {
         // Arrange/Act
@@ -74,32 +50,6 @@ public class DependencyInjectionPatterns
         var instance1 = scope1.ServiceProvider.GetRequiredService<IChatClient>();
         var instance1Copy = scope1.ServiceProvider.GetRequiredService<IChatClient>();
         var instance2 = scope2.ServiceProvider.GetRequiredService<IChatClient>();
-
-        // Each scope gets the same instance, because it's singleton
-        var instance = Assert.IsType<SingletonMiddleware>(instance1);
-        Assert.Same(instance, instance1Copy);
-        Assert.Same(instance, instance2);
-        Assert.IsType<TestChatClient>(instance.InnerClient);
-    }
-
-    [Fact]
-    public void CanRegisterKeyedSingletonUsingGenericType()
-    {
-        // Arrange/Act
-        ServiceCollection.AddSingleton(services => new TestChatClient { Services = services });
-        ServiceCollection.AddKeyedChatClient<TestChatClient>("mykey")
-            .UseSingletonMiddleware();
-
-        // Assert
-        var services = ServiceCollection.BuildServiceProvider();
-        using var scope1 = services.CreateScope();
-        using var scope2 = services.CreateScope();
-
-        Assert.Null(services.GetService<IChatClient>());
-
-        var instance1 = scope1.ServiceProvider.GetRequiredKeyedService<IChatClient>("mykey");
-        var instance1Copy = scope1.ServiceProvider.GetRequiredKeyedService<IChatClient>("mykey");
-        var instance2 = scope2.ServiceProvider.GetRequiredKeyedService<IChatClient>("mykey");
 
         // Each scope gets the same instance, because it's singleton
         var instance = Assert.IsType<SingletonMiddleware>(instance1);
