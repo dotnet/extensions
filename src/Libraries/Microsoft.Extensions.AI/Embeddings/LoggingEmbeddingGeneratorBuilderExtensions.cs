16fe41f8a850304092a0f18ba223d6af561ade6c
Stephen Toub <stoub@microsoft.com>
2024-11-20 13:59:53 -0500

    Change UseLogging to accept an ILoggerFactory instead of ILogger
    Fits better with DI, and makes it consistent with UseFunctionInvocation and UseOpenTelemetry.

--- src/Libraries/Microsoft.Extensions.AI/Embeddings/LoggingEmbeddingGeneratorBuilderExtensions.cs
+++ src/Libraries/Microsoft.Extensions.AI/Embeddings/LoggingEmbeddingGeneratorBuilderExtensions.cs
@@ -15,21 +15,24 @@ public static class LoggingEmbeddingGeneratorBuilderExtensions
     /// <typeparam name="TInput">Specifies the type of the input passed to the generator.</typeparam>
     /// <typeparam name="TEmbedding">Specifies the type of the embedding instance produced by the generator.</typeparam>
     /// <param name="builder">The <see cref="EmbeddingGeneratorBuilder{TInput, TEmbedding}"/>.</param>
-    /// <param name="logger">
-    /// An optional <see cref="ILogger"/> with which logging should be performed. If not supplied, an instance will be resolved from the service provider.
+    /// <param name="loggerFactory">
+    /// An optional <see cref="ILoggerFactory"/> used to create a logger with which logging should be performed.
+    /// If not supplied, a required instance will be resolved from the service provider.
     /// </param>
     /// <param name="configure">An optional callback that can be used to configure the <see cref="LoggingEmbeddingGenerator{TInput, TEmbedding}"/> instance.</param>
     /// <returns>The <paramref name="builder"/>.</returns>
     public static EmbeddingGeneratorBuilder<TInput, TEmbedding> UseLogging<TInput, TEmbedding>(
-        this EmbeddingGeneratorBuilder<TInput, TEmbedding> builder, ILogger? logger = null, Action<LoggingEmbeddingGenerator<TInput, TEmbedding>>? configure = null)
+        this EmbeddingGeneratorBuilder<TInput, TEmbedding> builder,
+        ILoggerFactory? loggerFactory = null,
+        Action<LoggingEmbeddingGenerator<TInput, TEmbedding>>? configure = null)
         where TEmbedding : Embedding
     {
         _ = Throw.IfNull(builder);
 
         return builder.Use((innerGenerator, services) =>
         {
-            logger ??= services.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(LoggingEmbeddingGenerator<TInput, TEmbedding>));
-            var generator = new LoggingEmbeddingGenerator<TInput, TEmbedding>(innerGenerator, logger);
+            loggerFactory ??= services.GetRequiredService<ILoggerFactory>();
+            var generator = new LoggingEmbeddingGenerator<TInput, TEmbedding>(innerGenerator, loggerFactory.CreateLogger(typeof(LoggingEmbeddingGenerator<TInput, TEmbedding>)));
             configure?.Invoke(generator);
             return generator;
         });
