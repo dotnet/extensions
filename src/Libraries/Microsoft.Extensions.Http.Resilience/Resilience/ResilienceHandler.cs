ad1ca1ad29e8721a781220a2e61d66d5606e3b6d
Iliar Turdushev <iliarturdu@microsoft.com>
2024-11-24 13:28:54 +0100

    Test

--- src/Libraries/Microsoft.Extensions.Http.Resilience/Resilience/ResilienceHandler.cs
+++ src/Libraries/Microsoft.Extensions.Http.Resilience/Resilience/ResilienceHandler.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#pragma warning disable LA0006
+
 using System;
 using System.Net.Http;
 using System.Threading;
