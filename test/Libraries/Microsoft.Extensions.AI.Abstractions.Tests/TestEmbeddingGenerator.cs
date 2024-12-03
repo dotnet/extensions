e8efa1f1c551efa729be9263fcf864addfef74b1
Stephen Toub <stoub@microsoft.com>
2024-12-03 12:48:41 -0500

    Make UseLogging a nop when NullLoggerFactory is used (#5717)

--- test/Libraries/Microsoft.Extensions.AI.Abstractions.Tests/TestEmbeddingGenerator.cs
+++ test/Libraries/Microsoft.Extensions.AI.Abstractions.Tests/TestEmbeddingGenerator.cs
@@ -10,11 +10,19 @@ namespace Microsoft.Extensions.AI;
 
 public sealed class TestEmbeddingGenerator : IEmbeddingGenerator<string, Embedding<float>>
 {
+    public TestEmbeddingGenerator()
+    {
+        GetServiceCallback = DefaultGetServiceCallback;
+    }
+
     public EmbeddingGeneratorMetadata Metadata { get; } = new();
 
     public Func<IEnumerable<string>, EmbeddingGenerationOptions?, CancellationToken, Task<GeneratedEmbeddings<Embedding<float>>>>? GenerateAsyncCallback { get; set; }
 
-    public Func<Type, object?, object?> GetServiceCallback { get; set; } = (_, _) => null;
+    public Func<Type, object?, object?> GetServiceCallback { get; set; }
+
+    private object? DefaultGetServiceCallback(Type serviceType, object? serviceKey) =>
+        serviceType is not null && serviceKey is null && serviceType.IsInstanceOfType(this) ? this : null;
 
     public Task<GeneratedEmbeddings<Embedding<float>>> GenerateAsync(IEnumerable<string> values, EmbeddingGenerationOptions? options = null, CancellationToken cancellationToken = default)
         => GenerateAsyncCallback!.Invoke(values, options, cancellationToken);
