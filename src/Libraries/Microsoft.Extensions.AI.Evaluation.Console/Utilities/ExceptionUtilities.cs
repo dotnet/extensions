4e9b72102fc1df386d4016c38a2c4bc0137def0f
Shyam Namboodiripad <gnamboo@microsoft.com>
2025-02-12 13:05:12 -0800

    Feedback: Avoid deep recursion

--- src/Libraries/Microsoft.Extensions.AI.Evaluation.Console/Utilities/ExceptionUtilities.cs
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Console/Utilities/ExceptionUtilities.cs
@@ -2,25 +2,45 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Microsoft.Extensions.AI.Evaluation.Console.Utilities;
 
 internal static class ExceptionUtilities
 {
-    internal static bool IsCancellation(this Exception exception)
+    internal static bool IsCancellation(this Exception exception) =>
+        exception switch
+        {
+            OperationCanceledException => true,
+            AggregateException aggregateException => aggregateException.ContainsOnlyCancellations(),
+            _ => false
+        };
+
+    private static bool ContainsOnlyCancellations(this AggregateException exception)
     {
-        if (exception is OperationCanceledException)
+        var exceptionsToCheck = new Stack<Exception>();
+        exceptionsToCheck.Push(exception);
+
+        bool containsAtLeastOneCancellation = false;
+        while (exceptionsToCheck.TryPop(out Exception? current))
         {
-            return true;
+            if (current is AggregateException aggregateException)
+            {
+                foreach (var innerException in aggregateException.InnerExceptions)
+                {
+                    exceptionsToCheck.Push(innerException);
+                }
+            }
+            else if (current is OperationCanceledException)
+            {
+                containsAtLeastOneCancellation = true;
+            }
+            else
+            {
+                return false;
+            }
         }
-        else if (exception is AggregateException aggregateException)
-        {
-            return aggregateException.InnerExceptions.All(IsCancellation);
-        }
-        else
-        {
-            return false;
-        }
+
+        return containsAtLeastOneCancellation;
     }
 }
