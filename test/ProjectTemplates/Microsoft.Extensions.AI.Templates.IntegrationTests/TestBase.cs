0d9b3a0bfbe1eb3210884dead15f2c004bc41313
Igor Velikorossov <igor.velikorossov@microsoft.com>
2025-03-12 21:11:52 +1100

    Test templates

---
+++ test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/TestBase.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+
+namespace Microsoft.Extensions.AI.Templates.IntegrationTests;
+
+/// <summary>
+/// The class contains the utils for unit and integration tests.
+/// </summary>
+public abstract class TestBase
+{
+    internal static string CodeBaseRoot { get; } = GetCodeBaseRoot();
+
+    // internal static string TemplateFeedLocation { get; } = Path.Combine(CodeBaseRoot, "template_feed");
+    internal static string TemplateFeedLocation { get; } = Path.Combine(CodeBaseRoot, "src", "ProjectTemplates");
+
+    private static string GetCodeBaseRoot()
+    {
+        string? directory = AppContext.BaseDirectory;
+
+        while (directory is not null)
+        {
+            var gitPath = Path.Combine(directory, ".git");
+            if (Directory.Exists(gitPath) || File.Exists(gitPath))
+            {
+                // Found the repo root, which should either have a .git folder or, if the repo
+                // is part of a Git worktree, a .git file.
+                return directory;
+            }
+
+            directory = Directory.GetParent(directory)?.FullName;
+        }
+
+        throw new InvalidOperationException("Failed to establish root of the repository");
+    }
+}
