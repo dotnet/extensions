ad7a81475a6c87f2e623fdcff0b8bebfbb20fd75
amadeuszl <lechniak.amadeusz07@gmail.com>
2025-03-14 08:24:50 +0100

    Refactor

--- src/Libraries/Microsoft.AspNetCore.Diagnostics.Middleware/Logging/LoggingRedactionOptions.cs
+++ src/Libraries/Microsoft.AspNetCore.Diagnostics.Middleware/Logging/LoggingRedactionOptions.cs
@@ -108,9 +108,7 @@ public class LoggingRedactionOptions
     /// If set to true, instead of logging <i>unknown</i> value for path attribute it will log whole path of routes not identified by ASP.NET Routing.
     /// </remarks>
     /// <value>Defaults to <see langword="false"/>.</value>
-#pragma warning disable CA1805 // Do not initialize unnecessarily
-    public bool IncludeUnmatchedRoutes { get; set; } = false;
-#pragma warning restore CA1805 // Do not initialize unnecessarily
+    public bool IncludeUnmatchedRoutes { get; set; }
 }
 
 #endif
