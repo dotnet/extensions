037c86609faa8422c88d2b3abaa1b6ab142e69d0
Iliar Turdushev <iliarturdu@microsoft.com>
2025-03-18 09:53:58 +0100

    [Telemetry, Logging] Emit {OriginalFormat} as the last property of the log record (#6111)
    * Fixes #5720
    
    Now {OriginalFormat} is emitted as the last item in the TagArray
    
    * Fixes #5720
    
    Adjusts ExtendedLogger to always return {OriginalFormat} as the last tag in the list
    
    * Fixes #5720
    
    Fixes ResourceMonitoring tests
    
    * Fixes #5720
    
    Address PR comments

---
+++ test/Generators/Microsoft.Gen.Logging/TestClasses/OriginalFormatTestExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Logging;
+
+namespace TestClasses
+{
+    internal static partial class OriginalFormatTestExtensions
+    {
+        [LoggerMessage(0, LogLevel.Information, "M0")]
+        public static partial void M0(ILogger logger);
+
+        [LoggerMessage(1, LogLevel.Information, "M1 {p0}")]
+        public static partial void M1(ILogger logger, string p0);
+
+        [LoggerMessage(2, LogLevel.Information, "M2 {p0}{p1}")]
+        public static partial void M2(ILogger logger, string p0, string p1);
+
+        [LoggerMessage(LogLevel.Information)]
+        public static partial void M3(ILogger logger, string p0, string p1, string p2);
+    }
+}
