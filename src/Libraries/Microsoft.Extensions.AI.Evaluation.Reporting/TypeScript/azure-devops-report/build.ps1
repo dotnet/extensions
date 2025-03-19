19220d77dd2636f114fc51bcc09028232c6ab977
Peter Waldschmidt <pewaldsc@microsoft.com>
2025-03-19 15:19:00 -0400

    Add license info to the Azure DevOps package

--- src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/TypeScript/azure-devops-report/build.ps1
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/TypeScript/azure-devops-report/build.ps1
@@ -1,7 +1,7 @@
 param (
     [string]$OutputPath,
     [string]$Version = $null,
-    [bool]$IncludeTestPackage = $false
+    [switch]$IncludeTestPackage = $false
 )
 
 # if version is not set, then run a script to get it
@@ -22,7 +22,7 @@ if ($null -eq $PackageVersion)
 Write-Host "Using version $PackageVersion"
 
 # Write-Information "Building Report Publishing task"
-Push-Location $PSScriptRoot/tasks/PublishAIEvaluationReport
+Set-Location $PSScriptRoot/tasks/PublishAIEvaluationReport
 npm install --omit=dev
 # Copy task files to dist folder
 New-Item -ItemType Directory -Path ./dist -Force
@@ -32,23 +32,26 @@ copy-item -Path ./package.json -Destination ./dist/ -Force
 copy-item -Path ./node_modules -Destination ./dist/node_modules -Force -Recurse
 
 # remove the test files from resolve package because they are currently breaking vsix signing (zero length)
-remove-item -Path ./dist/node_modules/resolve/test -Recurse -Force
+remove-item -Path ./dist/node_modules/resolve/test -Recurse -Force -ErrorAction SilentlyContinue
 
 @{  version = $PackageVersion
-    public = $true } | ConvertTo-Json -Compress | Out-File -FilePath $PSScriptRoot/override.json
-
+    public = $true } | ConvertTo-Json -Compress | Out-File -FilePath $PSScriptRoot/override.json -Encoding ascii
+    
 # Write-Information "Building Extension Package" 
 Set-Location $PSScriptRoot
 npm install
 npx tsc -b
 npx vite build
+    
+# Copy LICENSE file from the root
+copy-item -Path $PSScriptRoot/../../../../../LICENSE -Destination . -Force
 
 npx tfx-cli extension create --overrides-file $PSScriptRoot/override.json --output-path $OutputPath
 
 if ($true -eq $IncludeTestPackage) {
     @{  version = $PackageVersion
         id = "microsoft-extensions-ai-evaluation-report-test" 
-        name = "[TEST] Azure DevOps AI Evaluation Report" } | ConvertTo-Json -Compress | Out-File -FilePath $PSScriptRoot/override.json
+        name = "[TEST] Azure DevOps AI Evaluation Report" } | ConvertTo-Json -Compress | Out-File -FilePath $PSScriptRoot/override.json -Encoding ascii
 
     # Build Preview version of the extension for testing
     npx tfx-cli extension create --overrides-file $PSScriptRoot/override.json --output-path $OutputPath
