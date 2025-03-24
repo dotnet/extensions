878e61b44876fdc03a81a352b2c0d56277c84f9d
Makazeu <yeefan@outlook.com>
2025-03-24 23:08:12 +0800

    Add a simple unit test

---
+++ test/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring.Tests/Windows/Disk/WindowsDiskMetricsTests.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.Metrics;
+using System.Linq;
+using Microsoft.Extensions.Diagnostics.ResourceMonitoring.Test.Helpers;
+using Microsoft.Shared.Instruments;
+using Microsoft.TestUtilities;
+using Xunit;
+
+namespace Microsoft.Extensions.Diagnostics.ResourceMonitoring.Windows.Disk.Test;
+
+[OSSkipCondition(OperatingSystems.Linux | OperatingSystems.MacOSX, SkipReason = "Windows specific.")]
+public class WindowsDiskMetricsTests
+{
+    [ConditionalFact]
+    public void Creates_Meter_With_Correct_Name()
+    {
+        using var meterFactory = new TestMeterFactory();
+        var options = new ResourceMonitoringOptions { EnableDiskIoMetrics = true };
+
+        _ = new WindowsDiskMetrics(meterFactory, Microsoft.Extensions.Options.Options.Create(options));
+
+        Meter meter = meterFactory.Meters.Single();
+        Assert.Equal(ResourceUtilizationInstruments.MeterName, meter.Name);
+    }
+}
