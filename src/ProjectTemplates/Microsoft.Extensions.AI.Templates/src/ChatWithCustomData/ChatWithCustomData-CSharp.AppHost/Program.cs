c171afab55b36dc54b97104b56866e86c9cb73e4
Mackinnon Buck <mackinnon.buck@gmail.com>
2025-03-25 14:14:30 -0700

    Add quotes around connection strings

--- src/ProjectTemplates/Microsoft.Extensions.AI.Templates/src/ChatWithCustomData/ChatWithCustomData-CSharp.AppHost/Program.cs
+++ src/ProjectTemplates/Microsoft.Extensions.AI.Templates/src/ChatWithCustomData/ChatWithCustomData-CSharp.AppHost/Program.cs
@@ -6,11 +6,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 // You can do this using Visual Studio's "Manage User Secrets" UI, or on the command line:
 //   cd this-project-directory
 #if (IsOpenAI)
-//   dotnet user-secrets set ConnectionStrings:openai Key=YOUR-API-KEY
+//   dotnet user-secrets set ConnectionStrings:openai "Key=YOUR-API-KEY"
 #elif (IsGHModels)
-//   dotnet user-secrets set ConnectionStrings:openai Endpoint=https://models.inference.ai.azure.com;Key=YOUR-API-KEY
+//   dotnet user-secrets set ConnectionStrings:openai "Endpoint=https://models.inference.ai.azure.com;Key=YOUR-API-KEY"
 #else // IsAzureOpenAI
-//   dotnet user-secrets set ConnectionStrings:openai Endpoint=https://YOUR-DEPLOYMENT-NAME.openai.azure.com;Key=YOUR-API-KEY
+//   dotnet user-secrets set ConnectionStrings:openai "Endpoint=https://YOUR-DEPLOYMENT-NAME.openai.azure.com;Key=YOUR-API-KEY"
 #endif
 var openai = builder.AddConnectionString("openai");
 #endif
@@ -19,7 +19,7 @@ var openai = builder.AddConnectionString("openai");
 // You will need to set the connection string to your own value
 // You can do this using Visual Studio's "Manage User Secrets" UI, or on the command line:
 //   cd this-project-directory
-//   dotnet user-secrets set ConnectionStrings:azureAISearch Endpoint=https://YOUR-DEPLOYMENT-NAME.search.windows.net;Key=YOUR-API-KEY
+//   dotnet user-secrets set ConnectionStrings:azureAISearch "Endpoint=https://YOUR-DEPLOYMENT-NAME.search.windows.net;Key=YOUR-API-KEY"
 var azureAISearch = builder.AddConnectionString("azureAISearch");
 #endif
 #if (IsOllama) // AI SERVICE PROVIDER CONFIGURATION
--- test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/Snapshots/aichatweb.BasicAspire-as.verified/aichatweb/aichatweb.AppHost/Program.cs
+++ test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/Snapshots/aichatweb.BasicAspire-as.verified/aichatweb/aichatweb.AppHost/Program.cs
@@ -3,7 +3,7 @@
 // You will need to set the connection string to your own value
 // You can do this using Visual Studio's "Manage User Secrets" UI, or on the command line:
 //   cd this-project-directory
-//   dotnet user-secrets set ConnectionStrings:openai Endpoint=https://models.inference.ai.azure.com;Key=YOUR-API-KEY
+//   dotnet user-secrets set ConnectionStrings:openai "Endpoint=https://models.inference.ai.azure.com;Key=YOUR-API-KEY"
 var openai = builder.AddConnectionString("openai");
 
 var ingestionCache = builder.AddSqlite("ingestionCache");
