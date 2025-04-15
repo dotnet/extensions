a83cef580b1c6aa2f733fde54781e42013b46775
Peter Waldschmidt <pewaldsc@microsoft.com>
2025-04-15 16:03:54 -0400

    Make tsc build verbose

--- src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/TypeScript/azure-devops-report/vite.config.ts
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/TypeScript/azure-devops-report/vite.config.ts
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    minify: true,
-  },
   base: './',
   plugins: [
     react(),
