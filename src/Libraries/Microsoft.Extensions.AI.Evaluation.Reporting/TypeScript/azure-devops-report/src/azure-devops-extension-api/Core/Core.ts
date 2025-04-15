3c5bd6123b16d91e52ca1897f4b7183bd3afed52
Peter Waldschmidt <pewaldsc@microsoft.com>
2025-04-15 17:34:17 -0400

    Repackage azure-devops-extensions-api to fix Azure DevOps report plugin (#6299)
    * Repackage azure-devops-extensions-api
    
    * Unify headers
    
    * Regenerate the package-lock.json file
    
    * Remove unneeded packages
    
    * Make tsc build verbose
    
    * Add debug flag for vite build
    
    * Add NPM build command for testing
    
    * Fix the tsc build
    
    * Make sure we log stdout when an error occurs.

---
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/TypeScript/azure-devops-report/src/azure-devops-extension-api/Core/Core.ts
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+/**
+ * Represents a shallow reference to a TeamProject.
+ */
+export interface TeamProjectReference {
+    /**
+     * Project name.
+     */
+    name: string;
+}
+
