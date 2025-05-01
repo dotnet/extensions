e66a00bcc961ba7d99be092d364751bccfee547e
Mackinnon Buck <mackinnon.buck@gmail.com>
2025-05-01 13:24:16 -0700

    Generate all possible valid template combinations

--- test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/AIChatWebExecutionTests.cs
+++ test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/AIChatWebExecutionTests.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -20,28 +22,20 @@ public class AIChatWebExecutionTests : TemplateExecutionTestBase<AIChatWebExecut
         TestOutputFolderPrefix = "AIChatWeb"
     };
 
+    public static IEnumerable<object[]> GetBasicTemplateOptions()
+        => GetFilteredTemplateOptions("--aspire", "false");
+
+    public static IEnumerable<object[]> GetAspireTemplateOptions()
+        => GetFilteredTemplateOptions("--aspire", "true");
+
     [Theory]
-    [InlineData("AzureApp",
-        "--provider", "azureopenai",
-        "--vector-store", "azureaisearch")]
-    [InlineData("AzureAppManagedIdentity",
-        "--provider", "azureopenai",
-        "--vector-store", "azureaisearch",
-        "--managed-identity")]
-    [InlineData("GitHubModelsWithLocalStore",
-        "--provider", "githubmodels",
-        "--vector-store", "local")]
-    [InlineData("OllamaWithLocalStore",
-        "--provider", "ollama",
-        "--vector-store", "local")]
-    [InlineData("OpenAIWithLocalStore",
-        "--provider", "openai",
-        "--vector-store", "local")]
-    public async Task CreateRestoreAndBuild_BasicTemplate(string projectName, params string[] args)
+    [MemberData(nameof(GetBasicTemplateOptions))]
+    public async Task CreateRestoreAndBuild_BasicTemplate(params string[] args)
     {
+        const string ProjectName = "BasicApp";
         var project = await Fixture.CreateProjectAsync(
             templateName: "aichatweb",
-            projectName,
+            projectName: ProjectName,
             args);
 
         await Fixture.RestoreProjectAsync(project);
@@ -49,28 +43,106 @@ public class AIChatWebExecutionTests : TemplateExecutionTestBase<AIChatWebExecut
     }
 
     [Theory]
-    [InlineData("AzureApp",
-        "--provider", "azureopenai",
-        "--vector-store", "azureaisearch")]
-    [InlineData("GitHubModelsWithQdrant",
-        "--provider", "githubmodels",
-        "--vector-store", "qdrant")]
-    [InlineData("OllamaWithLocalStore",
-        "--provider", "ollama",
-        "--vector-store", "local")]
-    [InlineData("OpenAIWithLocalStore",
-        "--provider", "ollama",
-        "--vector-store", "local")]
-    public async Task CreateRestoreAndBuild_AspireTemplate(string projectName, params string[] args)
+    [MemberData(nameof(GetAspireTemplateOptions))]
+    public async Task CreateRestoreAndBuild_AspireTemplate(params string[] args)
     {
+        const string ProjectName = "AspireApp";
         var project = await Fixture.CreateProjectAsync(
             templateName: "aichatweb",
-            projectName,
+            ProjectName,
             args: ["--aspire", .. args]);
 
-        project.StartupProjectRelativePath = $"{projectName}.AppHost";
+        project.StartupProjectRelativePath = $"{ProjectName}.AppHost";
 
         await Fixture.RestoreProjectAsync(project);
         await Fixture.BuildProjectAsync(project);
+    }
+
+    private static readonly (string name, string[] values)[] _templateOptions = [
+        ("--provider",          ["azureopenai", "githubmodels", "ollama", "openai"]),
+        ("--vector-store",      ["azureaisearch", "local", "qdrant"]),
+        ("--managed-identity",  ["true", "false"]),
+        ("--aspire",            ["true", "false"]),
+    ];
+
+    private static IEnumerable<object[]> GetFilteredTemplateOptions(params string[] filter)
+    {
+        foreach (var options in GetAllPossibleOptions(_templateOptions))
+        {
+            if (!MatchesFilter())
+            {
+                continue;
+            }
+
+            if (HasOption("--managed-identity", "true"))
+            {
+                if (HasOption("--aspire", "true"))
+                {
+                    // The managed identity option is disabled for the Aspire template.
+                    continue;
+                }
+
+                if (!HasOption("--vector-store", "azureaisearch") &&
+                    !HasOption("--aspire", "false"))
+                {
+                    // Can only use managed identity when using Azure in the non-Aspire template.
+                    continue;
+                }
+            }
+
+            if (HasOption("--vector-store", "qdrant") &&
+                HasOption("--aspire", "false"))
+            {
+                // Can't use Qdrant without Aspire.
+                continue;
+            }
+
+            yield return options;
+
+            bool MatchesFilter()
+            {
+                for (var i = 0; i < filter.Length; i += 2)
+                {
+                    if (!HasOption(filter[i], filter[i + 1]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            bool HasOption(string name, string value)
+            {
+                for (var i = 0; i < options.Length; i += 2)
+                {
+                    if (string.Equals(name, options[i], StringComparison.Ordinal) &&
+                        string.Equals(value, options[i + 1], StringComparison.Ordinal))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    private static IEnumerable<string[]> GetAllPossibleOptions(ReadOnlyMemory<(string name, string[] values)> options)
+    {
+        if (options.Length == 0)
+        {
+            yield return [];
+            yield break;
+        }
+
+        var first = options.Span[0];
+        foreach (var restSelection in GetAllPossibleOptions(options[1..]))
+        {
+            foreach (var value in first.values)
+            {
+                yield return [first.name, value, .. restSelection];
+            }
+        }
     }
 }
