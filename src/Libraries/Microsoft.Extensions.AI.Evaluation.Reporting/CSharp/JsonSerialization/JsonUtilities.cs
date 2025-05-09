62178f5cda527061eeae7cc57b7f9b4ff2397a79
Shyam N <shyamnamboodiripad@users.noreply.github.com>
2025-05-09 11:33:25 -0700

    Some API related fixes for the evaluation libraries (#6402)
    * Rename IResultStore and IResponseCacheProvider
    
    IResultStore -> IEvaluationResultStore and
    IResponseCacheProvider -> IEvaluationResponseCacheProvider
    
    * Include missing EvaluationContextConverter in AzureStorageJsonUtilities
    
    Also use linked files to avoid the need to duplicate code.
    
    * Reorder enum members
    
    The new order goes from least desirable rating to most desirable.
    
    * Refactor extension method overloads
    
    Implement overloads that take ChatMessage by calling corresponding overloads that take ChatResponse.
    
    * Refactor AddTurnDetails to support adding details for multiple turns
    
    Adding single turns continues to be supported via a params array overload.
    
    * Add missing parameter for timeToLiveForCacheEntries to DiskBasedReportingConfiguration
    
    This was missed in an earlier PR that introduced the timeToLiveForCacheEntries on the constructor of DiskBasedResponseCacheProvider.
    
    Also reorder constructor parameters for AzureStorageReportingConfiguration so that the parameters for caching apear next to each other and so that the parameter ordering is aligned with DiskBasedReportingConfiguration.
    
    * Minor formatting changes

--- src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/CSharp/JsonSerialization/JsonUtilities.cs
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Reporting/CSharp/JsonSerialization/JsonUtilities.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -12,7 +13,7 @@ namespace Microsoft.Extensions.AI.Evaluation.Reporting.JsonSerialization;
 
 internal static partial class JsonUtilities
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "Default matches the generated source naming convention.")]
+    [SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "Default matches the generated source naming convention.")]
     internal static class Default
     {
         private static JsonSerializerOptions? _options;
@@ -45,7 +46,7 @@ internal static partial class JsonUtilities
         return options;
     }
 
-    [JsonSerializable(typeof(EvaluationResult))]
+    [JsonSerializable(typeof(ScenarioRunResult))]
     [JsonSerializable(typeof(Dataset))]
     [JsonSerializable(typeof(CacheEntry))]
     [JsonSourceGenerationOptions(
