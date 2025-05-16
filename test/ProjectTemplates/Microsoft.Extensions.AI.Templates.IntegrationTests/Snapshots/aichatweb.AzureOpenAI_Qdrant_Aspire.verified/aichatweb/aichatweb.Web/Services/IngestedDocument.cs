665207f5c940802a9828a5c740dbc559e50449d2
Jeff Handley <jeffhandley@users.noreply.github.com>
2025-05-16 10:08:46 -0700

    Add AzureOpenAI_Qdrant_Aspire and Ollama_Qdrant snapshot template tests

---
+++ test/ProjectTemplates/Microsoft.Extensions.AI.Templates.IntegrationTests/Snapshots/aichatweb.AzureOpenAI_Qdrant_Aspire.verified/aichatweb/aichatweb.Web/Services/IngestedDocument.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace aichatweb.Web.Services;
+
+public class IngestedDocument
+{
+    [VectorStoreRecordKey]
+    public required string Key { get; set; }
+
+    [VectorStoreRecordData(IsIndexed = true)]
+    public required string SourceId { get; set; }
+
+    [VectorStoreRecordData]
+    public required string DocumentId { get; set; }
+
+    [VectorStoreRecordData]
+    public required string DocumentVersion { get; set; }
+
+    // The vector is not used but required for some vector databases
+    [VectorStoreRecordVector(2, DistanceFunction = DistanceFunction.CosineSimilarity)]
+    public ReadOnlyMemory<float> Vector { get; set; } = new ReadOnlyMemory<float>([0, 0]);
+}
