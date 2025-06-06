cd0a2fdfb87f044ca5ae96b3f8dcc57863c71ccf
Shyam Namboodiripad <gnamboo@microsoft.com>
2025-06-06 12:34:07 -0700

    Introduce ToolCallAccuracyEvaluator

---
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Quality/AIToolExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.Shared.Diagnostics;
+
+namespace Microsoft.Extensions.AI.Evaluation.Quality;
+
+internal static class AIToolExtensions
+{
+    internal static string RenderAsJson(
+        this IEnumerable<AITool> toolDefinitions,
+        JsonSerializerOptions? options = null)
+    {
+        _ = Throw.IfNull(toolDefinitions);
+
+        var toolDefinitionsJsonArray = new JsonArray();
+
+        foreach (AIFunction function in toolDefinitions.OfType<AIFunction>())
+        {
+            JsonNode? functionJsonNode = JsonNode.Parse(function.JsonSchema.GetRawText());
+            if (functionJsonNode is not null)
+            {
+                toolDefinitionsJsonArray.Add(functionJsonNode);
+            }
+        }
+
+        string renderedToolDefinitions = toolDefinitionsJsonArray.ToJsonString(options);
+        return renderedToolDefinitions;
+    }
+}
---
+++ src/Libraries/Microsoft.Extensions.AI.Evaluation.Quality/ChatMessageExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.Shared.Diagnostics;
+
+namespace Microsoft.Extensions.AI.Evaluation.Quality;
+
+internal static class ChatMessageExtensions
+{
+    internal static string RenderAsJson(this IEnumerable<ChatMessage> messages, JsonSerializerOptions? options = null)
+    {
+        _ = Throw.IfNull(messages);
+
+        var messagesJsonArray = new JsonArray();
+
+        foreach (ChatMessage message in messages)
+        {
+            string messageJsonString =
+                JsonSerializer.Serialize(
+                    message,
+                    AIJsonUtilities.DefaultOptions.GetTypeInfo(typeof(ChatMessage)));
+
+            JsonNode? messageJsonNode = JsonNode.Parse(messageJsonString);
+            if (messageJsonNode is not null)
+            {
+                messagesJsonArray.Add(messageJsonNode);
+            }
+        }
+
+        string renderedMessages = messagesJsonArray.ToJsonString(options);
+        return renderedMessages;
+    }
+}
