ae446fbb8659307f34b1ecb6f4e943471e98d79c
Stephen Toub <stoub@microsoft.com>
2025-06-25 12:01:35 -0400

    Augment AIJsonUtilities.CreateJsonSchema for more types and annotations (#6540)
    * Augment AIJsonUtilities.CreateJsonSchema for more types and annotations
    
    * Stop suppressing existing format handling, and allow most annotations on netfx

--- src/Libraries/Microsoft.Extensions.AI.OpenAI/OpenAIClientExtensions.cs
+++ src/Libraries/Microsoft.Extensions.AI.OpenAI/OpenAIClientExtensions.cs
@@ -3,13 +3,20 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using OpenAI;
 using OpenAI.Assistants;
 using OpenAI.Audio;
 using OpenAI.Chat;
 using OpenAI.Embeddings;
 using OpenAI.Responses;
+
+#pragma warning disable S103 // Lines should not be too long
+#pragma warning disable S1067 // Expressions should not be too complex
+#pragma warning disable SA1515 // Single-line comment should be preceded by blank line
+#pragma warning disable CA1305 // Specify IFormatProvider
 
 namespace Microsoft.Extensions.AI;
 
@@ -26,7 +33,8 @@ public static class OpenAIClientExtensions
     internal static ChatRole ChatRoleDeveloper { get; } = new ChatRole("developer");
 
     /// <summary>
-    /// Gets the JSON schema transformer cache conforming to OpenAI <b>strict</b> restrictions per https://platform.openai.com/docs/guides/structured-outputs?api-mode=responses#supported-schemas.
+    /// Gets the JSON schema transformer cache conforming to OpenAI <b>strict</b> / structured output restrictions per
+    /// https://platform.openai.com/docs/guides/structured-outputs?api-mode=responses#supported-schemas.
     /// </summary>
     internal static AIJsonSchemaTransformCache StrictSchemaTransformCache { get; } = new(new()
     {
@@ -34,6 +42,65 @@ public static class OpenAIClientExtensions
         ConvertBooleanSchemas = true,
         MoveDefaultKeywordToDescription = true,
         RequireAllProperties = true,
+        TransformSchemaNode = (ctx, node) =>
+        {
+            // Move content from common but unsupported properties to description. In particular, we focus on properties that
+            // the AIJsonUtilities schema generator might produce and/or that are explicitly mentioned in the OpenAI documentation.
+
+            if (node is JsonObject schemaObj)
+            {
+                StringBuilder? additionalDescription = null;
+
+                ReadOnlySpan<string> unsupportedProperties =
+                [
+                    // Produced by AIJsonUtilities but not in allow list at https://platform.openai.com/docs/guides/structured-outputs#supported-properties:
+                    "contentEncoding", "contentMediaType", "not",
+
+                    // Explicitly mentioned at https://platform.openai.com/docs/guides/structured-outputs?api-mode=responses#key-ordering as being unsupported with some models:
+                    "minLength", "maxLength", "pattern", "format",
+                    "minimum", "maximum", "multipleOf",
+                    "patternProperties",
+                    "minItems", "maxItems",
+
+                    // Explicitly mentioned at https://learn.microsoft.com/azure/ai-services/openai/how-to/structured-outputs?pivots=programming-language-csharp&tabs=python-secure%2Cdotnet-entra-id#unsupported-type-specific-keywords
+                    // as being unsupported with Azure OpenAI:
+                    "unevaluatedProperties", "propertyNames", "minProperties", "maxProperties",
+                    "unevaluatedItems", "contains", "minContains", "maxContains", "uniqueItems",
+                ];
+
+                foreach (string propName in unsupportedProperties)
+                {
+                    if (schemaObj[propName] is { } propNode)
+                    {
+                        _ = schemaObj.Remove(propName);
+                        AppendLine(ref additionalDescription, propName, propNode);
+                    }
+                }
+
+                if (additionalDescription is not null)
+                {
+                    schemaObj["description"] = schemaObj["description"] is { } descriptionNode && descriptionNode.GetValueKind() == JsonValueKind.String ?
+                        $"{descriptionNode.GetValue<string>()}{Environment.NewLine}{additionalDescription}" :
+                        additionalDescription.ToString();
+                }
+
+                return node;
+
+                static void AppendLine(ref StringBuilder? sb, string propName, JsonNode propNode)
+                {
+                    sb ??= new();
+
+                    if (sb.Length > 0)
+                    {
+                        _ = sb.AppendLine();
+                    }
+
+                    _ = sb.Append(propName).Append(": ").Append(propNode);
+                }
+            }
+
+            return node;
+        },
     });
 
     /// <summary>Gets an <see cref="IChatClient"/> for use with this <see cref="ChatClient"/>.</summary>
