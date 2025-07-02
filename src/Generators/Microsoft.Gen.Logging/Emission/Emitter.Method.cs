d94683245e1fb4c7ff24b2d06da305d76351ef84
Nikita Balabaev <xakep139@users.noreply.github.com>
2025-07-02 14:47:33 +0700

    Align EventId generation with M.E.Logging source-gen
    This fixes #6498

--- src/Generators/Microsoft.Gen.Logging/Emission/Emitter.Method.cs
+++ src/Generators/Microsoft.Gen.Logging/Emission/Emitter.Method.cs
@@ -91,7 +91,9 @@ internal sealed partial class Emitter : EmitterBase
         }
         else
         {
-            OutLn($"new({GetNonRandomizedHashCode(eventName)}, {eventName}),");
+            var eventNameToCalcId = string.IsNullOrWhiteSpace(lm.EventName) ? lm.Name : lm.EventName;
+            var calculatedEventId = GetNonRandomizedHashCode(eventNameToCalcId);
+            OutLn($"new({calculatedEventId}, {eventName}),");
         }
 
         OutLn($"{stateName},");
