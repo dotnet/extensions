e1f72768468fed481eaf3bff54ff9070956af93e
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-07-03 15:33:44 +0300

    AIFunctionFactory: add test coverage for JSON comments.

--- src/Libraries/Microsoft.Extensions.AI.Abstractions/Functions/AIFunctionFactory.cs
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/Functions/AIFunctionFactory.cs
@@ -1058,8 +1058,7 @@ public static partial class AIFunctionFactory
            ( null   # null literal
            | false  # false literal
            | true   # true literal
-           | \d     # positive number
-           | -\d    # negative number
+           | -?\d   # number
            | "      # string
            | \[     # start array
            | {      # start object
