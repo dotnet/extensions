812bd52b8404e26e3a60db7f72c6841a8637cab2
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2025-07-16 15:51:27 +0200

    initial commit

---
+++ src/Generators/Microsoft.Gen.BuildMetadata/BuildMetadataGenerator.cs
@@ -0,0 +1,75 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.Gen.BuildMetadata;
+
+[Generator]
+public class BuildMetadataGenerator : IIncrementalGenerator
+{
+    private static DateTimeOffset _initializeDateTime = DateTimeOffset.UtcNow;
+
+    /// <inheritdoc/>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(context.CompilationProvider, (spc, compilation) => Execute(compilation, spc));
+    }
+
+    private static void Execute(Compilation compilation, SourceProductionContext context)
+    {
+        Model.BuildDateTime = _initializeDateTime;
+
+        OverwriteModelValuesForTesting(compilation.Assembly.GetAttributes());
+
+        EnsureBackwardsCompatibility(context);
+
+        var e = new Emitter();
+        var result = e.EmitExtensions(context.CancellationToken);
+        context.AddSource("BuildMetadataExtensions.g.cs", SourceText.From(result, Encoding.UTF8));
+    }
+
+    private static void EnsureBackwardsCompatibility(SourceProductionContext context)
+    {
+        var e = new Emitter();
+        var result = e.Emit(context.CancellationToken);
+        context.AddSource("BuildMetadataInitializer.g.cs", SourceText.From(result, Encoding.UTF8));
+    }
+
+    private static void OverwriteModelValuesForTesting(ImmutableArray<AttributeData> attributes)
+    {
+        const int TestAttributeConstructorArgumentsLength = 5;
+
+        if (attributes.IsDefaultOrEmpty || attributes[0].ConstructorArguments.Length != TestAttributeConstructorArgumentsLength)
+        {
+            return;
+        }
+
+        // internal attribute has five constructor args:
+        // buildId, buildNumber, sourceBranchName, sourceVersion, buildDateTime
+        var attribute = attributes[0];
+
+#pragma warning disable S109 // Magic numbers should not be used
+#pragma warning disable S1067 // Expressions should not be too complex
+        if (
+        attribute.ConstructorArguments[0].Value is string buildId &&
+        attribute.ConstructorArguments[1].Value is string buildNumber &&
+        attribute.ConstructorArguments[2].Value is string sourceBranchName &&
+        attribute.ConstructorArguments[3].Value is string sourceVersion &&
+        attribute.ConstructorArguments[4].Value is int buildDateTime)
+        {
+            Model.IsAzureDevOps = true;
+            Model.AzureBuildId = buildId;
+            Model.AzureBuildNumber = buildNumber;
+            Model.AzureSourceBranchName = sourceBranchName;
+            Model.AzureSourceVersion = sourceVersion;
+            Model.BuildDateTime = DateTimeOffset.FromUnixTimeSeconds(buildDateTime);
+        }
+#pragma warning restore S1067 // Expressions should not be too complex
+#pragma warning restore S109 // Magic numbers should not be used
+    }
+}
---
+++ test/Generators/Microsoft.Gen.BuildMetadata/Unit/GeneratorTests.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.Gen.Shared;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace Microsoft.Gen.BuildMetadata.Test;
+
+[Collection("BuildMetadataEmitterTests")]
+public class GeneratorTests
+{
+    private readonly VerifySettings _verifySettings;
+
+    public GeneratorTests()
+    {
+        _verifySettings = new VerifySettings();
+
+        _verifySettings.UseDirectory("Verified");
+
+        Model.BuildDateTime = DateTimeOffset.UtcNow;
+
+        Model.AzureBuildId = "AZURE_BUILDID";
+        Model.AzureBuildNumber = "AZURE_BUILDNUMBER";
+        Model.AzureSourceBranchName = "AZURE_SOURCEBRANCHNAME";
+        Model.AzureSourceVersion = "AZURE_SOURCEVERSION";
+
+        Model.GitHubRunId = "GITHUB_RUNID";
+        Model.GitHubRunNumber = "GITHUB_RUNNUMBER";
+        Model.GitHubRefName = "GITHUB_REFNAME";
+        Model.GitHubSha = "GITHUB_SHA";
+
+        _verifySettings.ScrubLinesWithReplace(value =>
+        {
+            if (value.Contains(GeneratorUtilities.GeneratedCodeAttribute))
+            {
+                return value.Replace(GeneratorUtilities.CurrentVersion, "VERSION");
+            }
+
+            if (value.Contains($"{Model.BuildDateTime:s}"))
+            {
+                return value.Replace($"{Model.BuildDateTime:s}", "DATETIMEOFFSET");
+            }
+
+            return value;
+        });
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task BuildMetadataGenerator_ShouldGenerate(
+        [CombinatorialValues(true, false, null)] bool? isAzureDevOps)
+    {
+        Model.IsAzureDevOps = isAzureDevOps != false;
+
+        var source = string.Empty;
+        var additionalRef = false;
+
+        if (isAzureDevOps is null)
+        {
+            additionalRef = true;
+            source = @"
+            using Microsoft.Gen.BuildMetadata;
+            [assembly: GenerateBuildMetadata(
+                ""TEST_BUILDID"",
+                ""TEST_BUILDNUMBER"",
+                ""TEST_SOURCEBRANCHNAME"",
+                ""TEST_SOURCEVERSION"",
+                123456)]";
+        }
+
+        var (d, sources) = await RunGenerator(source, additionalRef: additionalRef);
+
+        d.Should().BeEmpty();
+        sources.Should().HaveCount(2);
+
+        var settings = new VerifySettings(_verifySettings);
+        settings.DisableRequireUniquePrefix();
+        settings.UseParameters(isAzureDevOps);
+
+        await Verifier.Verify(sources.Select(s => s.SourceText.ToString()), settings);
+    }
+
+    private static async Task<(IReadOnlyList<Diagnostic> diagnostic, IReadOnlyList<GeneratedSourceResult> sources)> RunGenerator(string source, bool additionalRef = false)
+    {
+        Assembly[] refs = Array.Empty<Assembly>();
+
+        if (additionalRef)
+        {
+            refs = new[] { Assembly.GetAssembly(typeof(GenerateBuildMetadataAttribute))! };
+        }
+
+        return await RoslynTestUtils.RunGenerator(new BuildMetadataGenerator(), refs, sources: [source]);
+    }
+}
