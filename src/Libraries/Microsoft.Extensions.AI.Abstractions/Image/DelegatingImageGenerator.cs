7d2ea04a579a1712170b2fe34a7184dfdd80f8d1
Eric StJohn <ericstj@microsoft.com>
2025-08-11 20:45:54 -0700

    Add support for text-to-image (#6648)
    * Add ITextToImageClient
    
    * Remove URI based edit since it's not available
    
    * Add filename for edit
    
    * Add OpenAI implmentation of ITextToImageClient
    
    * Fix tests
    
    * Add tests for TextToImage
    
    * Add DeletgatingTextToImageClient and tests
    
    * Add integration test and fix some bugs
    
    * Add remaining support to MEAI for TextToImage
    
    * Make all TextToImageOptions optional
    
    These are all nullable now so that the client can use defaults where
    appropriate.
    
    Remove quality default since it's not consistent across models.
    
    Also remove setting ResponseFormat since this is not supported by
    gpt-image-1.
    
    * Address feedback
    
    * Document some exceptions
    
    * Address feedback
    
    * Make EditImageAsync plural
    
    OpenAI's image API supports multiple images and this does seem to be
    common functionality and a better generalization.  The client library
    doesn't expose this yet, but we should account for it.
    
    Image models may be capable of things like
    "Combine the subjects of these images into a single image" or
    "Create a single image that uses the subject from the first image and
    background for the second" etc.
    
    * Address feedback and add/fix tests.
    
    * Fix bad merge
    
    * Address feedback
    
    * Fix test
    
    * Use DataContent.Name for filename.
    
    * Add extensions for EditImageAsync
    
    Extension that accepts a single DataContent and one that accepts a byte[].
    
    I've left out streams and file paths, since these require more opinions
    about how to load them.  I filed #6683 to address streams.
    
    * Fix test
    
    * Remove use of `_model` field.
    
    * Rename ImageToText to Image
    
    * Rename TextToImage directories to Image
    
    * Rename files TextToImage -> Image
    
    * Add new request and response type
    
    * Make GenerateImagesAsync accept ImageRequest
    
    * Remove EditImageAsync
    
    * Adding GenerateStreamingImagesAsync
    
    * Update docs
    
    * Rename ImageClient ImageGenerator
    
    * Fix up some text-to-image references
    
    * Rename Image(Options|Request|Response)
    
    * Remove `Images` from `GenerateImagesAsync`
    
    * Remove streaming method
    
    We don't yet have any good public support for streaming to vet this API
    
    We can guess at how it might behave for OpenAI, but that doesn't
    really give enough confidence to build the API around it.
    
    * Address feedback
    
    * Provide OpenAI an appropriate filename
    
    * Remove Style from ImageGenerationOptions

---
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/Image/DelegatingImageGenerator.cs
@@ -0,0 +1,69 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Shared.Diagnostics;
+
+namespace Microsoft.Extensions.AI;
+
+/// <summary>
+/// Provides an optional base class for an <see cref="IImageGenerator"/> that passes through calls to another instance.
+/// </summary>
+/// <remarks>
+/// This is recommended as a base type when building generators that can be chained in any order around an underlying <see cref="IImageGenerator"/>.
+/// The default implementation simply passes each call to the inner generator instance.
+/// </remarks>
+[Experimental("MEAI001")]
+public class DelegatingImageGenerator : IImageGenerator
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegatingImageGenerator"/> class.
+    /// </summary>
+    /// <param name="innerGenerator">The wrapped generator instance.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="innerGenerator"/> is <see langword="null"/>.</exception>
+    protected DelegatingImageGenerator(IImageGenerator innerGenerator)
+    {
+        InnerGenerator = Throw.IfNull(innerGenerator);
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>Gets the inner <see cref="IImageGenerator" />.</summary>
+    protected IImageGenerator InnerGenerator { get; }
+
+    /// <inheritdoc />
+    public virtual Task<ImageGenerationResponse> GenerateAsync(
+        ImageGenerationRequest request, ImageGenerationOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return InnerGenerator.GenerateAsync(request, options, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public virtual object? GetService(Type serviceType, object? serviceKey = null)
+    {
+        _ = Throw.IfNull(serviceType);
+
+        // If the key is non-null, we don't know what it means so pass through to the inner service.
+        return
+            serviceKey is null && serviceType.IsInstanceOfType(this) ? this :
+            InnerGenerator.GetService(serviceType, serviceKey);
+    }
+
+    /// <summary>Provides a mechanism for releasing unmanaged resources.</summary>
+    /// <param name="disposing"><see langword="true"/> if being called from <see cref="Dispose()"/>; otherwise, <see langword="false"/>.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            InnerGenerator.Dispose();
+        }
+    }
+}
---
+++ test/Libraries/Microsoft.Extensions.AI.Abstractions.Tests/Image/ImageGeneratorExtensionsTests.cs
@@ -0,0 +1,222 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Extensions.AI;
+
+public class ImageGeneratorExtensionsTests
+{
+    [Fact]
+    public void GetService_InvalidArgs_Throws()
+    {
+        Assert.Throws<ArgumentNullException>("generator", () =>
+        {
+            _ = ImageGeneratorExtensions.GetService<object>(null!);
+        });
+    }
+
+    [Fact]
+    public void GetService_ValidGenerator_CallsUnderlyingGetService()
+    {
+        using var testGenerator = new TestImageGenerator();
+        var expectedResult = new object();
+        var expectedServiceKey = new object();
+
+        testGenerator.GetServiceCallback = (serviceType, serviceKey) =>
+        {
+            Assert.Equal(typeof(object), serviceType);
+            Assert.Same(expectedServiceKey, serviceKey);
+            return expectedResult;
+        };
+
+        var result = testGenerator.GetService<object>(expectedServiceKey);
+        Assert.Same(expectedResult, result);
+    }
+
+    [Fact]
+    public void GetService_ReturnsCorrectType()
+    {
+        using var testGenerator = new TestImageGenerator();
+        var metadata = new ImageGeneratorMetadata("test", null, "model");
+
+        testGenerator.GetServiceCallback = (serviceType, serviceKey) =>
+        {
+            return (serviceType == typeof(ImageGeneratorMetadata)) ? metadata : null;
+        };
+
+        var result = testGenerator.GetService<ImageGeneratorMetadata>();
+        Assert.Same(metadata, result);
+
+        var nullResult = testGenerator.GetService<string>();
+        Assert.Null(nullResult);
+    }
+
+    [Fact]
+    public async Task EditImageAsync_DataContent_CallsGenerateImagesAsync()
+    {
+        // Arrange
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3, 4 };
+        var dataContent = new DataContent(imageData, "image/png") { Name = "test.png" };
+        var prompt = "Edit this image";
+        var options = new ImageGenerationOptions { Count = 2 };
+        var expectedResponse = new ImageGenerationResponse();
+        var cancellationToken = new CancellationToken(canceled: false);
+
+        testGenerator.GenerateImagesAsyncCallback = (request, o, ct) =>
+        {
+            Assert.NotNull(request.OriginalImages);
+            Assert.Single(request.OriginalImages);
+            Assert.Same(dataContent, Assert.Single(request.OriginalImages));
+            Assert.Equal(prompt, request.Prompt);
+            Assert.Same(options, o);
+            Assert.Equal(cancellationToken, ct);
+            return Task.FromResult(expectedResponse);
+        };
+
+        // Act
+        var result = await testGenerator.EditImageAsync(dataContent, prompt, options, cancellationToken);
+
+        // Assert
+        Assert.Same(expectedResponse, result);
+    }
+
+    [Fact]
+    public async Task EditImageAsync_DataContent_NullArguments_Throws()
+    {
+        using var testGenerator = new TestImageGenerator();
+        var dataContent = new DataContent(new byte[] { 1, 2, 3 }, "image/png");
+
+        await Assert.ThrowsAsync<ArgumentNullException>("generator", async () =>
+            await ImageGeneratorExtensions.EditImageAsync(null!, dataContent, "prompt"));
+
+        await Assert.ThrowsAsync<ArgumentNullException>("originalImage", async () =>
+            await testGenerator.EditImageAsync(null!, "prompt"));
+
+        await Assert.ThrowsAsync<ArgumentNullException>("prompt", async () =>
+            await testGenerator.EditImageAsync(dataContent, null!));
+    }
+
+    [Fact]
+    public async Task EditImageAsync_ByteArray_CallsGenerateImagesAsync()
+    {
+        // Arrange
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3, 4 };
+        var fileName = "test.jpg";
+        var prompt = "Edit this image";
+        var options = new ImageGenerationOptions { Count = 2 };
+        var expectedResponse = new ImageGenerationResponse();
+        var cancellationToken = new CancellationToken(canceled: false);
+
+        testGenerator.GenerateImagesAsyncCallback = (request, o, ct) =>
+        {
+            Assert.NotNull(request.OriginalImages);
+            Assert.Single(request.OriginalImages);
+            var dataContent = Assert.IsType<DataContent>(Assert.Single(request.OriginalImages));
+            Assert.Equal(imageData, dataContent.Data.ToArray());
+            Assert.Equal("image/jpeg", dataContent.MediaType);
+            Assert.Equal(fileName, dataContent.Name);
+            Assert.Equal(prompt, request.Prompt);
+            Assert.Same(options, o);
+            Assert.Equal(cancellationToken, ct);
+            return Task.FromResult(expectedResponse);
+        };
+
+        // Act
+        var result = await testGenerator.EditImageAsync(imageData, fileName, prompt, options, cancellationToken);
+
+        // Assert
+        Assert.Same(expectedResponse, result);
+    }
+
+    [Fact]
+    public async Task EditImageAsync_ByteArray_NullGenerator_Throws()
+    {
+        var imageData = new byte[] { 1, 2, 3 };
+
+        await Assert.ThrowsAsync<ArgumentNullException>("generator", async () =>
+            await ImageGeneratorExtensions.EditImageAsync(null!, imageData, "test.png", "prompt"));
+    }
+
+    [Fact]
+    public async Task EditImageAsync_ByteArray_NullFileName_Throws()
+    {
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3 };
+
+        await Assert.ThrowsAsync<ArgumentNullException>("fileName", async () =>
+            await testGenerator.EditImageAsync(imageData, null!, "prompt"));
+    }
+
+    [Fact]
+    public async Task EditImageAsync_ByteArray_NullPrompt_Throws()
+    {
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3 };
+
+        await Assert.ThrowsAsync<ArgumentNullException>("prompt", async () =>
+            await testGenerator.EditImageAsync(imageData, "test.png", null!));
+    }
+
+    [Theory]
+    [InlineData("test.png", "image/png")]
+    [InlineData("test.jpg", "image/jpeg")]
+    [InlineData("test.jpeg", "image/jpeg")]
+    [InlineData("test.webp", "image/webp")]
+    [InlineData("test.gif", "image/gif")]
+    [InlineData("test.bmp", "image/bmp")]
+    [InlineData("test.tiff", "image/tiff")]
+    [InlineData("test.tif", "image/tiff")]
+    [InlineData("test.unknown", "image/png")] // Unknown extension defaults to PNG
+    [InlineData("TEST.PNG", "image/png")] // Case insensitive
+    public async Task EditImageAsync_ByteArray_InfersCorrectMediaType(string fileName, string expectedMediaType)
+    {
+        // Arrange
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3, 4 };
+        var prompt = "Edit this image";
+
+        testGenerator.GenerateImagesAsyncCallback = (request, o, ct) =>
+        {
+            Assert.NotNull(request.OriginalImages);
+            var dataContent = Assert.IsType<DataContent>(Assert.Single(request.OriginalImages));
+            Assert.Equal(expectedMediaType, dataContent.MediaType);
+            return Task.FromResult(new ImageGenerationResponse());
+        };
+
+        // Act & Assert
+        await testGenerator.EditImageAsync(imageData, fileName, prompt);
+    }
+
+    [Fact]
+    public async Task EditImageAsync_AllMethods_PassDefaultOptionsAndCancellation()
+    {
+        // Arrange
+        using var testGenerator = new TestImageGenerator();
+        var imageData = new byte[] { 1, 2, 3, 4 };
+        var dataContent = new DataContent(imageData, "image/png");
+        var prompt = "Edit this image";
+
+        int callCount = 0;
+        testGenerator.GenerateImagesAsyncCallback = (request, o, ct) =>
+        {
+            callCount++;
+            Assert.Null(o); // Default options should be null
+            Assert.Equal(CancellationToken.None, ct); // Default cancellation token
+            Assert.NotNull(request.OriginalImages); // Should have original images for editing
+            return Task.FromResult(new ImageGenerationResponse());
+        };
+
+        // Act - Test all two overloads with default parameters
+        await testGenerator.EditImageAsync(dataContent, prompt);
+        await testGenerator.EditImageAsync(imageData, "test.png", prompt);
+
+        // Assert
+        Assert.Equal(2, callCount);
+    }
+}
