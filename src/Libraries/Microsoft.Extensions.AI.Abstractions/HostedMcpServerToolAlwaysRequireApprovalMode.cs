6911f8829c1f9a4c81dd393cecd41d2b2855ca2f
David Cantú <dacantu@microsoft.com>
2025-09-08 14:17:59 -0500

    Add abstraction for remote MCP servers (#6664)
    * Simpfliy Responses streaming handling
    
    * Add abstraction for remote MCP servers
    
    * Update MCP support to accomodate responses and approvals
    
    ---------
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

---
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/HostedMcpServerToolAlwaysRequireApprovalMode.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.Extensions.AI;
+
+/// <summary>
+/// Indicates that approval is always required for tool calls to a hosted MCP server.
+/// </summary>
+/// <remarks>
+/// Use <see cref="HostedMcpServerToolApprovalMode.AlwaysRequire"/> to get an instance of <see cref="HostedMcpServerToolAlwaysRequireApprovalMode"/>.
+/// </remarks>
+[Experimental("MEAI001")]
+[DebuggerDisplay(nameof(AlwaysRequire))]
+public sealed class HostedMcpServerToolAlwaysRequireApprovalMode : HostedMcpServerToolApprovalMode
+{
+    /// <summary>Initializes a new instance of the <see cref="HostedMcpServerToolAlwaysRequireApprovalMode"/> class.</summary>
+    /// <remarks>Use <see cref="HostedMcpServerToolApprovalMode.AlwaysRequire"/> to get an instance of <see cref="HostedMcpServerToolAlwaysRequireApprovalMode"/>.</remarks>
+    public HostedMcpServerToolAlwaysRequireApprovalMode()
+    {
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is HostedMcpServerToolAlwaysRequireApprovalMode;
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => typeof(HostedMcpServerToolAlwaysRequireApprovalMode).GetHashCode();
+}
---
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/HostedMcpServerToolRequireSpecificApprovalMode.cs
@@ -0,0 +1,66 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Microsoft.Extensions.AI;
+
+/// <summary>
+/// Represents a mode where approval behavior is specified for individual tool names.
+/// </summary>
+[Experimental("MEAI001")]
+public sealed class HostedMcpServerToolRequireSpecificApprovalMode : HostedMcpServerToolApprovalMode
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HostedMcpServerToolRequireSpecificApprovalMode"/> class that specifies approval behavior for individual tool names.
+    /// </summary>
+    /// <param name="alwaysRequireApprovalToolNames">The list of tools names that always require approval.</param>
+    /// <param name="neverRequireApprovalToolNames">The list of tools names that never require approval.</param>
+    public HostedMcpServerToolRequireSpecificApprovalMode(IList<string>? alwaysRequireApprovalToolNames, IList<string>? neverRequireApprovalToolNames)
+    {
+        AlwaysRequireApprovalToolNames = alwaysRequireApprovalToolNames;
+        NeverRequireApprovalToolNames = neverRequireApprovalToolNames;
+    }
+
+    /// <summary>
+    /// Gets or sets the list of tool names that always require approval.
+    /// </summary>
+    public IList<string>? AlwaysRequireApprovalToolNames { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of tool names that never require approval.
+    /// </summary>
+    public IList<string>? NeverRequireApprovalToolNames { get; set; }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is HostedMcpServerToolRequireSpecificApprovalMode other &&
+        ListEquals(AlwaysRequireApprovalToolNames, other.AlwaysRequireApprovalToolNames) &&
+        ListEquals(NeverRequireApprovalToolNames, other.NeverRequireApprovalToolNames);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() =>
+        HashCode.Combine(GetListHashCode(AlwaysRequireApprovalToolNames), GetListHashCode(NeverRequireApprovalToolNames));
+
+    private static bool ListEquals(IList<string>? list1, IList<string>? list2) =>
+        ReferenceEquals(list1, list2) ||
+        (list1 is not null && list2 is not null && list1.SequenceEqual(list2));
+
+    private static int GetListHashCode(IList<string>? list)
+    {
+        if (list is null)
+        {
+            return 0;
+        }
+
+        HashCode hc = default;
+        foreach (string item in list)
+        {
+            hc.Add(item);
+        }
+
+        return hc.ToHashCode();
+    }
+}
