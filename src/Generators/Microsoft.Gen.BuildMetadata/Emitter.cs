03c247d8c1d4325a510baf60cc01608cdbece242
evgenyfedorov2 <25526458+evgenyfedorov2@users.noreply.github.com>
2025-09-26 10:42:40 +0200

    PR Comments

--- src/Generators/Microsoft.Gen.BuildMetadata/Emitter.cs
+++ src/Generators/Microsoft.Gen.BuildMetadata/Emitter.cs
@@ -7,8 +7,12 @@ using Microsoft.Gen.Shared;
 
 namespace Microsoft.Gen.BuildMetadata;
 
+[SuppressMessage("Format", "S1199", Justification = "For better visualization of how the generated code will look like.")]
 internal sealed class Emitter : EmitterBase
 {
+    private const string DependencyInjectionNamespace = "global::Microsoft.Extensions.DependencyInjection.";
+    private const string ConfigurationNamespace = "global::Microsoft.Extensions.Configuration.";
+    private const string HostingNamespace = "global::Microsoft.Extensions.Hosting.";
     private readonly string? _buildId;
     private readonly string? _buildNumber;
     private readonly string? _sourceBranchName;
@@ -29,99 +33,115 @@ internal sealed class Emitter : EmitterBase
         return Capture();
     }
 
-    [SuppressMessage("Format", "IDE0055", Justification = "For better visualization of how the generated code will look like.")]
     private void GenerateBuildMetadataSource()
     {
         OutGeneratedCodeAttribute();
-        OutLn("[EditorBrowsable(EditorBrowsableState.Never)]");
-        OutLn("private sealed class BuildMetadataSource : IConfigurationSource");
+        OutLn("[global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]");
+        OutLn($"private sealed class BuildMetadataSource : {ConfigurationNamespace}IConfigurationSource");
         OutOpenBrace();
+        {
             OutLn("public string SectionName { get; }");
             OutLn();
 
             OutLn("public BuildMetadataSource(string sectionName)");
             OutOpenBrace();
+            {
                 OutNullGuards(checkBuilder: false);
                 OutLn("SectionName = sectionName;");
+            }
+
             OutCloseBrace();
             OutLn();
-
-            OutLn("public IConfigurationProvider Build(IConfigurationBuilder builder)");
+            OutLn($"public {ConfigurationNamespace}IConfigurationProvider Build({ConfigurationNamespace}IConfigurationBuilder builder)");
             OutOpenBrace();
-                OutLn("return new MemoryConfigurationProvider(new MemoryConfigurationSource())");
+            {
+                OutLn($"return new {ConfigurationNamespace}Memory.MemoryConfigurationProvider(new {ConfigurationNamespace}Memory.MemoryConfigurationSource())");
                 OutOpenBrace();
+                {
                     OutLn($$"""{ $"{SectionName}:buildid", "{{_buildId}}" },""");
                     OutLn($$"""{ $"{SectionName}:buildnumber", "{{_buildNumber}}" },""");
                     OutLn($$"""{ $"{SectionName}:sourcebranchname", "{{_sourceBranchName}}" },""");
                     OutLn($$"""{ $"{SectionName}:sourceversion", "{{_sourceVersion}}" },""");
+                }
+
                 OutCloseBraceWithExtra(";");
+            }
+
             OutCloseBrace();
+        }
+
         OutCloseBrace();
     }
 
-    [SuppressMessage("Format", "IDE0055", Justification = "For better visualization of how the generated code will look like.")]
     private void GenerateBuildMetadataExtensions()
     {
         OutLn("namespace Microsoft.Extensions.AmbientMetadata");
         OutOpenBrace();
-            OutLn("using System;");
-            OutLn("using System.ComponentModel;");
-            OutLn("using System.Diagnostics.CodeAnalysis;");
-            OutLn("using Microsoft.Extensions.Configuration;");
-            OutLn("using Microsoft.Extensions.Configuration.Memory;");
-            OutLn("using Microsoft.Extensions.DependencyInjection;");
-            OutLn("using Microsoft.Extensions.Hosting;");
-            OutLn();
-
+        {
             OutGeneratedCodeAttribute();
             OutLn("internal static class BuildMetadataGeneratedExtensions");
             OutOpenBrace();
+            {
                 OutLn("private const string DefaultSectionName = \"ambientmetadata:build\";");
                 OutLn();
 
                 GenerateBuildMetadataSource();
                 OutLn();
 
-                OutLn("public static IHostBuilder UseBuildMetadata(this IHostBuilder builder, string sectionName = DefaultSectionName)");
+                OutLn($"public static {HostingNamespace}IHostBuilder UseBuildMetadata(this {HostingNamespace}IHostBuilder builder, string sectionName = DefaultSectionName)");
                 OutOpenBrace();
+                {
                     OutNullGuards();
                     OutLn("_ = builder.ConfigureHostConfiguration(configBuilder => configBuilder.AddBuildMetadata(sectionName))");
                     Indent();
-                        OutLn(".ConfigureServices((hostBuilderContext, serviceCollection) =>");
-                        Indent();
-                            OutLn("serviceCollection.AddBuildMetadata(hostBuilderContext.Configuration.GetSection(sectionName)));");
-                        Unindent();
+                    OutLn(".ConfigureServices((hostBuilderContext, serviceCollection) =>");
+                    Indent();
+                    OutLn($"{DependencyInjectionNamespace}BuildMetadataServiceCollectionExtensions.AddBuildMetadata(serviceCollection, hostBuilderContext.Configuration.GetSection(sectionName)));");
+                    Unindent();
                     Unindent();
                     OutLn();
 
                     OutLn("return builder;");
+                }
+
                 OutCloseBrace();
                 OutLn();
 
                 OutLn("public static TBuilder UseBuildMetadata<TBuilder>(this TBuilder builder, string sectionName = DefaultSectionName)");
                 Indent();
-                    OutLn("where TBuilder : IHostApplicationBuilder");
+                OutLn($"where TBuilder : {HostingNamespace}IHostApplicationBuilder");
                 Unindent();
                 OutOpenBrace();
+                {
                     OutNullGuards();
                     OutLn("_ = builder.Configuration.AddBuildMetadata(sectionName);");
-                    OutLn("_ = builder.Services.AddBuildMetadata(builder.Configuration.GetSection(sectionName));");
+                    OutLn($"{DependencyInjectionNamespace}BuildMetadataServiceCollectionExtensions.AddBuildMetadata(builder.Services, builder.Configuration.GetSection(sectionName));");
                     OutLn();
 
                     OutLn("return builder;");
+                }
+
                 OutCloseBrace();
                 OutLn();
 
-                OutLn("public static IConfigurationBuilder AddBuildMetadata(this IConfigurationBuilder builder, string sectionName = DefaultSectionName)");
+#pragma warning disable S103 // Lines should not be too long
+                OutLn($"public static {ConfigurationNamespace}IConfigurationBuilder AddBuildMetadata(this {ConfigurationNamespace}IConfigurationBuilder builder, string sectionName = DefaultSectionName)");
+#pragma warning restore S103 // Lines should not be too long
                 OutOpenBrace();
+                {
                     OutNullGuards();
                     OutLn("return builder.Add(new BuildMetadataSource(sectionName));");
+                }
+
                 OutCloseBrace();
+            }
+
             OutCloseBrace();
+        }
+
         OutCloseBrace();
     }
 
-    [SuppressMessage("Format", "IDE0055", Justification = "For better visualization of how the generated code will look like.")]
     private void OutNullGuards(bool checkBuilder = true)
     {
         OutPP("#if !NET");
@@ -130,29 +150,35 @@ internal sealed class Emitter : EmitterBase
         {
             OutLn("if (builder is null)");
             OutOpenBrace();
-            OutLn("throw new ArgumentNullException(nameof(builder));");
+            OutLn("throw new global::System.ArgumentNullException(nameof(builder));");
             OutCloseBrace();
             OutLn();
         }
 
-            OutLn("if (string.IsNullOrWhiteSpace(sectionName))");
+        OutLn("if (global::System.string.IsNullOrWhiteSpace(sectionName))");
+        OutOpenBrace();
+        {
+            OutLn("if (sectionName is null)");
             OutOpenBrace();
-                OutLn("if (sectionName is null)");
-                OutOpenBrace();
-                    OutLn("throw new ArgumentNullException(nameof(sectionName));");
-                OutCloseBrace();
-                OutLn();
-                OutLn("throw new ArgumentException(\"The value cannot be an empty string or composed entirely of whitespace.\", nameof(sectionName));");
+            {
+                OutLn("throw new global::System.ArgumentNullException(nameof(sectionName));");
+            }
+
             OutCloseBrace();
+            OutLn();
+            OutLn("throw new global::System.ArgumentException(\"The value cannot be an empty string or composed entirely of whitespace.\", nameof(sectionName));");
+        }
+
+        OutCloseBrace();
 
         OutPP("#else");
 
         if (checkBuilder)
         {
-            OutLn("ArgumentNullException.ThrowIfNull(builder);");
+            OutLn("global::System.ArgumentNullException.ThrowIfNull(builder);");
         }
 
-            OutLn("ArgumentException.ThrowIfNullOrWhiteSpace(sectionName);");
+        OutLn("global::System.ArgumentException.ThrowIfNullOrWhiteSpace(sectionName);");
 
         OutPP("#endif");
         OutLn();
