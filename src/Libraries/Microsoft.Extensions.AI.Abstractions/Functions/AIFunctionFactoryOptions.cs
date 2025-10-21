bd93cd85b4ff38ffcefeea405ce4041222af03c2
Stephen Toub <stoub@microsoft.com>
2025-10-21 17:12:12 -0400

    Update src/Libraries/Microsoft.Extensions.AI.Abstractions/Functions/AIFunctionFactoryOptions.cs

--- src/Libraries/Microsoft.Extensions.AI.Abstractions/Functions/AIFunctionFactoryOptions.cs
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/Functions/AIFunctionFactoryOptions.cs
@@ -39,8 +39,7 @@ public sealed class AIFunctionFactoryOptions
 
     /// <summary>Gets or sets the name to use for the function.</summary>
     /// <value>
-    /// The name to use for the function. The default value is a name derived from any <see cref="DisplayNameAttribute"/> on the method represented by the passed <see cref="Delegate"/> or <see cref="MethodInfo"/>, if available,
-    /// or else from the name of the method.
+    /// The name to use for the function. The default value is a name derived from the passed <see cref="Delegate"/> or <see cref="MethodInfo"/> (for example, via a <see cref="DisplayNameAttribute"/> on the method).
     /// </value>
     public string? Name { get; set; }
 
