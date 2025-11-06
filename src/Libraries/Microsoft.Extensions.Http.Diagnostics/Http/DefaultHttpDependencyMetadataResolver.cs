e9269496828586afe213fd664f9b37ee02cb55b9
Dmytro Bohdanov <dbohdanov@microsoft.com>
2025-11-06 15:05:38 +0100

    fix lint

--- src/Libraries/Microsoft.Extensions.Http.Diagnostics/Http/DefaultHttpDependencyMetadataResolver.cs
+++ src/Libraries/Microsoft.Extensions.Http.Diagnostics/Http/DefaultHttpDependencyMetadataResolver.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.Shared.DiagnosticIds;
 
 namespace Microsoft.Extensions.Http.Diagnostics;
 
