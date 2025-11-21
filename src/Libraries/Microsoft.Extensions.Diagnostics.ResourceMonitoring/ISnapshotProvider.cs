e99b439ee6b809bda4a7c58e34dbc000c1df7cb8
Eric Chapman <echapman@bunnings.com.au>
2025-11-22 05:23:42 +0800

    Mark resource monitoring APIs as obsolete
    Changed the stage of ISnapshotProvider and Snapshot types and their members from 'Experimental' to 'Obsolete' in the resource monitoring JSON manifest. Also removed unused usings from ISnapshotProvider.cs.

--- src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/ISnapshotProvider.cs
+++ src/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring/ISnapshotProvider.cs
@@ -1,10 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.Shared.DiagnosticIds;
-
 namespace Microsoft.Extensions.Diagnostics.ResourceMonitoring;
 
 /// <summary>
