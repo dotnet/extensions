e33b937e2ee375e7ce26589495007534f8e339ac
amadeuszl <lechniak.amadeusz07@gmail.com>
2026-02-02 11:33:35 +0100

    Fix test

--- test/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring.Tests/Linux/AcceptanceTest.cs
+++ test/Libraries/Microsoft.Extensions.Diagnostics.ResourceMonitoring.Tests/Linux/AcceptanceTest.cs
@@ -469,7 +469,7 @@ public sealed class AcceptanceTest
         listener.RecordObservableInstruments();
 
         Assert.Equal(42, Math.Round(cpuLimitFromGauge * 100));
-        Assert.Equal(83, Math.Round(cpuRequestFromGauge * 100));
+        Assert.Equal(84, Math.Round(cpuRequestFromGauge * 100));
         Assert.Equal(167, Math.Round(cpuUserTime * 100));
         Assert.Equal(81, Math.Round(cpuKernelTime * 100));
 
