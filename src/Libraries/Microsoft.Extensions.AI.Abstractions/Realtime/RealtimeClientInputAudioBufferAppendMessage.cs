7c5eeb2c59897b2b3136a9d35cfd57c60f799f40
Tarek Mahmoud Sayed <tarekms@ntdev.microsoft.com>
2026-03-04 11:33:28 -0800

    Add null validation to Content property setter

--- src/Libraries/Microsoft.Extensions.AI.Abstractions/Realtime/RealtimeClientInputAudioBufferAppendMessage.cs
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/Realtime/RealtimeClientInputAudioBufferAppendMessage.cs
@@ -13,13 +13,15 @@ namespace Microsoft.Extensions.AI;
 [Experimental(DiagnosticIds.Experiments.AIRealTime, UrlFormat = DiagnosticIds.UrlFormat)]
 public class RealtimeClientInputAudioBufferAppendMessage : RealtimeClientMessage
 {
+    private DataContent _content;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="RealtimeClientInputAudioBufferAppendMessage"/> class.
     /// </summary>
     /// <param name="audioContent">The data content containing the audio buffer data to append.</param>
     public RealtimeClientInputAudioBufferAppendMessage(DataContent audioContent)
     {
-        Content = Throw.IfNull(audioContent);
+        _content = Throw.IfNull(audioContent);
     }
 
     /// <summary>
@@ -28,5 +30,9 @@ public class RealtimeClientInputAudioBufferAppendMessage : RealtimeClientMessage
     /// <remarks>
     /// The content should include the audio buffer data that needs to be appended to the input audio buffer.
     /// </remarks>
-    public DataContent Content { get; set; }
+    public DataContent Content
+    {
+        get => _content;
+        set => _content = Throw.IfNull(value);
+    }
 }
