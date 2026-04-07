9b928349f247d3c8306ac26fc568cafe09cc42bd
Shay Rojansky <roji@roji.org>
2026-04-07 20:23:01 +0200

    Tweak UnreachableException

--- src/LegacySupport/UnreachableException/UnreachableException.cs
+++ src/LegacySupport/UnreachableException/UnreachableException.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if !NET7_0_OR_GREATER
-
 using System.Diagnostics.CodeAnalysis;
 
 #pragma warning disable CA1064 // Exceptions should be public
@@ -48,5 +46,3 @@ internal sealed class UnreachableException : Exception
     {
     }
 }
-
-#endif
