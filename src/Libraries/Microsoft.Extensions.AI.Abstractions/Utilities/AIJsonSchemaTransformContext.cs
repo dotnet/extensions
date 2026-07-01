8cf9f0ec58186524a97a2ad1f99f35cf97ada7b3
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2026-06-30 19:05:38 -0700

    fix warnings

--- src/Libraries/Microsoft.Extensions.AI.Abstractions/Utilities/AIJsonSchemaTransformContext.cs
+++ src/Libraries/Microsoft.Extensions.AI.Abstractions/Utilities/AIJsonSchemaTransformContext.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Extensions.AI;
 /// Defines the context for transforming a schema node withing a larger schema document.
 /// </summary>
 /// <remarks>
-/// This struct is being passed to the user-provided <see cref="AIJsonSchemaTransformOptions.TransformSchemaNode"/> 
+/// This struct is being passed to the user-provided <see cref="AIJsonSchemaTransformOptions.TransformSchemaNode"/>
 /// callback by the <see cref="AIJsonUtilities.CreateJsonSchema"/> method and cannot be instantiated directly.
 /// </remarks>
 public readonly struct AIJsonSchemaTransformContext
@@ -34,12 +34,12 @@ public readonly struct AIJsonSchemaTransformContext
     public string? PropertyName => Path is [.., "properties", string name] ? name : null;
 
     /// <summary>
-    /// Gets a value that indicates whether the current schema is a collection element.
+    /// Gets a value indicating whether the current schema is a collection element.
     /// </summary>
     public bool IsCollectionElementSchema => Path is [.., "items"];
 
     /// <summary>
-    /// Gets a value that indicates whether the current schema is a dictionary value.
+    /// Gets a value indicating whether the current schema is a dictionary value.
     /// </summary>
     public bool IsDictionaryValueSchema => Path is [.., "additionalProperties"];
 }
