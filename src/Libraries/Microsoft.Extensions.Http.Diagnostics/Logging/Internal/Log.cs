7836d76052c37d2ad3120f7809a84a142f2972f9
Louis Deconinck <louis.dck@gmail.com>
2026-09-10 21:42:33 +0200

    Fix duplicate separator in HTTP logging
    Fixes #7737

--- src/Libraries/Microsoft.Extensions.Http.Diagnostics/Logging/Internal/Log.cs
+++ src/Libraries/Microsoft.Extensions.Http.Diagnostics/Logging/Internal/Log.cs
@@ -164,10 +164,11 @@ internal static partial class Log
         var httpMethod = request[startIndex].Value;
         var httpHost = request[startIndex + 1].Value;
         var httpPath = request[startIndex + 2].Value;
+        var separator = httpPath is string path && path.Length > 0 && path[0] == '/' ? string.Empty : "/";
 #if NET
-        return string.Create(CultureInfo.InvariantCulture, stackalloc char[256], $"{httpMethod} {httpHost}/{httpPath}");
+        return string.Create(CultureInfo.InvariantCulture, stackalloc char[256], $"{httpMethod} {httpHost}{separator}{httpPath}");
 #else
-        return FormattableString.Invariant($"{httpMethod} {httpHost}/{httpPath}");
+        return FormattableString.Invariant($"{httpMethod} {httpHost}{separator}{httpPath}");
 #endif
     }
 
