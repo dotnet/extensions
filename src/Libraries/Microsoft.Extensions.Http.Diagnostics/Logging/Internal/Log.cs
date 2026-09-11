e4591440d1fe7b4139c5addefe3ea7a847d99406
Louis Deconinck <louis.dck@gmail.com>
2026-09-11 15:31:49 +0200

    Optimize HTTP logging formatter

--- src/Libraries/Microsoft.Extensions.Http.Diagnostics/Logging/Internal/Log.cs
+++ src/Libraries/Microsoft.Extensions.Http.Diagnostics/Logging/Internal/Log.cs
@@ -164,11 +164,32 @@ internal static partial class Log
         var httpMethod = request[startIndex].Value;
         var httpHost = request[startIndex + 1].Value;
         var httpPath = request[startIndex + 2].Value;
-        var separator = httpPath is string path && path.Length > 0 && path[0] == '/' ? string.Empty : "/";
+        var hasLeadingSlash =
+            httpPath is string path &&
+            path.Length > 0 &&
+            path[0] == '/';
 #if NET
-        return string.Create(CultureInfo.InvariantCulture, stackalloc char[256], $"{httpMethod} {httpHost}{separator}{httpPath}");
+        if (hasLeadingSlash)
+        {
+            return string.Create(
+                CultureInfo.InvariantCulture,
+                stackalloc char[256],
+                $"{httpMethod} {httpHost}{httpPath}");
+        }
+
+        return string.Create(
+            CultureInfo.InvariantCulture,
+            stackalloc char[256],
+            $"{httpMethod} {httpHost}/{httpPath}");
 #else
-        return FormattableString.Invariant($"{httpMethod} {httpHost}{separator}{httpPath}");
+        if (hasLeadingSlash)
+        {
+            return FormattableString.Invariant(
+                $"{httpMethod} {httpHost}{httpPath}");
+        }
+
+        return FormattableString.Invariant(
+            $"{httpMethod} {httpHost}/{httpPath}");
 #endif
     }
 
